b2c01ef4f52aea229925eb621b33cdd9722104ed
Daniel Thwaites <danthwaites30@btinternet.com>
2020-12-21 14:10:40 +0000

    Update console colors to match Kitty

--- modules/console.nix
+++ modules/console.nix
@@ -14,12 +14,12 @@ with config.lib.stylix.colors;
       base0C-hex
       base05-hex
       base03-hex
-      base08-hex
-      base0B-hex
-      base0A-hex
-      base0D-hex
-      base0E-hex
-      base0C-hex
+      base09-hex
+      base01-hex
+      base02-hex
+      base04-hex
+      base06-hex
+      base0F-hex
       base07-hex
     ];
   };
