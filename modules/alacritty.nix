458d0d3719f2f79031426fb0e500a61b1e2688a8
Lyndon Sanche <lsanche@lyndeno.ca>
2022-07-20 07:59:30 -0600

    Finish alacritty module

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -5,12 +5,12 @@ let
       owner = "aarowill";
       repo = "base16-alacritty";
       rev = "914727e48ebf3eab1574e23ca0db0ecd0e5fe9d0";
-      sha256 = lib.fakeSha256;
+      sha256 = "sha256-oDsuiKx8gt+Ov7hZ9PibIQtE81IRSLO+n5N99WeiK34=";
     };
   };
 in
 {
   home-manager.sharedModules = [{
     programs.alacritty.settings.import = [ themeFile ];
-  };
+  }];
 }
