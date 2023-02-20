28c4968982eb504c0afeb19f9899e8b9a0698e65
deinferno <14363193+deinferno@users.noreply.github.com>
2023-02-21 00:06:25 +0500

    Add a xresources module

---
+++ modules/xresources/hm.nix
@@ -0,0 +1,30 @@
+{ pkgs, config, lib, ... }:
+
+with config.stylix.fonts;
+
+let
+  themeFile = config.lib.stylix.colors {
+    templateRepo = pkgs.fetchFromGitHub {
+      owner = "tinted-theming";
+      repo = "base16-xresources";
+      rev = "6711cf4fa61e903e52ef6eac186b83e04a0397d8";
+      sha256 = "sha256-WazWviLhQRCyF9EBi2IDn9h8wrKc00PpqtltFDpUP5Q=";
+    };
+  };
+in
+{
+  options.stylix.targets.xresources.enable =
+    config.lib.stylix.mkEnableTarget "Xresources" true;
+
+  config = lib.mkIf config.stylix.targets.xresources.enable {
+    home-manager.sharedModules = [{
+      xresources = {
+        properties = {
+          "*.faceName" = monospace.name;
+          "*.renderFont" = true;
+        };
+        extraConfig = builtins.readFile themeFile;
+      };
+    }];
+  };
+}
