d4759279ce0f60ed5991ce53abecd0fa80679aa4
Daniel Thwaites <danthwaites30@btinternet.com>
2023-03-20 09:49:54 +0000

    Add module for Foot :sparkles:

---
+++ modules/foot/hm.nix
@@ -0,0 +1,26 @@
+{ pkgs, config, lib, ... }:
+
+let
+  cfg = config.stylix.targets.foot;
+
+  theme = config.lib.stylix.colors {
+    templateRepo = pkgs.fetchFromGitHub {
+      owner = "tinted-theming";
+      repo = "base16-foot";
+      rev = "22579065fbe21361b64d80bae798666efaa85ce0";
+      sha256 = "gb43UufuN6iRu6MfTZP+a3GUUQ+hKa5e6G4IiWEc68A=";
+    };
+  };
+
+in {
+  options.stylix.targets.foot.enable =
+    config.lib.stylix.mkEnableTarget "Foot" true;
+
+  config.programs.foot.settings.main = lib.mkIf cfg.enable {
+    include = theme;
+    font =
+      with config.stylix.fonts;
+      "${monospace.name}:size=${toString sizes.terminal}";
+    dpi-aware = "no";
+  };
+}
