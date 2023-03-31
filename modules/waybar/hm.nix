58be697b3a4c74257fa117925306e0b3aa90f1c2
SomeGuyNamedMy <mfdear444@gmail.com>
2023-03-31 12:18:44 -0400

    add padding

--- modules/waybar/hm.nix
+++ modules/waybar/hm.nix
@@ -29,43 +29,34 @@ with config.stylix.fonts;
 
     @define-color base08 ${base08}; @define-color base09 ${base09}; @define-color base0A ${base0A}; @define-color base0B ${base0B};
     @define-color base0C ${base0C}; @define-color base0D ${base0D}; @define-color base0E ${base0E}; @define-color base0F ${base0F};
-      * {
-          border: none;
-          border-radius: 0;
-          font-family: ${sansSerif.name};
-          font-size: ${builtins.toString sizes.desktop};
-      }
-      window#waybar {
-          background: @base00;
-          color: @base05;
-      }
-      ''
-      + (if config.stylix.targets.waybar.enableLeftBackColors then builtins.readFile ./left.css else ''
-      .modules-left #workspaces button {
-          border-bottom: 3px solid transparent;
-      }
-      .modules-left #workspaces button.focused,
-      .modules-left #workspaces button.active {
-          border-bottom: 3px solid @base05;
-      }
-      '')
-      + (if config.stylix.targets.waybar.enableCenterBackColors then builtins.readFile ./center.css else ''
-      .modules-center #workspaces button {
-          border-bottom: 3px solid transparent;
-      }
-      .modules-center #workspaces button.focused,
-      .modules-center #workspaces button.active {
-          border-bottom: 3px solid @base05;
-      }
-      '')
-      + (if config.stylix.targets.waybar.enableRightBackColors then builtins.readFile ./right.css else ''
-      .modules-right #workspaces button {
-          border-bottom: 3px solid transparent;
-      }
-      .modules-right #workspaces button.focused,
-      .modules-right #workspaces button.active {
-          border-bottom: 3px solid @base05;
-      }
-      '');
+    ''
+    + (builtins.readFile ./base.css)
+    + (if config.stylix.targets.waybar.enableLeftBackColors then builtins.readFile ./left.css else ''
+    .modules-left #workspaces button {
+        border-bottom: 3px solid transparent;
+    }
+    .modules-left #workspaces button.focused,
+    .modules-left #workspaces button.active {
+        border-bottom: 3px solid @base05;
+    }
+    '')
+    + (if config.stylix.targets.waybar.enableCenterBackColors then builtins.readFile ./center.css else ''
+    .modules-center #workspaces button {
+        border-bottom: 3px solid transparent;
+    }
+    .modules-center #workspaces button.focused,
+    .modules-center #workspaces button.active {
+        border-bottom: 3px solid @base05;
+    }
+    '')
+    + (if config.stylix.targets.waybar.enableRightBackColors then builtins.readFile ./right.css else ''
+    .modules-right #workspaces button {
+        border-bottom: 3px solid transparent;
+    }
+    .modules-right #workspaces button.focused,
+    .modules-right #workspaces button.active {
+        border-bottom: 3px solid @base05;
+    }
+    '');
   };
 }
