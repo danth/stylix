8e8906faa83f702653dcbfa3564ddb6722903b96
Mason Dear <83663766+SomeGuyNamedMy@users.noreply.github.com>
2023-03-31 14:29:08 -0400

    fix another typo

--- stylix/opacity.nix
+++ stylix/opacity.nix
@@ -10,22 +10,22 @@ in {
         desktop = mkOption {
             description = mkDoc "The opacity of the windows of bars/widgets";
             type = types.float;
-            default = 1;
+            default = 1.0;
         };
         applications = mkOption {
             description = mkDoc "The opacity of the windows of applications";
             type = types.float;
-            default = 1;
+            default = 1.0;
         };
         terminal = mkOption {
             description = mkDoc "The opacity of the windows of terminals";
             type = types.float;
-            default = 1;
+            default = 1.0;
         };
         popups = mkOption {
             description = mkDoc "The opacity of the windows of notifications/popups";
             type = types.float;
-            default = 1;
+            default = 1.0;
         };
     };
 }
