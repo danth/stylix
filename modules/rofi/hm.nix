982f2bfa3af189a738cf303c5151586d7038edc8
SomeGuyNamedMy <mfdear444@gmail.com>
2023-04-12 19:47:21 -0400

    fix

--- modules/rofi/hm.nix
+++ modules/rofi/hm.nix
@@ -3,14 +3,6 @@
 with config.stylix.fonts;
 
 let
-  themeFile = config.lib.stylix.colors {
-    templateRepo = pkgs.fetchFromGitHub {
-      owner = "tinted-theming";
-      repo = "base16-rofi";
-      rev = "3f64a9f8d8cb7db796557b516682b255172c4ab4";
-      sha256 = "sha256-RZpjCQ8KGO3cv9A/lNNoTE+WJ9sNk5sz0zJq02zzxA8=";
-    };
-  };
   rofiOpacity = builtins.toString (builtins.ceil (config.stylix.opacity.popups * 100));
   extraCss = ''
   * {
@@ -18,6 +10,14 @@ let
    lightbg: rgba ( {{base01-rgb-r}}, {{base01-rgb-g}}, {{base01-rgb-b}}, ${rofiOpacity} % );
   }
   '';
+  themeFile = config.lib.stylix.colors {
+    templateRepo = pkgs.fetchFromGitHub {
+      owner = "tinted-theming";
+      repo = "base16-rofi";
+      rev = "3f64a9f8d8cb7db796557b516682b255172c4ab4";
+      sha256 = "sha256-RZpjCQ8KGO3cv9A/lNNoTE+WJ9sNk5sz0zJq02zzxA8=";
+    };
+  }/default.mustache + extraCss;
 in
 {
   options.stylix.targets.rofi.enable =
@@ -26,7 +26,7 @@ in
   config = lib.mkIf config.stylix.targets.rofi.enable {
     programs.rofi = {
       font = "${monospace.name} ${toString sizes.popups}";
-      theme = themeFile + extraCss;
+      theme = themeFile;
     };
   };
 }
