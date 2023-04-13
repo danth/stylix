dfd8d72e6414aee6cd44c74617be4f8821965321
SomeGuyNamedMy <mfdear444@gmail.com>
2023-04-12 20:08:40 -0400

    fix

--- modules/rofi/hm.nix
+++ modules/rofi/hm.nix
@@ -4,13 +4,11 @@ with config.stylix.fonts;
 
 let
   rofiOpacity = builtins.toString (builtins.ceil (config.stylix.opacity.popups * 100));
-  themeFile = config.lib.stylix.colors {
-    templateRepo = pkgs.fetchFromGitHub {
+  themeFile = pkgs.fetchFromGitHub {
       owner = "tinted-theming";
       repo = "base16-rofi";
       rev = "3f64a9f8d8cb7db796557b516682b255172c4ab4";
       sha256 = "sha256-RZpjCQ8KGO3cv9A/lNNoTE+WJ9sNk5sz0zJq02zzxA8=";
-    };
   };
   finalString = builtins.replaceStrings [
    "background: rgba ( {{base00-rgb-r}}, {{base00-rgb-g}}, {{base00-rgb-b}}, 100 % );"
@@ -19,7 +17,9 @@ let
    "background: rgba ( {{base00-rgb-r}}, {{base00-rgb-g}}, {{base00-rgb-b}}, ${rofiOpacity} % );"
    "lightbg: rgba ( {{base01-rgb-r}}, {{base01-rgb-g}}, {{base01-rgb-b}}, ${rofiOpacity} % );"
   ] (builtins.readFile themeFile/default.mustache);
-  finalFile = builtins.toFile "Final" finalString;
+  finalFile = config.lib.stylix.colors {
+      templateRepo = builtins.toFile "Final" finalString;
+  }
 in
 {
   options.stylix.targets.rofi.enable =
