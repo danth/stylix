c07754c6a6ec1f3617f24ed738fb9d3569eb25d0
SomeGuyNamedMy <mfdear444@gmail.com>
2023-04-13 12:55:11 -0400

    add template file locally

--- modules/rofi/hm.nix
+++ modules/rofi/hm.nix
@@ -4,19 +4,13 @@ with config.stylix.fonts;
 
 let
   rofiOpacity = builtins.toString (builtins.ceil (config.stylix.opacity.popups * 100));
-  themeFile = pkgs.fetchFromGitHub {
-      owner = "tinted-theming";
-      repo = "base16-rofi";
-      rev = "3f64a9f8d8cb7db796557b516682b255172c4ab4";
-      sha256 = "sha256-RZpjCQ8KGO3cv9A/lNNoTE+WJ9sNk5sz0zJq02zzxA8=";
-  };
   finalString = builtins.replaceStrings [
    "background: rgba ( {{base00-rgb-r}}, {{base00-rgb-g}}, {{base00-rgb-b}}, 100 % );"
    "lightbg: rgba ( {{base01-rgb-r}}, {{base01-rgb-g}}, {{base01-rgb-b}}, 100 % );"
   ] [
    "background: rgba ( {{base00-rgb-r}}, {{base00-rgb-g}}, {{base00-rgb-b}}, ${rofiOpacity} % );"
    "lightbg: rgba ( {{base01-rgb-r}}, {{base01-rgb-g}}, {{base01-rgb-b}}, ${rofiOpacity} % );"
-  ] (builtins.readFile ${themeFile}/templates/default.mustache);
+  ] (builtins.readFile ./templates.mustache);
   finalFile = config.lib.stylix.colors {
       templateRepo = builtins.toFile "Final" finalString;
   };
