018928392f2ddd4880dd274f7dc4ec488727ea3b
SomeGuyNamedMy <mfdear444@gmail.com>
2023-04-13 01:12:39 -0400

    fix

--- modules/rofi/hm.nix
+++ modules/rofi/hm.nix
@@ -10,23 +10,22 @@ let
       rev = "3f64a9f8d8cb7db796557b516682b255172c4ab4";
       sha256 = "sha256-RZpjCQ8KGO3cv9A/lNNoTE+WJ9sNk5sz0zJq02zzxA8=";
   };
+  finalString = builtins.replaceStrings [
+   "background: rgba ( {{base00-rgb-r}}, {{base00-rgb-g}}, {{base00-rgb-b}}, 100 % );"
+   "lightbg: rgba ( {{base01-rgb-r}}, {{base01-rgb-g}}, {{base01-rgb-b}}, 100 % );"
+  ] [
+   "background: rgba ( {{base00-rgb-r}}, {{base00-rgb-g}}, {{base00-rgb-b}}, ${rofiOpacity} % );"
+   "lightbg: rgba ( {{base01-rgb-r}}, {{base01-rgb-g}}, {{base01-rgb-b}}, ${rofiOpacity} % );"
+  ] (builtins.readFile ${themeFile}/templates/default.mustache);
+  finalFile = config.lib.stylix.colors {
+      templateRepo = builtins.toFile "Final" finalString;
+  };
 in
 {
   options.stylix.targets.rofi.enable =
     config.lib.stylix.mkEnableTarget "Rofi" true;
 
-  config = let
-    finalString = builtins.replaceStrings [
-     "background: rgba ( {{base00-rgb-r}}, {{base00-rgb-g}}, {{base00-rgb-b}}, 100 % );"
-     "lightbg: rgba ( {{base01-rgb-r}}, {{base01-rgb-g}}, {{base01-rgb-b}}, 100 % );"
-    ] [
-     "background: rgba ( {{base00-rgb-r}}, {{base00-rgb-g}}, {{base00-rgb-b}}, ${rofiOpacity} % );"
-     "lightbg: rgba ( {{base01-rgb-r}}, {{base01-rgb-g}}, {{base01-rgb-b}}, ${rofiOpacity} % );"
-    ] (builtins.readFile themeFile/templates/default.mustache);
-    finalFile = config.lib.stylix.colors {
-        templateRepo = builtins.toFile "Final" finalString;
-    };
-  in lib.mkIf config.stylix.targets.rofi.enable {
+  config = lib.mkIf config.stylix.targets.rofi.enable {
     programs.rofi = {
       font = "${monospace.name} ${toString sizes.popups}";
       theme = finalFile;
