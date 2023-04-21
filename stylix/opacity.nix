433257184aa7a9e536061b4de999736531eb5d9c
SomeGuyNamedMy <mfdear444@gmail.com>
2023-04-21 16:23:00 -0400

    mkdoc to mddoc

--- stylix/opacity.nix
+++ stylix/opacity.nix
@@ -8,22 +8,22 @@ let
 in {
     options.stylix.opacity = {
         desktop = mkOption {
-            description = mkDoc "The opacity of the windows of bars/widgets";
+            description = mdDoc "The opacity of the windows of bars/widgets";
             type = types.float;
             default = fromOs [ "opacity" "desktop" ] 1.0;
         };
         applications = mkOption {
-            description = mkDoc "The opacity of the windows of applications";
+            description = mdDoc "The opacity of the windows of applications";
             type = types.float;
             default = fromOs [ "opacity" "applications" ] 1.0;
         };
         terminal = mkOption {
-            description = mkDoc "The opacity of the windows of terminals";
+            description = mdDoc "The opacity of the windows of terminals";
             type = types.float;
             default = fromOs [ "opacity" "terminal" ] 1.0;
         };
         popups = mkOption {
-            description = mkDoc "The opacity of the windows of notifications/popups";
+            description = mdDoc "The opacity of the windows of notifications/popups";
             type = types.float;
             default = fromOs [ "opacity" "popups" ] 1.0;
         };
