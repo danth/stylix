c1b8b98eb10fd0e753132dfce3da680d9da3f9a5
SomeGuyNamedMy <mfdear444@gmail.com>
2023-04-21 16:15:13 -0400

    another zathura fix

--- modules/zathura/hm.nix
+++ modules/zathura/hm.nix
@@ -1,9 +1,7 @@
 { config, lib, ... }:
 
 with config.lib.stylix.colors;
-let
-  zathOpacity = builtins.toString (builtins.ceil (config.stylix.opacity.applications * 100));
-in {
+{
   options.stylix.targets.zathura.enable =
     config.lib.stylix.mkEnableTarget "Zathura" true;
 
@@ -11,7 +9,7 @@ in {
     # Taken from here:
     #   https://github.com/doenerkebap/base16-zathura
     programs.zathura.options = {
-      default-bg = "rgba(${base00-rgb-r}, ${base00-rgb-g}, ${base00-rgb-b}, ${zathOpacity});";
+      default-bg = "rgba(${base00-rgb-r}, ${base00-rgb-g}, ${base00-rgb-b}, ${config.stylix.opacity.applications});";
       default-fg = "#${base01}";
       statusbar-fg = "#${base04}";
       statusbar-bg = "#${base02}";
