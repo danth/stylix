af09d19cc18ad56699f6e449cd7252c783c8eb7c
SomeGuyNamedMy <mfdear444@gmail.com>
2023-04-21 15:41:44 -0400

    fix zathura

--- modules/zathura/hm.nix
+++ modules/zathura/hm.nix
@@ -11,7 +11,7 @@ in {
     # Taken from here:
     #   https://github.com/doenerkebap/base16-zathura
     programs.zathura.options = {
-      default-bg = "#rgba(${base00-rgb-r}, ${base00-rgb-g}, ${base00-rgb-b}, ${zathOpacity})";
+      default-bg = "#${base00}$${zathOpacity}";
       default-fg = "#${base01}";
       statusbar-fg = "#${base04}";
       statusbar-bg = "#${base02}";
