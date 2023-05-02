32110c477bd3940800208ac0aeba96351968bd83
Mason Dear <83663766+SomeGuyNamedMy@users.noreply.github.com>
2023-05-02 17:23:43 -0400

    add statments about the limited amount of supported applications

--- stylix/opacity.nix
+++ stylix/opacity.nix
@@ -8,22 +8,22 @@ let
 in {
     options.stylix.opacity = {
         desktop = mkOption {
-            description = mdDoc "The opacity of the windows of bars/widgets";
+            description = mdDoc "The opacity of the windows of bars/widgets, the amount of applications supported is currently limited";
             type = types.float;
             default = fromOs [ "opacity" "desktop" ] 1.0;
         };
         applications = mkOption {
-            description = mdDoc "The opacity of the windows of applications";
+            description = mdDoc "The opacity of the windows of applications, the amount of applications supported is currently limited";
             type = types.float;
             default = fromOs [ "opacity" "applications" ] 1.0;
         };
         terminal = mkOption {
-            description = mdDoc "The opacity of the windows of terminals";
+            description = mdDoc "The opacity of the windows of terminals, this works accross all terminals supported by stylix";
             type = types.float;
             default = fromOs [ "opacity" "terminal" ] 1.0;
         };
         popups = mkOption {
-            description = mdDoc "The opacity of the windows of notifications/popups";
+            description = mdDoc "The opacity of the windows of notifications/popups, the amount of applications supported is currently limited";
             type = types.float;
             default = fromOs [ "opacity" "popups" ] 1.0;
         };
