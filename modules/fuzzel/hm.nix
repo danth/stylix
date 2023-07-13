952ba1c7567c97b984368c1b92d1e01b6e092465
Daniel Thwaites <danthwaites30@btinternet.com>
2023-07-13 19:24:44 +0100

    Add support for Fuzzel :sparkles:

---
+++ modules/fuzzel/hm.nix
@@ -0,0 +1,24 @@
+{ pkgs, config, lib, ... }:
+
+with config.lib.stylix.colors;
+
+let
+  opacity = lib.toHexString (builtins.ceil (config.stylix.opacity.popups * 255));
+
+in {
+  options.stylix.targets.fuzzel.enable =
+    config.lib.stylix.mkEnableTarget "Fuzzel" config.programs.fuzzel.enable;
+
+  config.programs.fuzzel.settings =
+    lib.mkIf config.stylix.targets.fuzzel.enable {
+      colors = {
+        background = "${base00-hex}${opacity}";
+        text = "${base05-hex}ff";
+        match = "${base0A-hex}ff";
+        selection = "${base03-hex}ff";
+        selection-text = "${base05-hex}ff";
+        selection-match = "${base0A-hex}ff";
+        border = "${base0D-hex}ff";
+      };
+    };
+}
