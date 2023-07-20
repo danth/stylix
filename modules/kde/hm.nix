589745601f790c9c533eb3ab6f9e9b9685063849
Bernardo Meurer <bernardo@meurer.org>
2023-07-20 12:33:56 -0400

    fix(modules/kde/hm): don't enable by default on darwin
    It doesn't make much sense, and it breaks eval on darwin since
    xdg.systemDirs is not available.

--- modules/kde/hm.nix
+++ modules/kde/hm.nix
@@ -134,7 +134,7 @@ let
 
 in {
   options.stylix.targets.kde.enable =
-    config.lib.stylix.mkEnableTarget "KDE" true;
+    config.lib.stylix.mkEnableTarget "KDE" (!pkgs.stdenv.hostPlatform.isDarwin);
 
   config = lib.mkIf config.stylix.targets.kde.enable {
     qt = {
