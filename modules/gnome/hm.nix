776ea994be4d30b2d21d23fe35a7199255dfa06a
SomeGuyNamedMy <mfdear444@gmail.com>
2023-08-12 14:33:27 -0400

    fix gnome

--- modules/gnome/hm.nix
+++ modules/gnome/hm.nix
@@ -46,20 +46,12 @@ in {
         document-font-name = "${serif.name}  ${toString (sizes.applications - 1)}";
         monospace-font-name = "${monospace.name} ${toString sizes.applications}";
 
-        if config.lib.stylix.backgroundPolarity == "dark"
+        color-scheme = if config.lib.stylix.backgroundPolarity == "dark"
         then "prefer-dark"
         else "default";
 
         "org/gnome/shell/extensions/user-theme".name = "Stylix";
       };
-      # We show the same colours regardless of this setting, and the quick
-      # settings tile is removed. The value is still used by Epiphany to
-      # request dark mode for websites which support it.
-      "org/gnome/desktop/interface" = with config.stylix.fonts; {
-        color-scheme = if config.lib.stylix.backgroundPolarity == "dark"
-        then "prefer-dark"
-        else "default";
-      "org/gnome/shell/extensions/user-theme".name = "Stylix";
     };
 
     xdg.dataFile."themes/Stylix/gnome-shell/gnome-shell.css" = {
