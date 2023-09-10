55b210b6250bb726337b8f7ec2b27c8fbbcf2743
Basil Keeler <70387628+TotalChaos05@users.noreply.github.com>
2023-09-10 14:58:13 -0500

    Add support for Hyprland (#145)
    Co-authored-by: Daniel Thwaites <danthwaites30@btinternet.com>

---
+++ modules/hyprland/hm.nix
@@ -0,0 +1,27 @@
+{ config, lib, ... }:
+
+with config.lib.stylix.colors;
+
+let
+  rgb = color: "rgb(${color})";
+  rgba = color: alpha: "rgba(${color}${alpha})";
+
+  settings = {
+    decoration."col.shadow" = rgba base00 "99";
+    general = {
+      "col.active_border" = rgb base0A;
+      "col.inactive_border" = rgb base03;
+      "col.group_border" = rgb base0D;
+      "col.group_border_active" = rgb base06;
+      "col.group_border_locked_active" = rgb base06;
+    };
+    misc.background_color = rgb base00;
+  };
+
+in {
+  options.stylix.targets.hyprland.enable =
+    config.lib.stylix.mkEnableTarget "Hyprland" true;
+
+  config.wayland.windowManager.hyprland.settings =
+    lib.mkIf config.stylix.targets.hyprland.enable settings;
+}
