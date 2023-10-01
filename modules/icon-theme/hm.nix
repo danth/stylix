32da08c497f144d325af7501f2027335db82498b
Ofek Itscovits <ofek4430@gmail.com>
2023-10-01 21:13:40 +0300

    Make the color list configurable

--- modules/icon-theme/hm.nix
+++ modules/icon-theme/hm.nix
@@ -31,56 +31,91 @@ in
         type = lib.types.enum [ "color" "color-from-palette" "palette" ];
         default = "palette";
       };
-      colorModeColor = lib.mkOption {
+      colors = lib.mkOption {
         description = "The color to use when mode is set to color";
-        type = lib.types.str;
-        default = config.lib.stylix.colors.withHashtag.base09;
-      };
-      smooth = lib.mkOption {
-        description = "#TODO explain this? kinda hard, showing some pictures will be easier.";
-        type = lib.types.bool;
-        default = true;
-      };
-    };
-  };
-
-  config = cfg.enable {
-    home-manager.users.${config.modules.user.name} = {
-      gtk = {
-        enable = true;
-        iconTheme = mkMerge [
-          {
-            name = cfg.name;
-          }
-
-          (lib.mkIf (!cfg.adjustColorScheme.enable) {
-            package = cfg.package;
-          })
-          (lib.mkIf (cfg.adjustColorScheme.enable) {
-            package = cfg.package.overrideAttrs
-              (oldAttrs: rec {
-                propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [
-                  (pkgs.python3.withPackages
-                    (ps: with ps; [
-                      colormath
-                      tqdm
-                      pillow
-                    ])
-                  )
-                ];
-                postInstall = with cfg.adjustColorScheme; with config.lib.stylix.colors.withHashtag; ''
-                  python3 ${./recolor.py} --src $out/share/icons --smooth '${toString smooth}' \
-                  ${if cfg.adjustColorScheme.mode == "color" then "--color '${colorModeColor}'"
-                  else if cfg.adjustColorScheme.mode == "color-from-palette" then "--color-from-palette '${base08},${base09},${base0A},${base0B},${base0C},${base0D},${base0E},${base0F}'"
-                  else "--palette '${base00},${base01},${base02},${base03},${base04},${base05},${base06},${base07},${base08},${base09},${base0A},${base0B},${base0C},${base0D},${base0E},${base0F}'"}
-                  for theme in $out/share/icons/*; do
-                    gtk-update-icon-cache $theme
-                  done
-                '';
-              });
-          })
-        ];
+        type = lib.types.listOf (lib.types.str);
+        default =
+          if cfg.adjustColorScheme.mode == "color" then
+            config.lib.stylix.colors.withHashtag.base09
+          else if cfg.adjustColorScheme.mode == "color-from-palette" then
+            with config.lib.stylix.colors.withHashtag; [
+              base08
+              base09
+              base0A
+              base0B
+              base0C
+              base0D
+              base0E
+              base0F
+            ]
+          else
+            with config.lib.stylix.colors.withHashtag; [
+              base00
+              base01
+              base02
+              base03
+              base04
+              base05
+              base06
+              base07
+              base08
+              base09
+              base0A
+              base0B
+              base0C
+              base0D
+              base0E
+              base0F
+            ];
+        smooth = lib.mkOption {
+          description = "#TODO explain this? kinda hard, showing some pictures will be easier.";
+          type = lib.types.bool;
+          default = true;
+        };
       };
     };
-  };
-}
+
+    config = cfg.enable {
+      home-manager.users.${config.modules.user.name} = {
+        gtk = {
+          enable = true;
+          iconTheme = lib.mkMerge [
+            {
+              name = cfg.name;
+            }
+
+            (lib.mkIf (!cfg.adjustColorScheme.enable) {
+              package = cfg.package;
+            })
+            (lib.mkIf (cfg.adjustColorScheme.enable) {
+              package = cfg.package.overrideAttrs
+                (oldAttrs: rec {
+                  propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [
+                    (pkgs.python3.withPackages
+                      (ps: with ps; [
+                        colormath
+                        tqdm
+                        pillow
+                      ])
+                    )
+                  ];
+                  postInstall = ''
+                    python3 ${./recolor.py} --src $out/share/icons --smooth '${toString cfg.adjustColorScheme.smooth}' \
+                    ${if cfg.adjustColorScheme.mode == "color" then
+                      "--color '${builtins.head cfg.adjustColorScheme.colors}'"
+                    else if cfg.adjustColorScheme.mode == "color-from-palette" then
+                      "--color-from-palette '${builtins.concatStringsSep "," cfg.adjustColorScheme.colors}'"
+                    else
+                      "--palette ''${builtins.concatStringsSep "," cfg.adjustColorScheme.colors}''"}
+
+                    for theme in $out/share/icons/*; do
+                      gtk-update-icon-cache $theme
+                    done
+                  '';
+                });
+            })
+          ];
+        };
+      };
+    };
+  }
