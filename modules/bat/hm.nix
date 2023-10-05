c7223c18648f956ce893bf0702b426b9a4db9928
Bernardo Meurer <bernardo@meurer.org>
2023-10-05 16:15:09 -0400

    fix(bat): don't use IFD

--- modules/bat/hm.nix
+++ modules/bat/hm.nix
@@ -1,17 +1,14 @@
 { pkgs, config, lib, ... }:
 
-let
-  themeFile = config.lib.stylix.colors {
-    template = ./base16-stylix.mustache;
-    extension = ".tmTheme";
-  };
-in
 {
   options.stylix.targets.bat.enable =
     config.lib.stylix.mkEnableTarget "Bat" config.programs.bat.enable;
 
   config = lib.mkIf config.stylix.targets.bat.enable {
-    programs.bat.themes."base16-stylix" = builtins.readFile themeFile;
+    programs.bat.themes."base16-stylix".src = config.lib.stylix.colors {
+      template = ./base16-stylix.mustache;
+      extension = ".tmTheme";
+    };
     home.sessionVariables.BAT_THEME = "base16-stylix";
   };
 }
