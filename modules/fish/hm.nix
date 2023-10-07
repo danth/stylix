13a7eac8988793b1efab92cc711203f8d33eaef7
Bernardo Meurer <bernardo@meurer.org>
2023-10-07 16:05:13 -0400

    fix(fish): don't use IFD

--- modules/fish/hm.nix
+++ modules/fish/hm.nix
@@ -5,6 +5,19 @@
     config.lib.stylix.mkEnableTarget "Fish" true;
 
   config = lib.mkIf config.stylix.targets.fish.enable {
-    programs.fish.interactiveShellInit = import ./prompt.nix { inherit pkgs config; };
+    programs.fish = {
+      interactiveShellInit = "base16-${config.lib.stylix.colors.slug}";
+      plugins = [{
+        name = "stylix";
+        src = config.lib.stylix.colors {
+          templateRepo = pkgs.fetchFromGitHub {
+            owner = "tomyun";
+            repo = "base16-fish";
+            rev = "7f647967fddedaf803191bc9113b13d2071dc3cf";
+            sha256 = "IGUbLjsmmAvB9UKGkR7oqdpjeVEfzt83GpyBkrZf2O4=";
+          };
+        };
+      }];
+    };
   };
 }
--- modules/fish/nixos.nix
+++ modules/fish/nixos.nix
@@ -5,6 +5,17 @@
     config.lib.stylix.mkEnableTarget "Fish" true;
 
   config = lib.mkIf config.stylix.targets.fish.enable {
-    programs.fish.promptInit = import ./prompt.nix { inherit pkgs config; };
+    environment.etc."fish/stylix.fish".source = config.lib.stylix.colors {
+      templateRepo = pkgs.fetchFromGitHub {
+        owner = "tomyun";
+        repo = "base16-fish";
+        rev = "7f647967fddedaf803191bc9113b13d2071dc3cf";
+        sha256 = "IGUbLjsmmAvB9UKGkR7oqdpjeVEfzt83GpyBkrZf2O4=";
+      };
+    };
+    programs.fish.promptInit = ''
+      source /etc/fish/stylix.fish
+      base16-${config.lib.stylix.colors.slug}
+    '';
   };
 }
