61cb5835fb05c435d5b5666c8aa6ed5903b92bd7
Daniel Thwaites <danthwaites30@btinternet.com>
2023-10-08 18:57:30 +0100

    Fix wallpaper not appearing on Hyprland :bug:
    Wallpapers cannot be applied until the swww daemon has finished
    loading, so the commands need to be in a script to make sure they
    run one after the other.

--- modules/hyprland/hm.nix
+++ modules/hyprland/hm.nix
@@ -6,17 +6,29 @@ let
   rgb = color: "rgb(${color})";
   rgba = color: alpha: "rgba(${color}${alpha})";
 
+  wallpaperScript = text:
+    let script = pkgs.writeShellApplication {
+      name = "wallpaper";
+      runtimeInputs = [ pkgs.swww ];
+      text = ''
+        # Start the daemon, ignoring an error if it was already running
+        swww init 2>/dev/null || true
+        # Apply the wallpaper
+        ${text}
+      '';
+    };
+    in "${script}/bin/wallpaper";
+
   settings = {
-    exec-once = if !(config.lib.stylix.isVideo config.stylix.wallpaper) then [      
-      "${pkgs.swww}/bin/swww-daemon" 
-    ] else [];
-    exec = if (config.lib.stylix.isVideo config.stylix.wallpaper) then [
-      "${pkgs.mpvpaper}/bin/mpvpaper '*' -o 'no-audio --loop' ${config.stylix.wallpaper.video}"              
-    ] else if (config.lib.stylix.isSlideshow config.stylix.wallpaper) then [
-      "${config.lib.stylix.waylandSlideshowScript}"
-    ] else [
-      "${pkgs.swww}/bin/swww img ${config.stylix.wallpaper.animation}"
-    ];
+    exec = [(
+      if config.lib.stylix.isVideo config.stylix.wallpaper
+      then "${pkgs.mpvpaper}/bin/mpvpaper '*' -o 'no-audio --loop' ${config.stylix.wallpaper.video}"
+      else if config.lib.stylix.isSlideshow config.stylix.wallpaper
+      then wallpaperScript "exec ${config.lib.stylix.waylandSlideshowScript}"
+      else if config.lib.stylix.isAnimation config.stylix.wallpaper
+      then wallpaperScript "swww img ${config.stylix.wallpaper.animation}"
+      else wallpaperScript "swww img ${config.stylix.wallpaper.image}"
+    )];
 
     decoration."col.shadow" = rgba base00 "99";
     general = {
