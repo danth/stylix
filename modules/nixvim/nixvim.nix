d00295d39f2b3be8812cfc81bc6d3027e4a05a93
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-10-23 22:38:24 +0200

    refactor(modules/nixvim): use already provided functionality
    Co-authored-by: Daniel Thwaites <danthwaites30@btinternet.com>

--- modules/nixvim/nixvim.nix
+++ modules/nixvim/nixvim.nix
@@ -8,25 +8,24 @@
 
   config = lib.mkIf config.stylix.targets.nixvim.enable {
     programs.nixvim.colorschemes.base16.customColorScheme = let
-      colors = config.lib.stylix.colors;
-      prependHash = color: "#${color}";
+      colors = config.lib.stylix.colors.withHashtag;
     in {
-      base00 = prependHash colors.base00;
-      base01 = prependHash colors.base01;
-      base02 = prependHash colors.base02;
-      base03 = prependHash colors.base03;
-      base04 = prependHash colors.base04;
-      base05 = prependHash colors.base05;
-      base06 = prependHash colors.base06;
-      base07 = prependHash colors.base07;
-      base08 = prependHash colors.base08;
-      base09 = prependHash colors.base09;
-      base0A = prependHash colors.base0A;
-      base0B = prependHash colors.base0B;
-      base0C = prependHash colors.base0C;
-      base0D = prependHash colors.base0D;
-      base0E = prependHash colors.base0E;
-      base0F = prependHash colors.base0F;
+      base00 = colors.base00;
+      base01 = colors.base01;
+      base02 = colors.base02;
+      base03 = colors.base03;
+      base04 = colors.base04;
+      base05 = colors.base05;
+      base06 = colors.base06;
+      base07 = colors.base07;
+      base08 = colors.base08;
+      base09 = colors.base09;
+      base0A = colors.base0A;
+      base0B = colors.base0B;
+      base0C = colors.base0C;
+      base0D = colors.base0D;
+      base0E = colors.base0E;
+      base0F = colors.base0F;
     };
   };
 }
