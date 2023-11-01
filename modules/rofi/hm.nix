c720ab86a6fda539235b1373d394abcde71ea34f
Daniel Thwaites <danthwaites30@btinternet.com>
2023-11-01 14:10:37 +0000

    Tidy up Rofi module :art:

--- modules/rofi/hm.nix
+++ modules/rofi/hm.nix
@@ -1,28 +1,26 @@
 { pkgs, config, lib, ... }:
-with config.stylix.fonts;
-let
-  finalString = ''
-   * { background: rgba ( {{base00-rgb-r}}, {{base00-rgb-g}}, {{base00-rgb-b}}, ${config.lib.stylix.popupsOpacity-int} % );
-       lightbg: rgba ( {{base01-rgb-r}}, {{base01-rgb-g}}, {{base01-rgb-b}}, ${config.lib.stylix.popupsOpacity-int} % );
-       red: rgba ( {{base08-rgb-r}}, {{base08-rgb-g}}, {{base08-rgb-b}}, ${config.lib.stylix.popupsOpacity-int} % );
-       blue: rgba ( {{base0D-rgb-r}}, {{base0D-rgb-g}}, {{base0D-rgb-b}}, ${config.lib.stylix.popupsOpacity-int} % );
-       lightfg: rgba ( {{base06-rgb-r}}, {{base06-rgb-g}}, {{base06-rgb-b}}, ${config.lib.stylix.popupsOpacity-int} % );
-       foreground: rgba ( {{base05-rgb-r}}, {{base05-rgb-g}}, {{base05-rgb-b}}, ${config.lib.stylix.popupsOpacity-int} % );
-   }
-   '' + builtins.toString (builtins.readFile ./template.mustache);
-  finalFile = config.stylix.colors {
-      template = finalString;
-      extension = ".rasi";
-  };
-in
+
 {
   options.stylix.targets.rofi.enable =
     config.lib.stylix.mkEnableTarget "Rofi" true;
 
-  config = lib.mkIf config.stylix.targets.rofi.enable {
-    programs.rofi = {
-      font = "${monospace.name} ${toString sizes.popups}";
-      theme = finalFile;
+  config.programs.rofi = lib.mkIf config.stylix.targets.rofi.enable {
+    theme = config.stylix.colors {
+      template = with config.lib.stylix; ''
+        * {
+          background: rgba ( {{base00-rgb-r}}, {{base00-rgb-g}}, {{base00-rgb-b}}, ${popupsOpacity-int} % );
+          lightbg: rgba ( {{base01-rgb-r}}, {{base01-rgb-g}}, {{base01-rgb-b}}, ${popupsOpacity-int} % );
+          red: rgba ( {{base08-rgb-r}}, {{base08-rgb-g}}, {{base08-rgb-b}}, ${popupsOpacity-int} % );
+          blue: rgba ( {{base0D-rgb-r}}, {{base0D-rgb-g}}, {{base0D-rgb-b}}, ${popupsOpacity-int} % );
+          lightfg: rgba ( {{base06-rgb-r}}, {{base06-rgb-g}}, {{base06-rgb-b}}, ${popupsOpacity-int} % );
+          foreground: rgba ( {{base05-rgb-r}}, {{base05-rgb-g}}, {{base05-rgb-b}}, ${popupsOpacity-int} % );
+        }
+        ${builtins.readFile ./template.mustache}
+      '';
+      extension = ".rasi";
     };
+
+    font = with config.stylix.fonts;
+      "${monospace.name} ${toString sizes.popups}";
   };
 }
