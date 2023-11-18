d1b6ac5aec115cc12df2117705db2950f92576ed
Daniel Thwaites <danthwaites30@btinternet.com>
2023-11-18 23:44:19 +0000

    Fix error with wallpaper symlink for slideshows :ambulance:
    Source cannot be a list of files. For now this just falls back to
    using the first image in the list.

--- stylix/hm/xdg.nix
+++ stylix/hm/xdg.nix
@@ -5,14 +5,19 @@ args:
   imports = [ (import ../palette.nix args) ];
 
   config.xdg.configFile = {
-    "stylix/wallpaper".source = if (config.lib.stylix.types.image.check config.stylix.wallpaper) then config.stylix.wallpaper.image
-    else if (config.lib.stylix.types.animation.check config.stylix.wallpaper) then config.stylix.wallpaper.animation 
-    else if (config.lib.stylix.types.video.check config.stylix.wallpaper) then config.stylix.wallpaper.video 
-    else config.stylix.wallpaper.images;
+    "stylix/wallpaper".source =
+      if config.lib.stylix.types.video.check config.stylix.wallpaper
+      then config.stylix.wallpaper.video
+      else
+        if config.lib.stylix.types.animation.check config.stylix.wallpaper
+        then config.stylix.wallpaper.animation
+        else config.stylix.wallpaper.image;
+
     "stylix/palette.json".source = config.stylix.colors {
       template = ../palette.json.mustache;
       extension = ".json";
     };
+
     "stylix/palette.html".source = config.stylix.colors {
       template = ../palette.html.mustache;
       extension = ".html";
