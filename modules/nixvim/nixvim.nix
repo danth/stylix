8bb621eb27ef67087f84d6e9b93a7649152b06b2
willemml <willem@leit.so>
2023-11-24 16:12:56 -0800

    Fixes nixvim module
    - Enables nixvim's base16 styling instead of just configuring it
    - Adds aliases/imports of nixvim.nix in darwin.nix, hm.nix and
      nixos.nix
    - Fixes stylix causing build errors when nixvim is not installed or
      its module is not imported

--- modules/nixvim/nixvim.nix
+++ modules/nixvim/nixvim.nix
@@ -1,31 +1,35 @@
 {
   config,
   lib,
+  options,
   ...
 }: {
   options.stylix.targets.nixvim.enable =
-    config.lib.stylix.mkEnableTarget "nixvim" true;
+    config.lib.stylix.mkEnableTarget "nixvim" (config.programs ? nixvim);
 
-  config = lib.mkIf config.stylix.targets.nixvim.enable {
-    programs.nixvim.colorschemes.base16.customColorScheme = let
-      colors = config.lib.stylix.colors.withHashtag;
-    in {
-      base00 = colors.base00;
-      base01 = colors.base01;
-      base02 = colors.base02;
-      base03 = colors.base03;
-      base04 = colors.base04;
-      base05 = colors.base05;
-      base06 = colors.base06;
-      base07 = colors.base07;
-      base08 = colors.base08;
-      base09 = colors.base09;
-      base0A = colors.base0A;
-      base0B = colors.base0B;
-      base0C = colors.base0C;
-      base0D = colors.base0D;
-      base0E = colors.base0E;
-      base0F = colors.base0F;
-    };
-  };
+  config = lib.mkIf ((config.programs ? nixvim) && config.stylix.targets.nixvim.enable) (
+    lib.optionalAttrs (builtins.hasAttr "nixvim" options.programs) {
+      programs.nixvim.colorschemes.base16.customColorScheme = let
+        colors = config.lib.stylix.colors.withHashtag;
+      in {
+        base00 = colors.base00;
+        base01 = colors.base01;
+        base02 = colors.base02;
+        base03 = colors.base03;
+        base04 = colors.base04;
+        base05 = colors.base05;
+        base06 = colors.base06;
+        base07 = colors.base07;
+        base08 = colors.base08;
+        base09 = colors.base09;
+        base0A = colors.base0A;
+        base0B = colors.base0B;
+        base0C = colors.base0C;
+        base0D = colors.base0D;
+        base0E = colors.base0E;
+        base0F = colors.base0F;
+      };
+      programs.nixvim.colorschemes.base16.enable = true;
+    }
+  );
 }
