cceb60dfed63e205350fccbb331db25b1bae2177
Daniel Thwaites <danthwaites30@btinternet.com>
2024-01-03 19:36:48 +0000

    Remove unnecessary mkMerge :recycle:

--- modules/bemenu/hm.nix
+++ modules/bemenu/hm.nix
@@ -25,29 +25,27 @@ in {
     };
   };
 
-  config = lib.mkMerge [
-    (lib.mkIf config.stylix.targets.bemenu.enable {
-      programs.bemenu.settings = with config.stylix.targets.bemenu; {
-        tb = "${base01}${bemenuOpacity}"; # Title bg
-        nb = "${base01}${bemenuOpacity}"; # Normal bg
-        fb = "${base01}${bemenuOpacity}"; # Filter bg
-        hb = "${base03}${bemenuOpacity}"; # Highlighted bg
-        sb = "${base03}${bemenuOpacity}"; # Selected bg
-        scb = "${base01}"; # Scrollbar bg
+  config = lib.mkIf config.stylix.targets.bemenu.enable {
+    programs.bemenu.settings = with config.stylix.targets.bemenu; {
+      tb = "${base01}${bemenuOpacity}"; # Title bg
+      nb = "${base01}${bemenuOpacity}"; # Normal bg
+      fb = "${base01}${bemenuOpacity}"; # Filter bg
+      hb = "${base03}${bemenuOpacity}"; # Highlighted bg
+      sb = "${base03}${bemenuOpacity}"; # Selected bg
+      scb = "${base01}"; # Scrollbar bg
 
-        hf = "${base0A}"; # Highlighted fg
-        sf = "${base0B}"; # Selected fg
-        tf = "${base05}"; # Title fg
-        ff = "${base05}"; # Filter fg
-        nf = "${base05}"; # Normal fg
-        scf = "${base03}"; # Scrollbar fg
+      hf = "${base0A}"; # Highlighted fg
+      sf = "${base0B}"; # Selected fg
+      tf = "${base05}"; # Title fg
+      ff = "${base05}"; # Filter fg
+      nf = "${base05}"; # Normal fg
+      scf = "${base03}"; # Scrollbar fg
 
-        ab = "${if alternate then base00 else base01}"; # Alternate bg
-        af = "${if alternate then base04 else base05}"; # Alternate fg
+      ab = "${if alternate then base00 else base01}"; # Alternate bg
+      af = "${if alternate then base04 else base05}"; # Alternate fg
 
-        # Font name
-        fn = "${sansSerif.name} ${lib.optionalString (fontSize != null) (builtins.toString fontSize)}";
-      };
-    })
-  ];
+      # Font name
+      fn = "${sansSerif.name} ${lib.optionalString (fontSize != null) (builtins.toString fontSize)}";
+    };
+  };
 }
