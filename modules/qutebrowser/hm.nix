1e6374b813724718676ae28a02b8043479fa9a1a
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-02-01 13:17:23 +0100

    style(modules/qutebrowser): consistently wrap long lines

--- modules/qutebrowser/hm.nix
+++ modules/qutebrowser/hm.nix
@@ -203,7 +203,8 @@ in {
           isDark = config.stylix.polarity == "dark";
         in {
           darkmode.enabled = lib.mkIf isDark (lib.mkDefault true);
-          preferred_color_scheme = lib.mkIf
+          preferred_color_scheme =
+            lib.mkIf
             isDark (lib.mkDefault config.stylix.polarity);
         };
       };
