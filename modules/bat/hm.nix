32a796929226869542b29c0031848f6dc392a3bd
xokdvium <serjtsimmerman@gmail.com>
2024-03-12 00:41:22 +0300

    yazi: init (#229)

--- modules/bat/hm.nix
+++ modules/bat/hm.nix
@@ -5,6 +5,8 @@
     config.lib.stylix.mkEnableTarget "Bat" config.programs.bat.enable;
 
   config = lib.mkIf config.stylix.targets.bat.enable {
+    # This theme is reused for yazi. Changes to the template 
+    # will need to be applied to modules/yazi/hm.nix
     programs.bat.themes."base16-stylix".src = config.lib.stylix.colors {
       template = ./base16-stylix.mustache;
       extension = ".tmTheme";
