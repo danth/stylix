eb64377e66122de7a36ca7a611aa97ddf4c8e5e8
Andy Bell <andy-bell101@users.noreply.github.com>
2024-03-14 12:47:33 +0000

    lazygit: init (#284)

---
+++ modules/lazygit/hm.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  colors = config.lib.stylix.colors.withHashtag;
+in {
+  options.stylix.targets.lazygit.enable = config.lib.stylix.mkEnableTarget "lazygit" config.programs.lazygit.enable;
+
+  config = lib.mkIf config.stylix.targets.lazygit.enable {
+    programs.lazygit.settings.gui.theme = {
+      activeBorderColor = [
+        colors.base07
+        "bold"
+      ];
+      inactiveBorderColor = [colors.base04];
+      searchingActiveBorderColor = [colors.base02 "bold"];
+      optionsTextColor = [colors.base06];
+      selectedLineBgColor = [colors.base03];
+      cherryPickedCommitBgColor = [colors.base02];
+      cherryPickedCommitFgColor = [colors.base03];
+      unstagedChangesColor = [colors.base08];
+      defaultFgColor = [colors.base05];
+    };
+  };
+}
