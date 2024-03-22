5e1b0fa651dcaa96691a514a976c28548843d749
Daniel Thwaites <danthwaites30@btinternet.com>
2024-03-22 10:19:33 +0000

    nixos: include attribution

--- modules/nixos/nixos.nix
+++ modules/nixos/nixos.nix
@@ -1,5 +1,7 @@
 { pkgs, config, lib, ... }:
 
+with config.lib.stylix.colors;
+
 {
   options.stylix.targets.nixos.enable =
     config.lib.stylix.mkEnableTarget "the NixOS logo" true;
@@ -7,8 +9,8 @@
   config.nixpkgs.overlays = lib.mkIf config.stylix.targets.nixos.enable [(self: super: {
     nixos-icons = super.nixos-icons.overrideAttrs (oldAttrs: {
       patchPhase = ''
-        substituteInPlace logo/white.svg \
-          --replace-fail '#ffffff' '#${config.lib.stylix.colors.base05}'
+        substituteInPlace logo/white.svg --replace-fail '#ffffff' '#${base05}'
+        sed -i '2i<!-- The original NixOS logo from ${oldAttrs.src.url} modified to use colors from ${scheme}. This file is licensed under https://creativecommons.org/licenses/by/4.0 -->' logo/white.svg
         ${lib.getExe pkgs.resvg} logo/white.svg logo/white.png
       '';
     });
