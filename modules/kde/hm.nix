b6dbe9ac5d57d27d5620445f20cad2c353089f86
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-04-01 19:51:31 +0200

    kde: use provided `verboseEcho` function (#316)
    Follows: bad1af63ff330b397b87fc243d479701417740da

--- modules/kde/hm.nix
+++ modules/kde/hm.nix
@@ -255,10 +255,16 @@ in {
 
       if wallpaper_image="$(global_path plasma-apply-wallpaperimage)"; then
         "$wallpaper_image" "${themePackage}/share/wallpapers/stylix"
+      else
+        verboseEcho \
+          "plasma-apply-wallpaperimage: command not found"
       fi
 
       if look_and_feel="$(global_path plasma-apply-lookandfeel)"; then
         "$look_and_feel" --apply stylix
+      else
+        verboseEcho \
+          "Skipping plasma-apply-lookandfeel: command not found"
       fi
     '';
   };
