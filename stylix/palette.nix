406f793045ce82689194273cb7e7c4ad0fec530c
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-04-11 16:58:36 +0200

    stylix: escape spaces in wallpaper path (#318)
    Fixed version of a0bdd9c15b23a5db48d29afe3b238333605c357c (#317)
    which was reverted in f9b9bc7c8e69942cd2583a3309f86fc5260f1275 (#329)
    
    Closes #330
    
    Co-authored-by: Daniel Thwaites <danthwaites30@btinternet.com>

--- stylix/palette.nix
+++ stylix/palette.nix
@@ -10,7 +10,10 @@ let
 
   paletteJSON = let
     generatedJSON = pkgs.runCommand "palette.json" { } ''
-      ${palette-generator}/bin/palette-generator ${cfg.polarity} ${cfg.image} $out
+      ${palette-generator}/bin/palette-generator \
+        "${cfg.polarity}" \
+        ${lib.escapeShellArg "${cfg.image}"} \
+        "$out"
     '';
     palette = importJSON generatedJSON;
     scheme = base16.mkSchemeAttrs palette;
