73c870f08eb1065a6f9cae36c46731f3f84f3666
sodiboo <git@sodi.boo>
2024-04-20 13:53:36 +0200

    nixos-icons: `white.svg` -> `nix-snowflake-white.svg`
    These files were renamed in upstream nixos-artwork and merged into nixpkgs 2 days ago.
    
    - https://github.com/NixOS/nixos-artwork/commit/ce919b818047ac481b8f1bfb9bee377e2b2889e2
    - https://github.com/NixOS/nixpkgs/pull/303068
    
    Additionally, the source root of the package has been moved, so we patch the `src` directly
    rather than using mkDerivation's `patchPhase`

--- modules/nixos-icons/nixos.nix
+++ modules/nixos-icons/nixos.nix
@@ -8,17 +8,20 @@ with config.lib.stylix.colors;
 
   config.nixpkgs.overlays = lib.mkIf config.stylix.targets.nixos-icons.enable [(self: super: {
     nixos-icons = super.nixos-icons.overrideAttrs (oldAttrs: {
-      patchPhase = ''
-        substituteInPlace logo/white.svg --replace-fail '#ffffff' '#${base05}'
+      src = pkgs.applyPatches {
+        src = oldAttrs.src;
+        prePatch = ''
+          substituteInPlace logo/nix-snowflake-white.svg --replace-fail '#ffffff' '#${base05}'
 
-        # Insert attribution comment after the XML prolog
-        sed \
-          --in-place \
-          '2i<!-- The original NixOS logo from ${oldAttrs.src.url} is licensed under https://creativecommons.org/licenses/by/4.0 and has been modified to match the ${scheme} color scheme. -->' \
-          logo/white.svg
+          # Insert attribution comment after the XML prolog
+          sed \
+            --in-place \
+            '2i<!-- The original NixOS logo from ${oldAttrs.src.url} is licensed under https://creativecommons.org/licenses/by/4.0 and has been modified to match the ${scheme} color scheme. -->' \
+            logo/nix-snowflake-white.svg
 
-        ${lib.getExe pkgs.resvg} logo/white.svg logo/white.png
+          ${lib.getExe pkgs.resvg} logo/nix-snowflake-white.svg logo/nix-snowflake-white.png
       '';
+      };
     });
   })];
 }
