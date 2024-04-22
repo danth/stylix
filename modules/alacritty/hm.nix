2f29ecd3e4c0ebf75f46647b4c4fb7261b978aac
Sergei Zimmerman <serjtsimmerman@gmail.com>
2024-04-22 16:17:05 +0300

    alacritty: replace base16-alacritty with a custom theme (#346)
    Replaces mustache template with a theme specified using
    Home Manager options. This removes the need for separate
    templates for TOML and YAML formats.
    
    The new colors better align with other terminal modules (e.g. wezterm),
    and base24 schemes with bright- mnemonics are now supported.
    
    Closes #341.

--- modules/alacritty/hm.nix
+++ modules/alacritty/hm.nix
@@ -1,23 +1,17 @@
-{pkgs, config, lib, ... }:
-
-with config.stylix.fonts;
+# Documentation is available at:
+# - https://alacritty.org/config-alacritty.html
+# - `man 5 alacritty`
+{ config, lib, ... }:
 
 let
-  useYaml = (builtins.compareVersions config.programs.alacritty.package.version "0.13.0") < 0;
-  templateRepo = config.lib.stylix.templates.
-    "base16-alacritty${if useYaml then "-yaml" else ""}";
-
-  themeFile = config.lib.stylix.colors {
-    inherit templateRepo;
-  };
+  colors = config.lib.stylix.colors.withHashtag;
 in
 {
-  options.stylix.targets.alacritty.enable =
-    config.lib.stylix.mkEnableTarget "Alacritty" true;
+  options.stylix.targets.alacritty.enable = config.lib.stylix.mkEnableTarget "Alacritty" true;
 
   config = lib.mkIf config.stylix.targets.alacritty.enable {
     programs.alacritty.settings = {
-      font = {
+      font = with config.stylix.fonts; {
         normal = {
           family = monospace.name;
           style = "Regular";
@@ -25,7 +19,36 @@ in
         size = sizes.terminal;
       };
       window.opacity = with config.stylix.opacity; terminal;
-      import = [ themeFile ];
+      colors = with colors; {
+        primary = {
+          foreground = base05;
+          background = base00;
+          bright_foreground = base07;
+        };
+        selection = {
+          text = base05;
+          background = base02;
+        };
+        cursor = {
+          text = base00;
+          cursor = base05;
+        };
+        normal = {
+          black = base00;
+          white = base05;
+          inherit red green yellow blue magenta cyan;
+        };
+        bright = {
+          black = base03;
+          white = base07;
+          red = bright-red;
+          green = bright-green;
+          yellow = yellow;
+          blue = bright-blue;
+          magenta = bright-magenta;
+          cyan = bright-cyan;
+        };
+      };
     };
   };
 }
