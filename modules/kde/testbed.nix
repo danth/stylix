740f6b2e497986117bdb02b1a68edc1d67691f3a
Torben Schweren <torben@schweren.dev>
2024-05-22 18:46:16 +0200

    kde: Adapt for Plasma 6
    Include Plasma 6 and new attribute names

--- modules/kde/testbed.nix
+++ modules/kde/testbed.nix
@@ -1,7 +1,15 @@
 {
+  # Old attribute names for backwards compatibility
   services.xserver = {
     enable = true;
-    desktopManager.plasma5.enable = true;
+    desktopManager = {
+      plasma5.enable = true;
+      plasma6.enable = true;
+    };
     displayManager.sddm.enable = true;
   };
+
+  # New attribute names since 3rd of 2024-03-12 (https://github.com/NixOS/nixpkgs/commit/b07cdeb1b34503576ec4aba981740466d19cb8e5)
+  # There is no Plasma 5 equivalent for this
+  services.desktopManager.plasma6.enable = true;
 }
