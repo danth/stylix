8f8ea189a0b27e4f80516fb51f2b91dcba9b4ba9
Ed Roberts Rayne <96839304+edrobertsrayne@users.noreply.github.com>
2024-05-22 09:01:44 +0100

    hyprpaper: refactor

--- modules/hyprpaper/hm.nix
+++ modules/hyprpaper/hm.nix
@@ -2,18 +2,14 @@
   config,
   lib,
   ...
-}: let
-  settings = {
-    preload = ["${config.stylix.image}"];
-    wallpaper = [",${config.stylix.image}"];
-  };
-in {
+}: {
   options.stylix.targets.hyprpaper.enable =
     config.lib.stylix.mkEnableTarget "Hyprpaper" true;
 
   config = lib.mkIf config.stylix.targets.hyprland.enable {
-    services.hyprpaper = {
-      inherit settings;
+    services.hyprpaper.settings = {
+      preload = ["${config.stylix.image}"];
+      wallpaper = [",${config.stylix.image}"];
     };
   };
 }
