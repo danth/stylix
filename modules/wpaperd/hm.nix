f35a223c42784e56665ce5a1d3fbf1d3ebb8aece
mham <warofzen1@proton.me>
2024-05-22 19:53:54 +0700

    wpaperd: init
    add initial configuration for wpaperd

---
+++ modules/wpaperd/hm.nix
@@ -0,0 +1,13 @@
+{ config, lib, ... }:
+
+{
+  options.stylix.targets.wpaperd.enable = config.lib.stylix.mkEnableTarget "wpaperd" true;
+
+  config = lib.mkIf config.stylix.targets.wpaperd.enable {
+    programs.wpaperd.settings = {
+      any = {
+        path = "${config.stylix.image}";
+      };
+    };
+  };
+}
