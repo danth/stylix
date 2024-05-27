25d4b9a49d51921b86ec7bd67f527dabea04cea5
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-05-27 09:52:04 +0200

    gnome: use internal 'mkEnableTarget' function
    Follows: 679a80676840185cbc72a3751ccdac169477c21a

--- modules/gnome/nixos.nix
+++ modules/gnome/nixos.nix
@@ -5,12 +5,9 @@ let
 
 in {
   options.stylix.targets.gnome.enable =
-    lib.mkOption {
-      description = "Whether to style GNOME and GDM";
-      type = lib.types.bool;
-      default = config.stylix.autoEnable
-             && config.services.xserver.desktopManager.gnome.enable;
-    };
+    config.lib.stylix.mkEnableTarget
+    "GNOME"
+    config.services.xserver.desktopManager.gnome.enable;
 
   config = lib.mkIf config.stylix.targets.gnome.enable {
     # As Stylix is controlling the wallpaper, there is no need for this
