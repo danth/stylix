3b8653fd0464b00261a84f732b44b81e19af4be3
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-05-28 01:03:18 +0200

    Formatting

--- modules/vim/hm.nix
+++ modules/vim/hm.nix
@@ -28,22 +28,22 @@ let
       colorscheme base16-stylix
       unlet g:colors_name
       let g:colors = {
-	\ 'base00': '${base00}',
-	\ 'base01': '${base01}',
-	\ 'base02': '${base02}',
-	\ 'base03': '${base03}',
-	\ 'base04': '${base04}',
-	\ 'base05': '${base05}',
-	\ 'base06': '${base06}',
-	\ 'base07': '${base07}',
-	\ 'base08': '${base08}',
-	\ 'base09': '${base09}',
-	\ 'base0A': '${base0A}',
-	\ 'base0B': '${base0B}',
-	\ 'base0C': '${base0C}',
-	\ 'base0D': '${base0D}',
-	\ 'base0E': '${base0E}',
-	\ 'base0F': '${base0F}',
+				\ 'base00': '${base00}',
+				\ 'base01': '${base01}',
+				\ 'base02': '${base02}',
+				\ 'base03': '${base03}',
+				\ 'base04': '${base04}',
+				\ 'base05': '${base05}',
+				\ 'base06': '${base06}',
+				\ 'base07': '${base07}',
+				\ 'base08': '${base08}',
+				\ 'base09': '${base09}',
+				\ 'base0A': '${base0A}',
+				\ 'base0B': '${base0B}',
+				\ 'base0C': '${base0C}',
+				\ 'base0D': '${base0D}',
+				\ 'base0E': '${base0E}',
+				\ 'base0F': '${base0F}',
       \ }
       set guifont=${escape [" "] fonts.monospace.name}:h${toString fonts.sizes.terminal}
     '';
