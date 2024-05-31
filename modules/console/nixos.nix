ebaed9d4bf258f4eda7d0690c4092fadcbeefa9d
Geometer1729 <16kuhnb@gmail.com>
2024-05-31 15:41:07 -0400

    console: adjust colors (#404)
    Co-authored-by: Daniel Thwaites <danthwaites30@btinternet.com>

--- modules/console/nixos.nix
+++ modules/console/nixos.nix
@@ -8,20 +8,20 @@ with config.lib.stylix.colors;
 
   config.console.colors = lib.mkIf config.stylix.targets.console.enable [
     base00-hex
-    base08-hex
-    base0B-hex
-    base0A-hex
-    base0D-hex
-    base0E-hex
-    base0C-hex
+    red
+    green
+    yellow
+    blue
+    magenta
+    cyan
     base05-hex
     base03-hex
-    base09-hex
-    base01-hex
-    base02-hex
-    base04-hex
+    red
+    green
+    yellow
+    blue
+    magenta
+    cyan
     base06-hex
-    base0F-hex
-    base07-hex
   ];
 }
