0a4755b656c28043c52063384fc68bbb30913903
Matt Sturgeon <matt@sturgeon.me.uk>
2024-06-05 03:10:32 +0100

    nixvim: expose config as read-only option
    Allow standalone nixvim users to take advantage of stylix by exposing
    the generated config as `config.lib.stylix.nixvim.config`.
    
    This can be passed to the nixvim derivation's `extend` function or used
    directly in a nixvim configuration.

--- modules/nixvim/nixvim.nix
+++ modules/nixvim/nixvim.nix
@@ -6,15 +6,81 @@
 }:
 let
   cfg = config.stylix.targets.nixvim;
+  # Maps `stylix.targets.plugin` values to the appropriate nixvim configuration
+  pluginConfigs = {
+    "base16-nvim" = {
+      inherit highlightOverride;
+
+      colorschemes.base16 = {
+        enable = true;
+
+        colorscheme = {
+          inherit (config.lib.stylix.colors.withHashtag)
+            base00
+            base01
+            base02
+            base03
+            base04
+            base05
+            base06
+            base07
+            base08
+            base09
+            base0A
+            base0B
+            base0C
+            base0D
+            base0E
+            base0F
+            ;
+        };
+      };
+    };
+    "mini.base16" = {
+      inherit highlightOverride;
+
+      plugins.mini = {
+        enable = true;
+
+        modules.base16.palette = {
+          inherit (config.lib.stylix.colors.withHashtag)
+            base00
+            base01
+            base02
+            base03
+            base04
+            base05
+            base06
+            base07
+            base08
+            base09
+            base0A
+            base0B
+            base0C
+            base0D
+            base0E
+            base0F
+            ;
+        };
+      };
+    };
+  };
+  # Transparent is used a few times below
+  transparent = {
+    bg = "none";
+    ctermbg = "none";
+  };
+  highlightOverride = {
+    Normal = lib.mkIf cfg.transparentBackground.main transparent;
+    NonText = lib.mkIf cfg.transparentBackground.main transparent;
+    SignColumn = lib.mkIf cfg.transparentBackground.signColumn transparent;
+  };
 in
 {
   options.stylix.targets.nixvim = {
     enable = config.lib.stylix.mkEnableTarget "nixvim" true;
     plugin = lib.mkOption {
-      type = lib.types.enum [
-        "base16-nvim"
-        "mini.base16"
-      ];
+      type = lib.types.enum (builtins.attrNames pluginConfigs);
       default = "mini.base16";
       description = "Plugin used for the colorscheme";
     };
@@ -63,80 +129,14 @@ in
     })
   ];
 
-  config =
-    lib.mkIf (config.stylix.enable && cfg.enable && options.programs ? nixvim)
-      (
-        lib.optionalAttrs (options.programs ? nixvim) (
-          lib.mkMerge [
-            (lib.mkIf (cfg.plugin == "base16-nvim") {
-              programs.nixvim.colorschemes.base16 = {
-                enable = true;
-
-                colorscheme = {
-                  inherit (config.lib.stylix.colors.withHashtag)
-                    base00
-                    base01
-                    base02
-                    base03
-                    base04
-                    base05
-                    base06
-                    base07
-                    base08
-                    base09
-                    base0A
-                    base0B
-                    base0C
-                    base0D
-                    base0E
-                    base0F
-                    ;
-                };
-              };
-            })
-            (lib.mkIf (cfg.plugin == "mini.base16") {
-              programs.nixvim.plugins.mini = {
-                enable = true;
-
-                modules.base16.palette = {
-                  inherit (config.lib.stylix.colors.withHashtag)
-                    base00
-                    base01
-                    base02
-                    base03
-                    base04
-                    base05
-                    base06
-                    base07
-                    base08
-                    base09
-                    base0A
-                    base0B
-                    base0C
-                    base0D
-                    base0E
-                    base0F
-                    ;
-                };
-              };
-            })
-            {
-              programs.nixvim = {
-                highlightOverride =
-                  let
-                    transparent = {
-                      bg = "none";
-                      ctermbg = "none";
-                    };
-                  in
-                  {
-                    Normal = lib.mkIf cfg.transparentBackground.main transparent;
-                    NonText = lib.mkIf cfg.transparentBackground.main transparent;
-                    SignColumn = lib.mkIf cfg.transparentBackground.signColumn transparent;
-                  };
-              };
-            }
-          ]
-        )
-      );
+  config = lib.mkMerge [
+    {
+      lib.stylix.nixvim.config = pluginConfigs.${cfg.plugin};
+    }
+    (lib.mkIf (config.stylix.enable && cfg.enable && options.programs ? nixvim) (
+      lib.optionalAttrs (options.programs ? nixvim) {
+        programs.nixvim = config.lib.stylix.nixvim.config;
+      }
+    ))
+  ];
 }
