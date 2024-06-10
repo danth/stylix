cbb3583e11c33a20b4909d92f0747839ead49ee2
Daniel Thwaites <danthwaites30@btinternet.com>
2024-06-10 01:10:08 +0100

    gnome: move conditional to `mkIf`

--- modules/gnome/nixos.nix
+++ modules/gnome/nixos.nix
@@ -5,11 +5,13 @@ let
 
 in {
   options.stylix.targets.gnome.enable =
-    config.lib.stylix.mkEnableTarget
-    "GNOME and GDM"
-    config.services.xserver.desktopManager.gnome.enable;
+    config.lib.stylix.mkEnableTarget "GNOME and GDM" true;
 
-  config = lib.mkIf (config.stylix.enable && config.stylix.targets.gnome.enable) {
+  config = lib.mkIf (
+    config.stylix.enable
+    && config.stylix.targets.gnome.enable
+    && config.services.xserver.desktopManager.gnome.enable
+  ) {
     # As Stylix is controlling the wallpaper, there is no need for this
     # pack of default wallpapers to be installed.
     # If you want to use one, you can set stylix.image to something like
