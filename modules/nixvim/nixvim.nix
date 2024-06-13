d6b10df004437ed68eb842b6a4d8cf975d5dea31
Daniel Thwaites <danthwaites30@btinternet.com>
2024-06-13 02:20:01 +0100

    nixvim: remove unnecessary `let`

--- modules/nixvim/nixvim.nix
+++ modules/nixvim/nixvim.nix
@@ -17,10 +17,8 @@
     lib.optionalAttrs (builtins.hasAttr "nixvim" options.programs) {
       programs.nixvim = {
         colorschemes.base16 = {
-          colorscheme = let
-            colors = config.lib.stylix.colors.withHashtag;
-          in {
-            inherit (colors)
+          colorscheme = {
+            inherit (config.lib.stylix.colors.withHashtag)
               base00 base01 base02 base03 base04 base05 base06 base07
               base08 base09 base0A base0B base0C base0D base0E base0F;
           };
