513b62b904b49b6683725751079488a8b91baffe
Mikilio <official.mikilio@gmail.com>
2024-06-25 07:01:15 +0200

    fix: remove warning platformTheme -> platformTheme.name

--- modules/qt/hm.nix
+++ modules/qt/hm.nix
@@ -40,7 +40,7 @@
 
     qt = {
       enable = true;
-      platformTheme = "qtct";
+      platformTheme.name = "qtct";
     };
 
     xdg.configFile."Kvantum/kvantum.kvconfig".source = (pkgs.formats.ini {}).generate "kvantum.kvconfig" {
