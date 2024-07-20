d92b00667e85c2c0b0134a453cc8fd220cc9c50e
Pedro Adorno <pedroadorno99@gmail.com>
2024-07-19 22:21:47 -0300

    ncspot: init

---
+++ modules/ncspot/hm.nix
@@ -0,0 +1,32 @@
+{ config, lib, ... }:
+
+{
+  options.stylix.targets.ncspot.enable =
+    config.lib.stylix.mkEnableTarget "Ncspot" true;
+
+  config = lib.mkIf (config.stylix.enable && config.stylix.targets.ncspot.enable) {
+    programs.ncspot.settings =
+    let 
+      colors = config.lib.stylix.colors.withHashtag;
+    in {
+      theme = with colors; {
+        background = base00;
+        primary = base05;
+        secondary = base03;
+        title = base06;
+        playing = base0A;
+        playing_bg = base00;
+        highlight = base05;
+        highlight_bg = base02;
+        error = base07;
+        error_bg = base0F;
+        statusbar = base00;
+        statusbar_progress = base04;
+        statusbar_bg = base04;
+        cmdline = base05;
+        cmdline_bg = base00;
+        search_match = base05;
+      };
+    };
+  };
+}
