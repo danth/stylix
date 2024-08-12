danth/stylix

commit 3567250ba0497fa457212c72af709554997c280b

Properly warn users that stylix.palette.* has been removed

stylix/palette.nix
@@ -20,6 +20,26 @@ let
   };
 
 in {
+  # TODO link to doc on how to do instead
+  imports = [
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base00" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base01" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base02" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base03" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base04" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base05" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base06" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base07" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base08" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base09" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base0A" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base0B" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base0C" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base0D" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base0E" ] "Using stylix.palette to override scheme is not supported anymore")
+    (lib.mkRemovedOptionModule [ "stylix" "palette" "base0F" ] "Using stylix.palette to override scheme is not supported anymore")
+  ];
+
   options.stylix = {
     polarity = mkOption {
       type = types.enum [ "either" "light" "dark" ];
@@ -61,8 +81,6 @@ in {
         default = generatedPalette;
       };
     };
-
-    # TODO proper removal of palette
 
     base16Scheme = mkOption {
       description = ''

commit 81b6f01d2e82ef7a8105ace8597cb7237bb50c2d

treewide: add soft deprecation dates
Add soft deprecation dates to guide the hard deprecation transitions.

Fixes: 3567250ba049 ("Properly warn users that stylix.palette.* has been removed")
Fixes: 94aa0fc0fbe9 ("nixvim: rename transparency options to camelCase (#497)")

modules/nixvim/nixvim.nix
@@ -14,9 +14,12 @@
   };
 
   imports = [
+    # Added: 2024-08-06
     (lib.mkRenamedOptionModule
       [ "stylix" "targets" "nixvim" "transparent_bg" "main" ]
       [ "stylix" "targets" "nixvim" "transparentBackground" "main" ])
+
+    # Added: 2024-08-06
     (lib.mkRenamedOptionModule
       [ "stylix" "targets" "nixvim" "transparent_bg" "sign_column" ]
       [ "stylix" "targets" "nixvim" "transparentBackground" "signColumn" ])

stylix/palette.nix
@@ -23,21 +23,52 @@ let
 in {
   # TODO link to doc on how to do instead
   imports = [
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base00" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base01" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base02" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base03" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base04" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base05" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base06" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base07" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base08" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base09" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base0A" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base0B" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base0C" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base0D" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base0E" ] "Using stylix.palette to override scheme is not supported anymore")
+
+    # Added: 2023-02-02
     (lib.mkRemovedOptionModule [ "stylix" "palette" "base0F" ] "Using stylix.palette to override scheme is not supported anymore")
   ];
 