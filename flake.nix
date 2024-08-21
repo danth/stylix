0c801a159a54fea5392da120d5df9eeb0093d902
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-08-21 17:33:52 +0200

    stylix: integrate pre-commit hooks into developer shell
    Link: https://github.com/danth/stylix/pull/519

--- flake.nix
+++ flake.nix
@@ -106,7 +106,12 @@
         };
 
         devShells.default = pkgs.mkShell {
-          packages = [ inputs.home-manager.packages.${system}.default ];
+          inherit (inputs.self.checks.${system}.git-hooks) shellHook;
+
+          packages = with inputs; [
+            home-manager.packages.${system}.default
+            self.checks.${system}.git-hooks.enabledPackages
+          ];
         };
 
         packages = let
