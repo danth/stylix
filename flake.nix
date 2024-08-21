c83cefd8fb348a40fbe8ae79da460dbb403fa740
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-08-21 16:56:50 +0200

    stylix: add deadnix and statix pre-commit hooks
    Add the deadnix and statix pre-commit hooks used in the Lint GitHub
    workflow.
    
    Link: https://github.com/danth/stylix/pull/519

--- flake.nix
+++ flake.nix
@@ -42,6 +42,16 @@
       url = "github:numtide/flake-utils";
     };
 
+    git-hooks = {
+      inputs = {
+        flake-compat.follows = "flake-compat";
+        nixpkgs-stable.follows = "git-hooks/nixpkgs";
+        nixpkgs.follows = "nixpkgs";
+      };
+
+      url = "github:cachix/git-hooks.nix";
+    };
+
     gnome-shell = {
       flake = false;
 
@@ -70,6 +80,15 @@
         inherit (nixpkgs) lib;
         pkgs = nixpkgs.legacyPackages.${system};
       in {
+        checks.git-hooks = inputs.git-hooks.lib.${system}.run {
+          hooks = {
+            deadnix.enable = true;
+            statix.enable = true;
+          };
+
+          src = ./.;
+        };
+
         devShells.default = pkgs.mkShell {
           packages = [ inputs.home-manager.packages.${system}.default ];
         };
