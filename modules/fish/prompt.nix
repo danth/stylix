danth/stylix

commit 94d70292d0c687ebacb65d00bd516cbefa18d3ca

fish: fix base16-fish causing startup issues with tmux (#503)
* fish: fix base16-fish causing startup issues with tmux

Closes #488

Co-authored-by: Daniel Thwaites <danthwaites30@btinternet.com>

modules/fish/prompt.nix
@@ -6,5 +6,9 @@ let
   };
 in ''
   source ${theme}
-  base16-${config.lib.stylix.colors.slug}
+
+  # See https://github.com/tomyun/base16-fish/issues/7 for why this condition exists
+  if test -n "$base16_theme" && status --is-interactive && test -z "$TMUX"
+    base16-${config.lib.stylix.colors.slug}
+  end
 ''

commit 3a4101c4f4abee41859c0cb98f6250f04c80d0f6

fish: remove obsolete $base16_theme check
Remove the obsolete $base16_theme check which was incorrectly taken from
[1] as $base16_theme is unused in Stylix.

[1]: https://github.com/tomyun/base16-fish/issues/7#issuecomment-963376055

Closes: https://github.com/danth/stylix/issues/538
Fixes: 94d70292d0c6 ("fish: fix base16-fish causing startup issues with tmux (#503)")
Link: https://github.com/danth/stylix/pull/539

Tested-by: Donovan Glover <donovan@dglover.co>
Approved-by: Donovan Glover <donovan@dglover.co>
Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

modules/fish/prompt.nix
@@ -8,7 +8,7 @@ in ''
   source ${theme}
 
   # See https://github.com/tomyun/base16-fish/issues/7 for why this condition exists
-  if test -n "$base16_theme" && status --is-interactive && test -z "$TMUX"
+  if status --is-interactive && test -z "$TMUX"
     base16-${config.lib.stylix.colors.slug}
   end
 ''