dd9a0c4c3fda84c7b18a49e900aef493429dcd29
Jackaed <ed.andy.jackson@gmail.com>
2024-09-04 18:15:41 +0200

    Undo whatever tf that was

--- modules/neovim/hm.nix
+++ modules/neovim/hm.nix
@@ -2,13 +2,9 @@
 
 {
   options.stylix.targets.neovim = {
-    colorscheme = lib.mkOption {
-      type = lib.types.enum [ "base16-nvim" "mini.base16" ];
-      default = "mini.base16";
-      description = "Strategy for applying the colorscheme";
-    };
     enable =
       config.lib.stylix.mkEnableTarget "Neovim" true;
+
     transparentBackground = {
       main = lib.mkEnableOption "background transparency for the main Neovim window";
       signColumn = lib.mkEnableOption "background transparency for the Neovim sign column";
@@ -25,22 +21,12 @@
           plugin = pkgs.vimPlugins.base16-nvim;
           type = "lua";
           config = lib.mkMerge [
-            (with config.lib.stylix.colors.withHashtag; lib.mkIf (cfg.colorscheme == "base16-nvim") ''
+            (with config.lib.stylix.colors.withHashtag; ''
               require('base16-colorscheme').setup({
                 base00 = '${base00}', base01 = '${base01}', base02 = '${base02}', base03 = '${base03}',
                 base04 = '${base04}', base05 = '${base05}', base06 = '${base06}', base07 = '${base07}',
                 base08 = '${base08}', base09 = '${base09}', base0A = '${base0A}', base0B = '${base0B}',
                 base0C = '${base0C}', base0D = '${base0D}', base0E = '${base0E}', base0F = '${base0F}'
-              })
-            '')
-            (with config.lib.stylix.colors.withHashtag; lib.mkIf (cfg.colorscheme == "mini.base16") ''
-              require('mini.base16').setup({
-                palette = {
-                  base00 = '${base00}', base01 = '${base01}', base02 = '${base02}', base03 = '${base03}',
-                  base04 = '${base04}', base05 = '${base05}', base06 = '${base06}', base07 = '${base07}',
-                  base08 = '${base08}', base09 = '${base09}', base0A = '${base0A}', base0B = '${base0B}',
-                  base0C = '${base0C}', base0D = '${base0D}', base0E = '${base0E}', base0F = '${base0F}'
-                }
               })
             '')
             (lib.mkIf cfg.transparentBackground.main ''
