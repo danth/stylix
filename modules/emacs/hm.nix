danth/stylix

commit 35233f929629c8eb64e939e35260fc8347f94df9

emacs: explicitly set font size (#553)
The emacs module currently doesn't set the font size, only the colours
and font family. This PR sets it to `sizes.terminal` (taking a lead from
the `guifont` setting in the `vim` module) using `font-spec`, which is a
more flexible way to specify fonts in elisp. To indicate the size is
points it needs to be a floating point value.

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

modules/emacs/hm.nix
@@ -5,6 +5,7 @@ with config.stylix.fonts;
 
 let
   emacsOpacity = builtins.toString (builtins.ceil (config.stylix.opacity.applications * 100));
+  emacsSize = builtins.toString (sizes.terminal * 1.0);
 in
 {
   options.stylix.targets.emacs.enable =
@@ -65,7 +66,7 @@ in
         (setq base16-theme-256-color-source 'colors)
         (load-theme 'base16-stylix t)
         ;; Set font
-        (set-face-attribute 'default t :font "${monospace.name}" )
+        (set-face-attribute 'default t :font (font-spec :family "${monospace.name}" :size "${emacsSize}"))
         ;; -----------------------------
         ;; set opacity
         (add-to-list 'default-frame-alist '(alpha-background . ${emacsOpacity}))

commit 149b313ddf91c3cc94309170498b162cec666675

emacs: fix spurious quotes around :size arg (#555)
If the size is expressed as a string, Emacs calls bloody murder:

    invalid font property (:size . "14.000000")

Fixes: 35233f929629 ("emacs: explicitly set font size (#553)")

Reviewed-by: Jez Cope <jezcope@users.noreply.github.com>
Approved-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

modules/emacs/hm.nix
@@ -66,7 +66,7 @@ in
         (setq base16-theme-256-color-source 'colors)
         (load-theme 'base16-stylix t)
         ;; Set font
-        (set-face-attribute 'default t :font (font-spec :family "${monospace.name}" :size "${emacsSize}"))
+        (set-face-attribute 'default t :font (font-spec :family "${monospace.name}" :size ${emacsSize}))
         ;; -----------------------------
         ;; set opacity
         (add-to-list 'default-frame-alist '(alpha-background . ${emacsOpacity}))