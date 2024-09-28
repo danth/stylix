danth/stylix

commit 952ba1c7567c97b984368c1b92d1e01b6e092465

Add support for Fuzzel :sparkles:

modules/fuzzel/hm.nix
@@ -1,0 +1,24 @@
+{ pkgs, config, lib, ... }:
+
+with config.lib.stylix.colors;
+
+let
+  opacity = lib.toHexString (builtins.ceil (config.stylix.opacity.popups * 255));
+
+in {
+  options.stylix.targets.fuzzel.enable =
+    config.lib.stylix.mkEnableTarget "Fuzzel" config.programs.fuzzel.enable;
+
+  config.programs.fuzzel.settings =
+    lib.mkIf config.stylix.targets.fuzzel.enable {
+      colors = {
+        background = "${base00-hex}${opacity}";
+        text = "${base05-hex}ff";
+        match = "${base0A-hex}ff";
+        selection = "${base03-hex}ff";
+        selection-text = "${base05-hex}ff";
+        selection-match = "${base0A-hex}ff";
+        border = "${base0D-hex}ff";
+      };
+    };
+}

commit 1e9ec16a3739f275ec771434c2ad8cff9a54c42e

fuzzel: add missing colors (#578)
Fixes: 952ba1c7567c ("Add support for Fuzzel :sparkles:")

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

modules/fuzzel/hm.nix
@@ -14,10 +14,14 @@ in {
       colors = {
         background = "${base00-hex}${opacity}";
         text = "${base05-hex}ff";
+        placeholder = "${base03-hex}ff";
+        prompt = "${base05-hex}ff";
+        input = "${base05-hex}ff";
         match = "${base0A-hex}ff";
         selection = "${base03-hex}ff";
         selection-text = "${base05-hex}ff";
         selection-match = "${base0A-hex}ff";
+        counter = "${base06-hex}ff";
         border = "${base0D-hex}ff";
       };
 