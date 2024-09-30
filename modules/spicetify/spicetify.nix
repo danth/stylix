danth/stylix

commit 0eea8bcb0f9c3c7638e7ee64f98ed9b4ec716830

spicetify: init (#574)
Creates a theme for Spotify, when used in combination with
https://github.com/Gerg-L/spicetify-nix

modules/spicetify/spicetify.nix
@@ -1,0 +1,43 @@
+{ config, options, lib, pkgs, ... }:
+
+{
+  options.stylix.targets.spicetify.enable =
+    config.lib.stylix.mkEnableTarget "Spicetify" true;
+
+  config = lib.mkIf (config.stylix.enable && config.stylix.targets.spicetify.enable && (config.programs?spicetify)) (
+    lib.optionalAttrs (builtins.hasAttr "spicetify" options.programs) {
+      programs.spicetify = {
+        theme = {
+          name = "stylix";
+          src = pkgs.writeTextFile {
+            name = "color.ini";
+            destination = "/color.ini";
+            text = with config.lib.stylix.colors; ''
+              [base]
+              text               = ${base05}
+              subtext            = ${base05}
+              main               = ${base00}
+              main-elevated      = ${base02}
+              highlight          = ${base02}
+              highlight-elevated = ${base03}
+              sidebar            = ${base01}
+              player             = ${base05}
+              card               = ${base04}
+              shadow             = ${base00}
+              selected-row       = ${base05}
+              button             = ${base05}
+              button-active      = ${base05}
+              button-disabled    = ${base04}
+              tab-active         = ${base02}
+              notification       = ${base02}
+              notification-error = ${base08}
+              equalizer          = ${base0B}
+              misc               = ${base02}
+            '';
+          };
+        };
+        colorScheme = "base";
+      };
+    }
+  );
+}

modules/spicetify/hm.nix
@@ -1,0 +1,1 @@
+import ./spicetify.nix

modules/spicetify/nixos.nix
@@ -1,0 +1,1 @@
+import ./spicetify.nix

commit e7e97059776da7e34b739415a7bc8f80f606b803

spicetify: disable sidebar to resolve incompatiblity notification (#579)
Disable sidebar configuration to resolve an incompatibility notification
on startup.

Fixes: 0eea8bcb0f9c ("spicetify: init (#574)")

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

modules/spicetify/spicetify.nix
@@ -35,6 +35,8 @@
               misc               = ${base02}
             '';
           };
+          # Sidebar configuration is incompatible with the default navigation bar
+          sidebarConfig = false;
         };
         colorScheme = "base";
       };