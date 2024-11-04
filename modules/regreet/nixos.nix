04ed32d0d2346730627d46969e424d735cebb79e
laycookie <lodennis0@gmail.com>
2024-11-04 12:21:14 -0800

    24.05 compatability2

--- modules/regreet/nixos.nix
+++ modules/regreet/nixos.nix
@@ -6,28 +6,28 @@
 }:
 
 {
-  options.stylix.targets.regreet.enable = config.lib.stylix.mkEnableTarget "ReGreet" true;
+  # options.stylix.targets.regreet.enable = config.lib.stylix.mkEnableTarget "ReGreet" true;
 
-  config =
-    lib.mkIf
-      (config.stylix.enable && config.stylix.targets.regreet.enable && pkgs.stdenv.hostPlatform.isLinux)
-      {
-        # programs.regreet = {
-        #   settings.background.path = config.stylix.image;
-        #   font = {
-        #     inherit (config.stylix.fonts.sansSerif) name package;
-        #   };
-        #   cursorTheme = {
-        #     inherit (config.stylix.cursor) name package;
-        #   };
-        #   theme = {
-        #     package = pkgs.adw-gtk3;
-        #     name = "adw-gtk3";
-        #   };
-        #   extraCss = config.lib.stylix.colors {
-        #     template = ./../gtk/gtk.mustache;
-        #     extension = "css";
-        #   };
-        # };
-      };
+  # config =
+  #   lib.mkIf
+  #     (config.stylix.enable && config.stylix.targets.regreet.enable && pkgs.stdenv.hostPlatform.isLinux)
+  #     {
+  #       programs.regreet = {
+  #         settings.background.path = config.stylix.image;
+  #         font = {
+  #           inherit (config.stylix.fonts.sansSerif) name package;
+  #         };
+  #         cursorTheme = {
+  #           inherit (config.stylix.cursor) name package;
+  #         };
+  #         theme = {
+  #           package = pkgs.adw-gtk3;
+  #           name = "adw-gtk3";
+  #         };
+  #         extraCss = config.lib.stylix.colors {
+  #           template = ./../gtk/gtk.mustache;
+  #           extension = "css";
+  #         };
+  #       };
+  #     };
 }
