25aa79930cc06a0a504ac0242adffcf901785032
Daniel Thwaites <danth@danth.me>
2024-11-08 18:14:14 +0000

    chromium: add testbed
    (cherry picked from commit 8eb2da8d502d66e4660049254fec82e3e3e14c19)

---
+++ modules/chromium/testbed.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+
+let package = pkgs.chromium;
+
+in {
+  stylix.testbed.application = {
+    enable = true;
+    name = "chromium-browser";
+    inherit package;
+  };
+
+  environment.systemPackages = [ package ];
+}
