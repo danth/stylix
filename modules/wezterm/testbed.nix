f3463f5f96dc6643568824eb5e78bb19e5f6eff0
Daniel Thwaites <danth@danth.me>
2024-11-08 20:56:12 +0000

    wezterm: add testbed
    (cherry picked from commit 6741c2ef6cb881acaf6904c6dc1a6a5cd181994f)

---
+++ modules/wezterm/testbed.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+
+let package = pkgs.wezterm;
+
+in {
+  stylix.testbed.application = {
+    enable = true;
+    name = "org.wezfurlong.wezterm";
+    inherit package;
+  };
+
+  home-manager.sharedModules = [{
+    programs.wezterm = {
+      enable = true;
+      inherit package;
+    };
+  }];
+}
