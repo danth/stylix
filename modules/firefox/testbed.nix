6c67f75101679ce3c7d415995e090c61944e3bed
Daniel Thwaites <danth@danth.me>
2024-11-08 18:00:44 +0000

    firefox: add testbed

---
+++ modules/firefox/testbed.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+let package = pkgs.firefox;
+
+in {
+  stylix.testbed.application = {
+    enable = true;
+    name = "firefox";
+    inherit package;
+  };
+
+  home-manager.sharedModules = [{
+    programs.firefox = {
+      enable = true;
+      inherit package;
+      profiles.stylix.isDefault = true;
+    };
+
+    stylix.targets.firefox.profileNames = [ "stylix" ];
+  }];
+}
