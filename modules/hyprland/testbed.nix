d2a4bc5ad9add22bf947c8759ca734e2586830e4
Daniel Thwaites <danth@danth.me>
2024-11-08 16:51:43 +0000

    hyprland: add testbed

---
+++ modules/hyprland/testbed.nix
@@ -0,0 +1,14 @@
+{ lib, pkgs, ... }:
+
+{
+  programs.hyprland.enable = true;
+
+  home-manager.sharedModules = [{
+    wayland.windowManager.hyprland = {
+      enable = true;
+
+      # We need something to open a window so that we can check the window borders
+      settings.bind = [ "ALT, RETURN, exec, ${lib.getExe pkgs.foot}" ];
+    };
+  }];
+}
