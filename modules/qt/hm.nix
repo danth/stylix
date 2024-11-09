5ea12b9d3865ae47e71df66d4fef35f6e1b1ee5d
user <bluskript@gmail.com>
2024-11-08 16:24:21 -0800

    fix qt5ct package rename

--- modules/qt/hm.nix
+++ modules/qt/hm.nix
@@ -31,7 +31,7 @@
     '';
   in {
     home.packages = with pkgs; [
-      qt5ct
+      libsForQt5.qt5ct
       libsForQt5.qtstyleplugin-kvantum
       qt6Packages.qtstyleplugin-kvantum
       kvantumPackage
