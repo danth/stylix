131d2a01a42d4102ac10d810a9d39fa66065c040
Kamron Bhavnagri <kamwithk@tuta.io>
2024-11-30 08:26:31 +1100

    river: simplified configuration

--- modules/river/hm.nix
+++ modules/river/hm.nix
@@ -1,18 +1,14 @@
 { config, lib, ... }:
 
-with config.stylix;
-with config.lib.stylix.colors;
 {
   options.stylix.targets.river.enable = config.lib.stylix.mkEnableTarget "River" true;
 
-  config = lib.mkMerge [
-    (lib.mkIf (config.stylix.enable && config.stylix.targets.river.enable) {
-      wayland.windowManager.river.settings = {
-        border-color-focused = "0x${base0D}";
-        border-color-unfocused = "0x${base03}";
-        border-color-urgent = "0x${base08}";
-        xcursor-theme = cursor.name;
-      };
-    })
-  ];
+  config = lib.mkIf (config.stylix.enable && config.stylix.targets.river.enable) {
+    wayland.windowManager.river.settings = {
+      border-color-focused = "0x${config.lib.stylix.colors.base0D}";
+      border-color-unfocused = "0x${config.lib.stylix.colors.base03}";
+      border-color-urgent = "0x${config.lib.stylix.colors.base08}";
+      xcursor-theme = config.stylix.cursor.name;
+    };
+  };
 }
