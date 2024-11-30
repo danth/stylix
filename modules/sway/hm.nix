danth/stylix

commit 07795247c2db08711bbd9503e01752c315be0805

Add cursor support (#172)
Co-authored-by: Daniel Thwaites <danthwaites30@btinternet.com>

modules/sway/hm.nix
@@ -55,6 +55,10 @@ in {
         };
 
         output."*".bg = "${config.stylix.image} fill";
+
+        seat."*" = {
+          xcursor_theme = "${config.stylix.cursor.name} ${toString config.stylix.cursor.size}";
+        };
       };
     })
 

stylix/cursor.nix
@@ -1,0 +1,26 @@
+{ pkgs, config, lib, ... } @ args:
+
+with lib;
+
+let
+  cfg = config.stylix.cursor;
+  fromOs = import ./fromos.nix { inherit lib args; };
+in {
+    options.stylix.cursor = {
+        name = mkOption {
+            description = mdDoc "The cursor name within the package.";
+            type = types.str;
+            default = fromOs [ "cursor" "name" ] "Vanilla-DMZ";
+        };
+        package = mkOption {
+            description = mdDoc "Package providing the cursor theme.";
+            type = types.package;
+            default = fromOs [ "cursor" "package" ] pkgs.vanilla-dmz;
+        };
+        size = mkOption {
+            description = mdDoc "The cursor size.";
+            type = types.int;
+            default = fromOs [ "cursor" "size" ] 32;
+        };
+    };
+}

stylix/hm/cursor.nix
@@ -1,0 +1,22 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.stylix.cursor;
+
+in {
+  imports = [ ../cursor.nix ];
+
+  config = mkIf pkgs.stdenv.hostPlatform.isLinux {
+    home.pointerCursor = {
+      name = cfg.name;
+      package = cfg.package;
+      size = cfg.size;
+      x11 = {
+        enable = true;
+        defaultCursor = cfg.name;
+      };
+      gtk.enable = true;
+    };
+  };
+}

stylix/hm/default.nix
@@ -9,6 +9,7 @@ in {
     ../pixel.nix
     ../target.nix
     ../opacity.nix
+    ./cursor.nix
     ./fonts.nix
     (import ./palette.nix { inherit palette-generator base16; })
     (import ../templates.nix inputs)

stylix/nixos/cursor.nix
@@ -1,0 +1,10 @@
+{ config, ... }:
+
+let
+  cfg = config.stylix.cursor;
+in {
+  imports = [ ../cursor.nix ];
+  config = {
+    environment.variables.XCURSOR_SIZE = toString cfg.size;
+  };
+}

stylix/nixos/default.nix
@@ -10,6 +10,7 @@ in {
     ../pixel.nix
     ../target.nix
     ../opacity.nix
+    ./cursor.nix
     ./fonts.nix
     (import ./palette.nix { inherit palette-generator base16; })
     (import ../templates.nix inputs)

commit e1403a12d5c41a14f55905b7f2663b8c1837ccfb

sway: handle cursor names containing spaces
Closes: https://github.com/danth/stylix/issues/648
Fixes: 07795247c2db ("Add cursor support (#172)")
Link: https://github.com/danth/stylix/pull/654

modules/sway/hm.nix
@@ -55,10 +55,7 @@ in {
         };
 
         output."*".bg = "${config.stylix.image} ${config.stylix.imageScalingMode}";
-
-        seat."*" = {
-          xcursor_theme = "${config.stylix.cursor.name} ${toString config.stylix.cursor.size}";
-        };
+        seat."*".xcursor_theme = ''"${config.stylix.cursor.name}" ${toString config.stylix.cursor.size}'';
       };
     })
 