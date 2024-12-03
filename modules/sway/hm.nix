111c75d73439fe4bea2cdfbeb60aeeb6ea770220
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-12-03 16:40:44 +0100

    sway: handle cursor names containing spaces (#654)
    Closes: https://github.com/danth/stylix/issues/648
    Fixes: 07795247c2db ("Add cursor support (#172)")
    Link: https://github.com/danth/stylix/pull/654

--- modules/sway/hm.nix
+++ modules/sway/hm.nix
@@ -55,10 +55,7 @@ in {
         };
 
         output."*".bg = "${config.stylix.image} ${config.stylix.imageScalingMode}";
-
-        seat."*" = {
-          xcursor_theme = "${config.stylix.cursor.name} ${toString config.stylix.cursor.size}";
-        };
+        seat."*".xcursor_theme = ''"${config.stylix.cursor.name}" ${toString config.stylix.cursor.size}'';
       };
     })
 
