b972fd79a0d71f49b19591e1667b5823d195054a
Devin Droddy <devin.droddy@gmail.com>
2024-12-12 14:13:05 -0500

    wpaperd: use `stylix.imageScalingMode`

--- modules/wpaperd/hm.nix
+++ modules/wpaperd/hm.nix
@@ -4,6 +4,14 @@
   options.stylix.targets.wpaperd.enable = config.lib.stylix.mkEnableTarget "wpaperd" true;
 
   config = lib.mkIf (config.stylix.enable && config.stylix.targets.wpaperd.enable) {
-    programs.wpaperd.settings.any.path = "${config.stylix.image}";
+    programs.wpaperd.settings.any = {
+      path = "${config.stylix.image}";
+      mode = let
+        inherit (config.stylix) imageScalingMode;
+      in
+        if imageScalingMode == "fill"
+        then "fit"
+        else imageScalingMode;
+    };
   };
 }
