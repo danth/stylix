f9c1094a57ddd6a54c12b5f08d92352a979cf6ae
awwpotato <awwpotato@voidq.com>
2024-12-12 16:57:48 -0800

    librewolf: init

---
+++ modules/librewolf/hm.nix
@@ -0,0 +1,29 @@
+{ config, lib, ... }:
+
+let
+  profileSettings = {
+    settings = {
+      "font.name.monospace.x-western" = config.stylix.fonts.monospace.name;
+      "font.name.sans-serif.x-western" = config.stylix.fonts.sansSerif.name;
+      "font.name.serif.x-western" = config.stylix.fonts.serif.name;
+    };
+  };
+  makeProfileSettingsPair = profileName:
+    lib.nameValuePair profileName profileSettings;
+in {
+  options.stylix.targets.librewolf = {
+    enable =
+      config.lib.stylix.mkEnableTarget "Librewolf" true;
+
+    profileNames = lib.mkOption {
+      description = "The Librewolf profile names to apply styling on.";
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+    };
+  };
+
+  config = lib.mkIf (config.stylix.enable && config.stylix.targets.librewolf.enable) {
+    programs.librewolf.profiles = lib.listToAttrs
+      (map makeProfileSettingsPair config.stylix.targets.librewolf.profileNames);
+  };
+}
