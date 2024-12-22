96266cccfa73296bc1dcb01a48d0a0338338a416
bricked <git@bricked.dev>
2024-12-23 00:31:17 +0100

    gtk: support nix-flatpak overrides for flatpak theming

--- modules/gtk/hm.nix
+++ modules/gtk/hm.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, lib, ... }:
+{ pkgs, config, lib, options, ... }:
 
 let
   cfg = config.stylix.targets.gtk;
@@ -54,18 +54,8 @@ in {
       };
     })
 
+    # Flatpak apps apparently don't consume the CSS config. This workaround appends it to the theme directly.
     (lib.mkIf (cfg.enable && cfg.flatpakSupport.enable) {
-      # Let Flatpak apps read the theme and force them to use it.
-      # This is likely incompatible with other modules that write to this file.
-      xdg.dataFile."flatpak/overrides/global".text = ''
-        [Context]
-        filesystems=${config.home.homeDirectory}/.themes/${config.gtk.theme.name}:ro
-
-        [Environment]
-        GTK_THEME=${config.gtk.theme.name}
-      '';
-
-      # Flatpak apps apparently don't consume the CSS config. This workaround appends it to the theme directly.
       home.file.".themes/${config.gtk.theme.name}".source = pkgs.stdenv.mkDerivation {
         name = "flattenedGtkTheme";
         src = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}";
@@ -80,5 +70,27 @@ in {
         '';
       };
     })
+
+    # Let Flatpak apps read the theme and force them to use it.
+    (lib.mkIf (cfg.enable && cfg.flatpakSupport.enable) (let
+        filesystem = "${config.home.homeDirectory}/.themes/${config.gtk.theme.name}:ro";
+        theme = config.gtk.theme.name;
+      in if lib.hasAttrByPath ["services" "flatpak" "overrides"] options then {
+        # This requires nix-flatpak to be imported externally.
+        services.flatpak.overrides.global = {
+          Context.filesystems = [filesystem];
+          Environment.GTK_THEME = theme;
+        };
+      } else {
+        # This is likely incompatible with other modules that write to this file.
+        xdg.dataFile."flatpak/overrides/global".text = ''
+          [Context]
+          filesystems=${filesystem}
+
+          [Environment]
+          GTK_THEME=${theme}
+        '';
+      }
+    ))
   ];
 }
