883252231b02effad7d5761ad8938bc9de7ede57
bricked <git@bricked.dev>
2024-12-26 22:27:14 +0000

    steam: enable adwaita for steam theme by default
    Co-authored-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

--- modules/steam/hm.nix
+++ modules/steam/hm.nix
@@ -5,7 +5,7 @@ let
 in {
   options.stylix.targets.steam = {
     enable = config.lib.stylix.mkEnableTarget "Steam" true;
-    adwaitaForSteam.enable = config.lib.stylix.mkEnableTarget "Adwaita for Steam" false;
+    adwaitaForSteam.enable = config.lib.stylix.mkEnableTarget "Adwaita for Steam" true;
   };
 
   config = lib.mkIf (config.stylix.enable && cfg.enable) (lib.mkMerge [
