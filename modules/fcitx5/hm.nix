e3242f17d58b2f1744f68c06f59b149753aa3dd3
Louis Dalibard <ontake@ontake.dev>
2025-01-03 18:03:24 +0100

    fcitx5: fix highlights

--- modules/fcitx5/hm.nix
+++ modules/fcitx5/hm.nix
@@ -3,9 +3,12 @@
   lib,
   ...
 }: let
-  # Adapted from https://github.com/sanweiya/fcitx5-mellow-themes under the BSD 2 license (compatible with this project's license (MIT))
   theme = config.lib.stylix.colors {
     template = ./theme.conf.mustache;
+    extension = "conf";
+  };
+  theme-kwin = config.lib.stylix.colors {
+    template = ./theme-kwin.conf.mustache;
     extension = "conf";
   };
   highlight = config.lib.stylix.colors {
@@ -18,16 +21,22 @@
   };
 in {
   options.stylix.targets.fcitx5.enable =
-    config.lib.stylix.mkEnableTarget "fcitx5" true;
+    config.lib.stylix.mkEnableTarget
+    "fcitx5"
+    true;
 
   config =
-    lib.mkIf
-    (config.stylix.enable && config.stylix.targets.fcitx5.enable)
-    {
-      xdg.dataFile = {
-        "fcitx5/themes/stylix/highlight.svg".source = highlight;
-        "fcitx5/themes/stylix/panel.svg".source = panel;
-        "fcitx5/themes/stylix/theme.conf".source = theme;
-      };
+    lib.mkIf (
+      config.stylix.enable
+      && config.stylix.targets.fcitx5.enable
+      #&& config.i18n.inputMethod.enable
+      #&& (config.i18n.inputMethod.type == "fcitx5")
+    ) {
+      xdg.dataFile."fcitx5/themes/stylix/theme.conf".source = theme;
+      xdg.dataFile."fcitx5/themes/stylix/panel.svg".source = panel;
+      xdg.dataFile."fcitx5/themes/stylix/highlight.svg".source = highlight;
+      xdg.dataFile."fcitx5/themes/stylix-kwin/theme.conf".source = theme-kwin;
+      xdg.dataFile."fcitx5/themes/stylix-kwin/panel.svg".source = panel;
+      xdg.dataFile."fcitx5/themes/stylix-kwin/highlight.svg".source = highlight;
     };
 }
