6424e118cca8354ca4e538945cdb9cf7cb3952cd
Louis Dalibard <ontake@ontake.dev>
2025-01-04 15:51:43 +0100

    fcitx5: revert formatting

--- modules/fcitx5/hm.nix
+++ modules/fcitx5/hm.nix
@@ -21,16 +21,20 @@
     extension = "svg";
   };
 in {
-  options.stylix.targets.fcitx5.enable = config.lib.stylix.mkEnableTarget "fcitx5" true;
+  options.stylix.targets.fcitx5.enable =
+    config.lib.stylix.mkEnableTarget "fcitx5" true;
 
-  config = lib.mkIf (config.stylix.enable && config.stylix.targets.fcitx5.enable) {
-    xdg.dataFile = {
-      "fcitx5/themes/stylix/theme.conf".source = theme;
-      "fcitx5/themes/stylix/panel.svg".source = panel;
-      "fcitx5/themes/stylix/highlight.svg".source = highlight;
-      "fcitx5/themes/stylix-kwin/theme.conf".source = theme-kwin;
-      "fcitx5/themes/stylix-kwin/panel.svg".source = panel;
-      "fcitx5/themes/stylix-kwin/highlight.svg".source = highlight;
+  config =
+    lib.mkIf
+    (config.stylix.enable && config.stylix.targets.fcitx5.enable)
+    {
+      xdg.dataFile = {
+        "fcitx5/themes/stylix/theme.conf".source = theme;
+        "fcitx5/themes/stylix/panel.svg".source = panel;
+        "fcitx5/themes/stylix/highlight.svg".source = highlight;
+        "fcitx5/themes/stylix-kwin/theme.conf".source = theme-kwin;
+        "fcitx5/themes/stylix-kwin/panel.svg".source = panel;
+        "fcitx5/themes/stylix-kwin/highlight.svg".source = highlight;
+      };
     };
-  };
 }
