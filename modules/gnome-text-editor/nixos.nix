94babbd2719c0f5801eb17a089230aeda752e7af
bricked <git@bricked.dev>
2025-01-05 02:41:42 +0100

    gnome-text-editor: init module
    Add styling for the GNOME Text Editor application. Use the Gedit
    module's theme and override the HM and NixOS package to include it.
    Use Home Manager's dconf to make the theme the default.

---
+++ modules/gnome-text-editor/nixos.nix
@@ -0,0 +1,1 @@
+import ./gnome-text-editor.nix
---
+++ modules/gnome-text-editor/testbed.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+let
+  package = pkgs.gnome-text-editor;
+
+in
+{
+  stylix.testbed.application = {
+    enable = true;
+    name = "org.gnome.TextEditor";
+    inherit package;
+  };
+
+  environment.systemPackages = [ package ];
+}
