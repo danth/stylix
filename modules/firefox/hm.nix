e7c64157adebbfeff47a97420313f0724cb1830f
bricked <git@bricked.dev>
2025-01-06 19:02:24 +0100

    firefox: apply formatter

--- modules/firefox/hm.nix
+++ modules/firefox/hm.nix
@@ -1,41 +1,57 @@
-{ config, lib, ...}:
+{ config, lib, ... }:
 
 let
   targets = [
-    { path = "firefox"; name = "Firefox"; }
-    { path = "librewolf"; name = "LibreWolf"; }
+    {
+      path = "firefox";
+      name = "Firefox";
+    }
+    {
+      path = "librewolf";
+      name = "LibreWolf";
+    }
   ];
   eachConfig = mkCfg: targets: lib.mkMerge (map mkCfg targets);
-  eachTarget = mkCfg: lib.mkIf config.stylix.enable (eachConfig (target: let
-    cfg = config.stylix.targets.${target.path};
-    programCfg = config.programs.${target.path};
-  in
-    lib.mkIf cfg.enable (mkCfg {inherit target cfg programCfg;})
-  ) targets);
-in {
-  options.stylix.targets = lib.listToAttrs (map (target:
-    lib.nameValuePair target.path {
-      enable = config.lib.stylix.mkEnableTarget target.name true;
+  eachTarget =
+    mkCfg:
+    lib.mkIf config.stylix.enable (
+      eachConfig (
+        target:
+        let
+          cfg = config.stylix.targets.${target.path};
+          programCfg = config.programs.${target.path};
+        in
+        lib.mkIf cfg.enable (mkCfg {
+          inherit target cfg programCfg;
+        })
+      ) targets
+    );
+in
+{
+  options.stylix.targets = lib.listToAttrs (
+    map (
+      target:
+      lib.nameValuePair target.path {
+        enable = config.lib.stylix.mkEnableTarget target.name true;
 
-      profileNames = lib.mkOption {
-        description = "The ${target.name} profile names to apply styling on.";
-        type = lib.types.listOf lib.types.str;
-        default = [];
-      };
+        profileNames = lib.mkOption {
+          description = "The ${target.name} profile names to apply styling on.";
+          type = lib.types.listOf lib.types.str;
+          default = [ ];
+        };
 
-      firefoxGnomeTheme.enable =
-        config.lib.stylix.mkEnableTarget
-        ''
+        firefoxGnomeTheme.enable = config.lib.stylix.mkEnableTarget ''
           [Firefox GNOME
           theme](https://github.com/rafaelmardojai/firefox-gnome-theme)
-        ''
-        false;
-    })
-  targets);
+        '' false;
+      }
+    ) targets
+  );
 
   # This and the below assignment aren't merged because of
   # https://discourse.nixos.org/t/infinite-recursion-in-module-with-mkmerge/10989
-  config.programs = eachTarget ({target, cfg, ...}:
+  config.programs = eachTarget (
+    { target, cfg, ... }:
     eachConfig (profileName: {
       ${target.path}.profiles.${profileName} = lib.mkMerge [
         {
@@ -51,10 +67,12 @@ in {
             "svg.context-properties.content.enabled" = true;
           };
 
-          userChrome = builtins.readFile (config.lib.stylix.colors {
-            template = ./userChrome.mustache;
-            extension = "css";
-          });
+          userChrome = builtins.readFile (
+            config.lib.stylix.colors {
+              template = ./userChrome.mustache;
+              extension = "css";
+            }
+          );
 
           userContent = ''
             @import "firefox-gnome-theme/userContent.css";
@@ -64,9 +82,13 @@ in {
     }) cfg.profileNames
   );
 
-  config.home.file = eachTarget ({cfg, programCfg, ...}:
-    lib.mkIf cfg.firefoxGnomeTheme.enable (eachConfig (profileName: {
-      "${programCfg.configPath}/${profileName}/chrome/firefox-gnome-theme".source = config.lib.stylix.templates.firefox-gnome-theme;
-    }) cfg.profileNames)
+  config.home.file = eachTarget (
+    { cfg, programCfg, ... }:
+    lib.mkIf cfg.firefoxGnomeTheme.enable (
+      eachConfig (profileName: {
+        "${programCfg.configPath}/${profileName}/chrome/firefox-gnome-theme".source =
+          config.lib.stylix.templates.firefox-gnome-theme;
+      }) cfg.profileNames
+    )
   );
 }
