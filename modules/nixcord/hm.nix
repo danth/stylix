708556372c058bdcf66aa8fdf65ee1e18b4e3bc0
= <ari@proton.me>
2025-01-09 17:44:49 -0600

    nixcord: init module

---
+++ modules/nixcord/hm.nix
@@ -0,0 +1,18 @@
+{ config, lib, ... }:
+let
+  themeFile = config.lib.stylix.colors {
+    template = ../vesktop/template.mustache; # Uses the same template as vesktop
+    extension = ".css";
+  };
+in
+{
+  options.stylix.targets.nixcord.enable =
+    config.lib.stylix.mkEnableTarget "Nixcord" true;
+
+  config =
+    lib.mkIf (config.stylix.enable && config.stylix.targets.nixcord.enable)
+      {
+        xdg.configFile."Vencord/themes/stylix.theme.css".source = themeFile;
+        programs.nixcord.config.enabledThemes = [ "stylix.theme.css" ];
+      };
+}
