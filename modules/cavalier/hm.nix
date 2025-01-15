3abb6a45c32b59f8042ff1a8c371897ba866025f
bricked <git@bricked.dev>
2025-01-15 18:56:24 +0100

    cavalier: inline cfg variable

--- modules/cavalier/hm.nix
+++ modules/cavalier/hm.nix
@@ -5,21 +5,19 @@
     config.lib.stylix.mkEnableTarget "Cavalier" true;
 
   config =
-    let
-      cfg = config.stylix.targets.cavalier;
-    in
-    lib.mkIf (config.stylix.enable && cfg.enable) {
-      programs.cavalier.settings.general = {
-        ColorProfiles =
-          let
-            inherit (config.lib.stylix) colors;
-          in
-          lib.singleton {
-            Name = "Stylix";
-            FgColors = [ colors.base05 ];
-            BgColors = [ colors.base00 ];
-          };
-        ActiveProfile = 0;
+    lib.mkIf (config.stylix.enable && config.stylix.targets.cavalier.enable)
+      {
+        programs.cavalier.settings.general = {
+          ColorProfiles =
+            let
+              inherit (config.lib.stylix) colors;
+            in
+            lib.singleton {
+              Name = "Stylix";
+              FgColors = [ colors.base05 ];
+              BgColors = [ colors.base00 ];
+            };
+          ActiveProfile = 0;
+        };
       };
-    };
 }
