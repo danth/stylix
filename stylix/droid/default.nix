ace2c53550acaf4a1581e6802bca625f82e1190c
bricked <git@bricked.dev>
2025-01-17 23:34:55 +0100

    droid: add support for nix-on-droid platform
    Adds support for the nix-on-droid platform. Exposes the
    nixOnDroidModules.stylix module. Uses a temporary workaround for
    https://github.com/nix-community/nix-on-droid/issues/436.

---
+++ stylix/droid/default.nix
@@ -0,0 +1,27 @@
+inputs:
+{
+  palette-generator,
+  base16,
+  homeManagerModule,
+}:
+{ lib, ... }:
+
+let
+  autoload = import ../autoload.nix { inherit lib inputs; } "droid";
+in
+{
+  imports = [
+    ../pixel.nix
+    ../target.nix
+    ../opacity.nix
+    ./fonts.nix
+    (import ./palette.nix { inherit palette-generator base16; })
+    (import ../templates.nix inputs)
+    (import ../home-manager-integration.nix homeManagerModule)
+  ] ++ autoload;
+
+  # See https://github.com/nix-community/nix-on-droid/issues/436
+  options.lib = lib.mkOption {
+    type = with lib.types; attrsOf attrs;
+  };
+}
