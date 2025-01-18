e00ed7e7f4b828f8b12c5056a6167890e59854ce
hazel <hazel@meows.zip>
2025-01-18 03:36:18 -0800

    wpaperd: fix image scaling modes (#710)
    Previously, the fill image scaling mode in wpaperd was mapped to its fit
    mode, which scales the image to fit within the screen without cropping.
    The behavior is now updated to use wpaperd's center mode instead,
    aligning more closely with swaybg's fill mode. [1]
    
    [1]: https://github.com/danyspin97/wpaperd/blob/b0b7e66fd32dca36c431a174784a2e87af7edb77/README.md#wallpaper-configuration
    
    Link: https://github.com/danth/stylix/pull/710
    
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

--- modules/wpaperd/hm.nix
+++ modules/wpaperd/hm.nix
@@ -6,14 +6,31 @@
 
   config =
     lib.mkIf (config.stylix.enable && config.stylix.targets.wpaperd.enable)
-      {
-        programs.wpaperd.settings.any = {
-          path = "${config.stylix.image}";
-          mode =
-            let
-              inherit (config.stylix) imageScalingMode;
-            in
-            if imageScalingMode == "fill" then "fit" else imageScalingMode;
-        };
-      };
+      (
+        let
+          inherit (config.stylix) imageScalingMode;
+
+          # wpaperd doesn't have any mode close to the described behavior of center
+          modeMap = {
+            "stretch" = "stretch";
+            # wpaperd's center mode is closest to the described behavior of fill
+            "fill" = "center";
+            "fit" = "fit";
+            "tile" = "tile";
+          };
+
+          modeAttrs =
+            if builtins.hasAttr imageScalingMode modeMap then
+              { mode = modeMap.${imageScalingMode}; }
+            else
+              lib.info
+                "[stylix] wpaperd does not support '${imageScalingMode}' image scaling mode"
+                { };
+        in
+        {
+          programs.wpaperd.settings.any = {
+            path = "${config.stylix.image}";
+          } // modeAttrs;
+        }
+      );
 }
