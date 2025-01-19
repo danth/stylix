95e7a962dc95e6fe108be2b550c46ef70473ae04
bricked <git@bricked.dev>
2025-01-19 14:55:39 +0100

    droid: remove terminal target option

--- stylix/droid/terminal.nix
+++ stylix/droid/terminal.nix
@@ -22,9 +22,6 @@ let
   ] ++ (map (n: "color${toString n}") (lib.range 0 15));
 in
 {
-  options.stylix.targets.terminal.enable =
-    config.lib.stylix.mkEnableTarget "terminal" true;
-
   config = lib.mkIf config.stylix.enable {
     terminal.colors = lib.getAttrs colorAttrs (
       fromKittyConf (builtins.readFile theme)
