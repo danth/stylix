danth/stylix

commit a88c4d264a4379b7fe5a9e75ed51bea96f8dd407

stylix: update all flake inputs (#774)
Update all flake inputs and resolve the following warning:

    warning: input 'git-hooks' has an override for a non-existent input
    'nixpkgs-stable'

Link: https://github.com/danth/stylix/pull/774

Approved-by: Daniel Thwaites <danth@danth.me>

flake.nix
@@ -30,7 +30,6 @@
     git-hooks = {
       inputs = {
         flake-compat.follows = "flake-compat";
-        nixpkgs-stable.follows = "git-hooks/nixpkgs";
         nixpkgs.follows = "nixpkgs";
       };
 

commit f28165802cd459142014f079c3ce8d2dfc83797e

stylix: downgrade and lock base16-vim input
Closes: https://github.com/danth/stylix/issues/800
Fixes: a88c4d264a43 ("stylix: update all flake inputs (#774)")

flake.nix
@@ -12,7 +12,15 @@
 
     base16-vim = {
       flake = false;
-      url = "github:tinted-theming/base16-vim";
+
+      # TODO: Unlock this input once [1] ("Seemingly bad parsing of whitespace
+      # in abbriviated lists (affecting stylix's handling of base16-vim)") is
+      # resolved, preventing us from fetching commit [2] ("fix(theme): Remove
+      # illegal style attributes").
+      #
+      # [1]: https://github.com/SenchoPens/fromYaml/issues/1
+      # [2]: https://github.com/tinted-theming/tinted-vim/commit/0508601eff146db2537eff23e93dd0c543914896
+      url = "github:tinted-theming/base16-vim/577fe8125d74ff456cf942c733a85d769afe58b7";
     };
 
     base16.url = "github:SenchoPens/base16.nix";