danth/stylix

commit e594886eb0951a0a0c28ffa333a9df6fb13857a1

nixcord: init (#767)
Link: https://github.com/danth/stylix/pull/767

Reviewed-by: Daniel Thwaites <danth@danth.me>
Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

modules/nixcord/hm.nix
@@ -1,0 +1,27 @@
+{
+  config,
+  lib,
+  options,
+  ...
+}:
+let
+  themeFile = config.lib.stylix.colors {
+    template = ../vencord/template.mustache;
+    extension = ".css";
+  };
+  themeFileName = "stylix.theme.css";
+  cfg = config.stylix.targets.nixcord;
+in
+{
+  options.stylix.targets.nixcord.enable =
+    config.lib.stylix.mkEnableTarget "Nixcord" true;
+
+  config =
+    lib.mkIf (config.stylix.enable && cfg.enable && (config.programs ? nixcord))
+      (
+        lib.optionalAttrs (builtins.hasAttr "nixcord" options.programs) {
+          xdg.configFile."Vencord/themes/stylix.theme.css".source = themeFile;
+          programs.nixcord.config.enabledThemes = [ themeFileName ];
+        }
+      );
+}

commit 7fd04b4fae03141a739cb700edcfed2cb71e051d

nixcord: theme Vencord and Vesktop instead of just Vencord
Closes: https://github.com/danth/stylix/issues/802
Fixes: e594886eb095 ("nixcord: init (#767)")
Link: https://github.com/danth/stylix/pull/822

Co-authored-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

modules/nixcord/hm.nix
@@ -20,7 +20,20 @@ in
     lib.mkIf (config.stylix.enable && cfg.enable && (config.programs ? nixcord))
       (
         lib.optionalAttrs (builtins.hasAttr "nixcord" options.programs) {
-          xdg.configFile."Vencord/themes/stylix.theme.css".source = themeFile;
+          xdg.configFile =
+            let
+              inherit (config.programs) nixcord;
+            in
+            lib.mkMerge [
+              (lib.mkIf nixcord.discord.enable {
+                "Vencord/themes/stylix.theme.css".source = themeFile;
+              })
+
+              (lib.mkIf nixcord.vesktop.enable {
+                "vesktop/themes/stylix.theme.css".source = themeFile;
+              })
+            ];
+
           programs.nixcord.config.enabledThemes = [ themeFileName ];
         }
       );