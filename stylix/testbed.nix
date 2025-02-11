41fdc0097cda20f2d7f12ae6e2b8596e07af1fb7
NAHO <90870942+trueNAHO@users.noreply.github.com>
2025-02-11 20:48:36 +0100

    stylix: add schemeless testbed

--- stylix/testbed.nix
+++ stylix/testbed.nix
@@ -132,14 +132,14 @@ let
   makeTestbed =
     testbed: stylix:
     let
-      name =
-        builtins.concatStringsSep testbedFieldSeparator [
-          "testbed"
-          testbed.module
-          testbed.name
-          stylix.polarity
-        ]
-        ++ lib.optional (stylix.image == null) "imageless";
+      name = builtins.concatStringsSep testbedFieldSeparator [
+        "testbed"
+        testbed.module
+        testbed.name
+        stylix.polarity
+        "image${lib.optionalString (stylix.image == null) "less"}"
+        "scheme${lib.optionalString (stylix.base16Scheme or false == null) "less"}"
+      ];
 
       system = lib.nixosSystem {
         inherit (pkgs) system;
@@ -183,32 +183,44 @@ let
 
   # This generates a copy of each testbed for each of the following themes.
   makeTestbeds =
-    testbed:
-    map (makeTestbed testbed) [
-      {
-        enable = true;
-        image = pkgs.fetchurl {
+    let
+      images = {
+        dark = pkgs.fetchurl {
+          name = "mountains.jpg";
+          url = "https://unsplash.com/photos/ZqLeQDjY6fY/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzE2MzY1NDY4fA&force=true";
+          hash = "sha256-Dm/0nKiTFOzNtSiARnVg7zM0J1o+EuIdUQ3OAuasM58=";
+        };
+
+        light = pkgs.fetchurl {
           name = "three-bicycles.jpg";
           url = "https://unsplash.com/photos/hwLAI5lRhdM/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzE2MzYxNDcwfA&force=true";
           hash = "sha256-S0MumuBGJulUekoGI2oZfUa/50Jw0ZzkqDDu1nRkFUA=";
         };
+      };
+    in
+    testbed:
+    map (makeTestbed testbed) [
+      {
+        enable = true;
+        image = images.light;
         base16Scheme = "${inputs.tinted-schemes}/base16/catppuccin-latte.yaml";
         polarity = "light";
       }
       {
         enable = true;
-        image = pkgs.fetchurl {
-          name = "mountains.jpg";
-          url = "https://unsplash.com/photos/ZqLeQDjY6fY/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzE2MzY1NDY4fA&force=true";
-          hash = "sha256-Dm/0nKiTFOzNtSiARnVg7zM0J1o+EuIdUQ3OAuasM58=";
-        };
+        image = images.light;
         base16Scheme = "${inputs.tinted-schemes}/base16/catppuccin-macchiato.yaml";
         polarity = "dark";
       }
       {
         enable = true;
         image = null;
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+        base16Scheme = "${inputs.tinted-schemes}/base16/catppuccin-macchiato.yaml";
+        polarity = "dark";
+      }
+      {
+        enable = true;
+        image = images.dark;
         polarity = "dark";
       }
     ];
