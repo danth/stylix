danth/stylix

commit efb734ff436d7ba9df6648d3c9f3ce93a4b0f971

Prevent scheme generation from running when result is not used (#56)

stylix/palette.nix
@@ -8,16 +8,18 @@ let
 
   cfg = config.stylix;
 
-  paletteJSON = pkgs.runCommand "palette.json" { } ''
-    ${palette-generator}/bin/palette-generator ${cfg.polarity} ${cfg.image} $out
-  '';
-  generatedPalette = importJSON paletteJSON;
-
-  generatedScheme = generatedPalette // {
-    author = "Stylix";
-    scheme = "Stylix";
-    slug = "stylix";
-  };
+  paletteJSON = let
+    generatedJSON = pkgs.runCommand "palette.json" { } ''
+      ${palette-generator}/bin/palette-generator ${cfg.polarity} ${cfg.image} $out
+    '';
+    palette = importJSON generatedJSON;
+    scheme = base16.mkSchemeAttrs palette;
+    json = scheme {
+      template = builtins.readFile ./palette.json.mustache;
+      extension = ".json";
+    };
+  in json;
+  generatedScheme = importJSON paletteJSON;
 
   override =
     (if cfg.base16Scheme == fromOs [ "base16Scheme" ] {}
@@ -84,7 +86,7 @@ in {
         description = "The imported json";
         readOnly = true;
         internal = true;
-        default = generatedPalette;
+        default = generatedScheme;
       };
     };
 
@@ -124,5 +126,6 @@ in {
     # This attrset can be used like a function too, see
     # https://github.com/SenchoPens/base16.nix#mktheme
     lib.stylix.colors = (base16.mkSchemeAttrs cfg.base16Scheme).override override;
+    lib.stylix.scheme = base16.mkSchemeAttrs cfg.base16Scheme;
   };
 }

stylix/hm/palette.nix
@@ -7,7 +7,14 @@ args:
   config = {
     xdg.configFile = {
       # See ../nixos/palette.nix for the rational behind these two options
-      "stylix/palette.json".source = config.stylix.generated.json;
+      "stylix/generated.json".source = config.lib.stylix.scheme {
+        template = builtins.readFile ../palette.json.mustache;
+        extension = ".json";
+      };
+      "stylix/palette.json".source = config.lib.stylix.colors {
+        template = builtins.readFile ../palette.json.mustache;
+        extension = ".json";
+      };
       "stylix/palette.html".source = config.lib.stylix.colors {
         template = builtins.readFile ../palette.html.mustache;
         extension = ".html";

stylix/nixos/palette.nix
@@ -10,7 +10,18 @@ args:
       # garbage collection, so future configurations can be evaluated without
       # having to generate the palette again. The generator is not kept, only
       # the palette which came from it, so this uses very little disk space.
-      "stylix/palette.json".source = config.stylix.generated.json;
+      # The extra indirection should prevent the palette generator from running
+      # when the theme is manually specified. generated.json is necessary in
+      # the presence of overrides.
+      "stylix/generated.json".source = config.lib.stylix.scheme {
+        template = builtins.readFile ../palette.json.mustache;
+        extension = ".json";
+      };
+
+      "stylix/palette.json".source = config.lib.stylix.colors {
+        template = builtins.readFile ../palette.json.mustache;
+        extension = ".json";
+      };
 
       # We also provide a HTML version which is useful for viewing the colors
       # during development.

stylix/palette.json.mustache
@@ -1,0 +1,21 @@
+{
+  "base00": "{{{base00}}}",
+  "base01": "{{{base01}}}",
+  "base02": "{{{base02}}}",
+  "base03": "{{{base03}}}",
+  "base04": "{{{base04}}}",
+  "base05": "{{{base05}}}",
+  "base06": "{{{base06}}}",
+  "base07": "{{{base07}}}",
+  "base08": "{{{base08}}}",
+  "base09": "{{{base09}}}",
+  "base0A": "{{{base0A}}}",
+  "base0B": "{{{base0B}}}",
+  "base0C": "{{{base0C}}}",
+  "base0D": "{{{base0D}}}",
+  "base0E": "{{{base0E}}}",
+  "base0F": "{{{base0F}}}",
+  "scheme": "Stylix",
+  "author": "Stylix",
+  "slug": "stylix"
+}

commit 72a77f70514efc35efab32b4c02f52f55ec0f264

stylix: fix palette generator caching
This was broken in efb734ff436d7ba9df6648d3c9f3ce93a4b0f971,
where the added indirection prevents the output of the palette
generator becoming part of the final system.

Using `highestPrio` as a condition should prevent the palette
generator being forced to run when the output is not used, which
is the issue that the indirection was meant to solve.

stylix/palette.nix
@@ -3,29 +3,12 @@
   pkgs,
   lib,
   config,
+  options,
   ...
 }:
 
 let
   cfg = config.stylix;
-
-  paletteJSON =
-    let
-      generatedJSON = pkgs.runCommand "palette.json" { } ''
-        ${palette-generator}/bin/palette-generator \
-          "${cfg.polarity}" \
-          ${lib.escapeShellArg "${cfg.image}"} \
-          "$out"
-      '';
-      palette = lib.importJSON generatedJSON;
-      scheme = base16.mkSchemeAttrs palette;
-      json = scheme {
-        template = ./palette.json.mustache;
-        extension = ".json";
-      };
-    in
-    json;
-  generatedScheme = lib.importJSON paletteJSON;
 
 in
 {
@@ -87,19 +70,32 @@ in
 
     generated = {
       json = lib.mkOption {
-        type = lib.types.path;
-        description = "The result of palette-generator.";
+        type = lib.types.package;
+        description = "The output file produced by the palette generator.";
         readOnly = true;
         internal = true;
-        default = paletteJSON;
+        # This *must* be the derivation running the palette generator,
+        # and not anything indirect such as filling a template, otherwise
+        # the output of the palette generator will not be protected from
+        # garbage collection.
+        default = pkgs.runCommand "palette.json" { } ''
+          ${palette-generator}/bin/palette-generator \
+            "${cfg.polarity}" \
+            ${lib.escapeShellArg "${cfg.image}"} \
+            "$out"
+        '';
       };
 
       palette = lib.mkOption {
         type = lib.types.attrs;
-        description = "The imported json";
+        description = "The palette generated by the palette generator.";
         readOnly = true;
         internal = true;
-        default = generatedScheme;
+        default = (lib.importJSON cfg.generated.json) // {
+          author = "Stylix";
+          scheme = "Stylix";
+          slug = "stylix";
+        };
       };
 
       fileTree = lib.mkOption {
@@ -123,7 +119,7 @@ in
           lines
           attrs
         ];
-      default = generatedScheme;
+      default = cfg.generated.palette;
       defaultText = lib.literalMD ''
         The colors used in the theming.
 
@@ -149,21 +145,31 @@ in
     # This attrset can be used like a function too, see
     # https://github.com/SenchoPens/base16.nix/blob/b390e87cd404e65ab4d786666351f1292e89162a/README.md#theme-step-22
     lib.stylix.colors = (base16.mkSchemeAttrs cfg.base16Scheme).override cfg.override;
-    lib.stylix.scheme = base16.mkSchemeAttrs cfg.base16Scheme;
 
     stylix.generated.fileTree = {
-      # Making palette.json part of the system closure will protect it from
-      # garbage collection, so future configurations can be evaluated without
-      # having to generate the palette again. The generator is not kept, only
-      # the palette which came from it, so this uses very little disk space.
-      # The extra indirection should prevent the palette generator from running
-      # when the theme is manually specified. generated.json is necessary in
-      # the presence of overrides.
-      "stylix/generated.json".source = config.lib.stylix.scheme {
-        template = ./palette.json.mustache;
-        extension = ".json";
+      # The raw output of the palette generator.
+      "stylix/generated.json" = {
+        # We import the generated palette during evaluation but don't make it
+        # a dependency, which means the garbage collector is free to delete it
+        # immediately. Future evaluations may need to download, compile, and
+        # run the palette generator from scratch to recreate the same palette.
+        #
+        # To improve performance, we can make the generated file part of the
+        # system, which protects it from garbage collection and so increases
+        # the potential for reuse between evaluations.
+        #
+        # The palette generator executable is not affected, and can still be
+        # cleaned up as usual, so the overhead on system size is less than a
+        # kilobyte.
+        source = cfg.generated.json;
+        # Only do this when `base16Scheme` is still the option default, which
+        # is when the generated palette is used. Depending on the file in other
+        # cases would force the palette generator to run when we never read the
+        # output.
+        enable = options.stylix.base16Scheme.highestPrio >= 1500;
       };
 
+      # The current palette, with overrides applied.
       "stylix/palette.json".source = config.lib.stylix.colors {
         template = ./palette.json.mustache;
         extension = ".json";