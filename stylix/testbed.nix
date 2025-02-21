danth/stylix

commit 211a8440e7ebcbae5fa272d95612a4c1732d8bc4

stylix: support multiple testbeds per module (#858)
Support multiple testbeds per module with
/modules/<MODULE>/testbeds/<TESTBED>.nix, while validating various
invariants.

stylix/testbed.nix
@@ -90,23 +90,48 @@ let
       };
     };
 
-  autoload = builtins.concatLists (
-    lib.mapAttrsToList (
-      name: _:
-      let
-        testbed = {
-          inherit name;
-          module = "${inputs.self}/modules/${name}/testbed.nix";
-        };
-      in
-      lib.optional (builtins.pathExists testbed.module) testbed
-    ) (builtins.readDir "${inputs.self}/modules")
-  );
+  autoload =
+    let
+      directory = "testbeds";
+      modules = "${inputs.self}/modules";
+    in
+    lib.flatten (
+      lib.mapAttrsToList (
+        module: _:
+        let
+          testbeds = "${modules}/${module}/${directory}";
+        in
+        lib.mapAttrsToList (
+          testbed: type:
+          if type != "regular" then
+            builtins.throw "${testbed} must be regular: ${type}"
+
+          else if !lib.hasSuffix ".nix" testbed then
+            builtins.throw "testbed must be a Nix file: ${testbeds}/${testbed}"
+
+          else if testbed == ".nix" then
+            builtins.throw "testbed must have a name: ${testbed}"
+
+          # To prevent ambiguity with the final derivation's hyphen field
+          # separator, testbed names should not contain hyphens.
+          else if lib.hasInfix "-" testbed then
+            builtins.throw "testbed name must not contain hyphens (-): ${testbed}"
+
+          else
+            {
+              inherit module;
+
+              name = lib.removeSuffix ".nix" testbed;
+              path = "${testbeds}/${testbed}";
+            }
+        ) (lib.optionalAttrs (builtins.pathExists testbeds) (builtins.readDir testbeds))
+      ) (builtins.readDir modules)
+    );
 
   makeTestbed =
     testbed: stylix:
     let
-      name = "testbed-${testbed.name}-${stylix.polarity}";
+      name = "testbed-${testbed.module}-${testbed.name}-${stylix.polarity}";
 
       system = lib.nixosSystem {
         inherit (pkgs) system;
@@ -116,7 +141,7 @@ let
           applicationModule
           inputs.self.nixosModules.stylix
           inputs.home-manager.nixosModules.home-manager
-          testbed.module
+          testbed.path
 
           {
             inherit stylix;

commit 612fa3db2c9cc51c8171823d9543b05853798788

stylix: parametrize and change testbed field separator
Parametrize and change the testbed field separator to the colon (':')
character to avoid ambiguity with module names containing hyphens ('-')
and allow testbed names to contain hyphens.

Fixes: 211a8440e7eb ("stylix: support multiple testbeds per module (#858)")

stylix/testbed.nix
@@ -6,6 +6,7 @@
 }:
 
 let
+  testbedFieldSeparator = ":";
   username = "guest";
 
   commonModule =
@@ -114,8 +115,8 @@ let
 
           # To prevent ambiguity with the final derivation's hyphen field
           # separator, testbed names should not contain hyphens.
-          else if lib.hasInfix "-" testbed then
-            builtins.throw "testbed name must not contain hyphens (-): ${testbed}"
+          else if lib.hasInfix testbedFieldSeparator testbed then
+            builtins.throw "testbed name must not contain the '${testbedFieldSeparator}' testbed field separator: ${testbed}"
 
           else
             {
@@ -131,7 +132,12 @@ let
   makeTestbed =
     testbed: stylix:
     let
-      name = "testbed-${testbed.module}-${testbed.name}-${stylix.polarity}";
+      name = builtins.concatStringsSep testbedFieldSeparator [
+        "testbed"
+        testbed.module
+        testbed.name
+        stylix.polarity
+      ];
 
       system = lib.nixosSystem {
         inherit (pkgs) system;