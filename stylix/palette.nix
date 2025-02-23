3bcb352b3be367f8b9c0d9b7897cc1e0ba58fc48
Louis Dalibard <ontake@ontake.dev>
2025-02-23 21:49:54 +0100

    feat: fixing some color mappings

--- stylix/palette.nix
+++ stylix/palette.nix
@@ -182,16 +182,16 @@ in {
             pkgs.writeText "palette.json" (builtins.toJSON {
               base00 = adjustLightness colors.background cfg.primaryScale.dark;
               base01 = adjustLightness colors.surface_container cfg.primaryScale.dark;
-              base02 = adjustLightness colors.surface_container_highest cfg.primaryScale.dark;
+              base02 = adjustLightness colors.surface_container_high cfg.primaryScale.dark;
               base03 = adjustLightness colors.outline cfg.primaryScale.dark;
               base04 = adjustLightness colors.on_surface_variant cfg.primaryScale.dark;
-              base05 = adjustLightness colors.on_surface cfg.primaryScale.dark;
+              base05 = adjustLightness colors.surface_container_highest cfg.primaryScale.dark;
               base06 = adjustLightness colors.on_secondary_fixed cfg.primaryScale.dark;
               base07 = adjustLightness colors.on_primary_container cfg.primaryScale.dark;
               base08 = adjustLightness colors.error cfg.primaryScale.dark;
               base09 = adjustLightness colors.on_tertiary cfg.primaryScale.dark;
               base0A = adjustLightness colors.on_secondary cfg.primaryScale.dark;
-              base0B = adjustLightness colors.on_primary cfg.primaryScale.dark;
+              base0B = adjustLightness colors.on_secondary_fixed_variant cfg.primaryScale.dark;
               base0C = adjustLightness colors.on_primary_fixed cfg.primaryScale.dark;
               base0D = adjustLightness colors.surface_tint cfg.primaryScale.dark;
               base0E = adjustLightness colors.on_tertiary_fixed cfg.primaryScale.dark;
