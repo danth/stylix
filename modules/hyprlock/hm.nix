danth/stylix

commit c8e4a0d2186d388e3c6f3240b8b3ae422a7780d7

treewide: optionalize stylix.image option (#717)
Optionalize the stylix.image option when stylix.base16Scheme is set,
making the following Stylix configurations valid:

    [
      // Now it possible to set 'stylix.image = null', if
      // stylix.base16Scheme is set.
      {
        base16Scheme = /* ... */;
      }

      // This configuration was already possible.
      {
        image = /* ... */;
      }

      // This configuration was already possible.
      {
        base16Scheme = /* ... */;
        image = /* ... */;
      }
    ]

Closes: https://github.com/danth/stylix/issues/200
Closes: https://github.com/danth/stylix/issues/442
Link: https://github.com/danth/stylix/pull/717

Co-authored-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
Tested-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
Reviewed-by: Daniel Thwaites <danth@danth.me>

modules/hyprlock/hm.nix
@@ -1,21 +1,29 @@
 { config, lib, ... }:
 
 with config.lib.stylix;
-{
-  options.stylix.targets.hyprlock.enable = mkEnableTarget "Hyprlock" true;
 
-  config =
-    lib.mkIf (config.stylix.enable && config.stylix.targets.hyprlock.enable)
-      {
-        programs.hyprlock.settings = {
-          background.path = "${config.stylix.image}";
-          input-field = with colors; {
-            outer_color = "rgb(${base03})";
-            inner_color = "rgb(${base00})";
-            font_color = "rgb(${base05})";
-            fail_color = "rgb(${base08})";
-            check_color = "rgb(${base0A})";
-          };
-        };
+let
+  cfg = config.stylix.targets.hyprlock;
+in
+{
+  options.stylix.targets.hyprlock = {
+    enable = mkEnableTarget "Hyprlock" true;
+    useWallpaper = mkEnableWallpaper "Hyprlock" true;
+  };
+
+  config = lib.mkIf (config.stylix.enable && cfg.enable) {
+    programs.hyprlock.settings = {
+      background = {
+        path = lib.mkIf cfg.useWallpaper config.stylix.image;
+        color = "rgb(${base00})";
       };
+      input-field = with colors; {
+        outer_color = "rgb(${base03})";
+        inner_color = "rgb(${base00})";
+        font_color = "rgb(${base05})";
+        fail_color = "rgb(${base08})";
+        check_color = "rgb(${base0A})";
+      };
+    };
+  };
 }

modules/feh/hm.nix
@@ -4,16 +4,21 @@
   lib,
   ...
 }:
-
+let
+  cfg = config.stylix.targets.feh;
+in
 {
-  options.stylix.targets.feh.enable =
-    config.lib.stylix.mkEnableTarget "the desktop background using Feh" true;
+  options.stylix.targets.feh = {
+    enable = config.lib.stylix.mkEnableTarget "the desktop background using Feh" (
+      config.stylix.image != null
+    );
+  };
 
   config.xsession.initExtra =
     lib.mkIf
       (
         config.stylix.enable
-        && config.stylix.targets.feh.enable
+        && cfg.enable
         && (
           with config.xsession.windowManager;
           bspwm.enable

modules/gnome/hm.nix
@@ -9,6 +9,7 @@ let
   inherit (config.stylix.fonts) sansSerif serif monospace;
   fontSize = toString config.stylix.fonts.sizes.applications;
   documentFontSize = toString (config.stylix.fonts.sizes.applications - 1);
+  cfg = config.stylix.targets.gnome;
 
   activator = pkgs.writeShellApplication {
     name = "stylix-activate-gnome";
@@ -42,10 +43,12 @@ let
 
 in
 {
-  options.stylix.targets.gnome.enable =
-    config.lib.stylix.mkEnableTarget "GNOME" true;
+  options.stylix.targets.gnome = {
+    enable = config.lib.stylix.mkEnableTarget "GNOME" true;
+    useWallpaper = config.lib.stylix.mkEnableWallpaper "GNOME" true;
+  };
 
-  config = lib.mkIf (config.stylix.enable && config.stylix.targets.gnome.enable) {
+  config = lib.mkIf (config.stylix.enable && cfg.enable) {
     dconf.settings = {
       "org/gnome/desktop/background" = {
         color-shading-type = "solid";
@@ -64,8 +67,8 @@ in
           # Seemingly no tile support... :(
           else
             "zoom";
-        picture-uri = "file://${config.stylix.image}";
-        picture-uri-dark = "file://${config.stylix.image}";
+        picture-uri = lib.mkIf cfg.useWallpaper "file://${config.stylix.image}";
+        picture-uri-dark = lib.mkIf cfg.useWallpaper "file://${config.stylix.image}";
       };
 
       "org/gnome/desktop/interface" = {

modules/hyprland/hm.nix
@@ -3,7 +3,9 @@
 {
   options.stylix.targets.hyprland = {
     enable = config.lib.stylix.mkEnableTarget "Hyprland" true;
-    hyprpaper.enable = config.lib.stylix.mkEnableTarget "Hyprpaper" true;
+    hyprpaper.enable = config.lib.stylix.mkEnableTarget "Hyprpaper" (
+      config.stylix.image != null
+    );
   };
 
   config =

modules/hyprpaper/hm.nix
@@ -1,14 +1,18 @@
 { config, lib, ... }:
+let
+  cfg = config.stylix.targets.hyprpaper;
+in
 {
-  options.stylix.targets.hyprpaper.enable =
-    config.lib.stylix.mkEnableTarget "Hyprpaper" true;
+  options.stylix.targets.hyprpaper = {
+    enable = config.lib.stylix.mkEnableTarget "Hyprpaper" (
+      config.stylix.image != null
+    );
+  };
 
-  config =
-    lib.mkIf (config.stylix.enable && config.stylix.targets.hyprpaper.enable)
-      {
-        services.hyprpaper.settings = {
-          preload = [ "${config.stylix.image}" ];
-          wallpaper = [ ",${config.stylix.image}" ];
-        };
-      };
+  config = lib.mkIf (config.stylix.enable && cfg.enable) {
+    services.hyprpaper.settings = {
+      preload = [ "${config.stylix.image}" ];
+      wallpaper = [ ",${config.stylix.image}" ];
+    };
+  };
 }

modules/kde/hm.nix
@@ -10,6 +10,7 @@ let
   inherit (config.lib.stylix)
     colors
     mkEnableTarget
+    mkEnableWallpaper
     ;
   inherit (config.stylix)
     image
@@ -20,8 +21,7 @@ let
     let
       satisfies = check: (check default) && (check withImage);
     in
-    # TODO: when adding `wallpaper` option to this module, replace this with `image == null || !cfg.wallpaper`
-    if image == null then
+    if image == null || !cfg.useWallpaper then
       default
     else if satisfies lib.isString then
       default + withImage
@@ -336,7 +336,7 @@ in
 {
   options.stylix.targets.kde = {
     enable = mkEnableTarget "KDE" true;
-
+    useWallpaper = mkEnableWallpaper "KDE" true;
     decorations = lib.mkOption {
       type = lib.types.str;
       default = "org.kde.breeze";

modules/sway/hm.nix
@@ -3,6 +3,8 @@
 with config.lib.stylix.colors.withHashtag;
 
 let
+  cfg = config.stylix.targets.sway;
+
   text = base05;
   urgent = base08;
   focused = base0D;
@@ -15,11 +17,13 @@ let
 
 in
 {
-  options.stylix.targets.sway.enable =
-    config.lib.stylix.mkEnableTarget "Sway" true;
+  options.stylix.targets.sway = {
+    enable = config.lib.stylix.mkEnableTarget "Sway" true;
+    useWallpaper = config.lib.stylix.mkEnableWallpaper "Sway" true;
+  };
 
   config = lib.mkMerge [
-    (lib.mkIf (config.stylix.enable && config.stylix.targets.sway.enable) {
+    (lib.mkIf (config.stylix.enable && cfg.enable) {
       wayland.windowManager.sway.config = {
         inherit fonts;
 
@@ -57,7 +61,8 @@ in
             };
           };
 
-        output."*".bg = "${config.stylix.image} ${config.stylix.imageScalingMode}";
+        output."*".bg =
+          lib.mkIf cfg.useWallpaper "${config.stylix.image} ${config.stylix.imageScalingMode}";
         seat."*".xcursor_theme =
           ''"${config.stylix.cursor.name}" ${toString config.stylix.cursor.size}'';
       };

modules/swaylock/hm.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  options,
   config,
   lib,
   ...
@@ -9,6 +8,8 @@
 with config.lib.stylix.colors;
 
 let
+  cfg = config.stylix.targets.swaylock;
+
   inside = base01-hex;
   outside = base01-hex;
   ring = base05-hex;
@@ -18,16 +19,26 @@ let
 
 in
 {
+  imports = [
+    (lib.mkRenamedOptionModuleWith {
+      from = [
+        "stylix"
+        "targets"
+        "swaylock"
+        "useImage"
+      ];
+      sinceRelease = 2505;
+      to = [
+        "stylix"
+        "targets"
+        "swaylock"
+        "useWallpaper"
+      ];
+    })
+  ];
   options.stylix.targets.swaylock = {
     enable = config.lib.stylix.mkEnableTarget "Swaylock" true;
-    useImage = lib.mkOption {
-      description = ''
-        Whether to use your wallpaper image for the Swaylock background.
-        If this is disabled, a plain color will be used instead.
-      '';
-      type = lib.types.bool;
-      default = true;
-    };
+    useWallpaper = config.lib.stylix.mkEnableWallpaper "Swaylock" true;
   };
 
   config =
@@ -71,7 +82,7 @@ in
             text-ver-color = text;
             text-wrong-color = text;
           }
-          // lib.optionalAttrs config.stylix.targets.swaylock.useImage {
+          // lib.optionalAttrs cfg.useWallpaper {
             image = "${config.stylix.image}";
           };
       };

commit 2c4b5bec3685da042a2241d3b26af5ea66f2b062

hyprlock: fix background color definition (#906)
Fixes: c8e4a0d2186d ("treewide: optionalize stylix.image option (#717)")

modules/hyprlock/hm.nix
@@ -15,7 +15,7 @@ in
     programs.hyprlock.settings = {
       background = {
         path = lib.mkIf cfg.useWallpaper config.stylix.image;
-        color = "rgb(${base00})";
+        color = "rgb(${colors.base00})";
       };
       input-field = with colors; {
         outer_color = "rgb(${base03})";