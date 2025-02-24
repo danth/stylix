danth/stylix

commit f121a142abde1b6aa9738e4c21a330c0ddd4eb70

stylix: parametrize and change testbed field separator (#887)
Parametrize and change the testbed field separator to the colon (':')
character to avoid ambiguity with module names containing hyphens ('-'),
and allow testbed names to contain hyphens.

Fixes: 211a8440e7eb ("stylix: support multiple testbeds per module (#858)")

stylix/testbed.nix
@@ -6,6 +6,7 @@
 }:
 
 let
+  testbedFieldSeparator = ":";
   username = "guest";
 
   commonModule =
@@ -114,8 +115,8 @@ let
 
           # To prevent ambiguity with the final derivation's hyphen field
           # separator, testbed names should not contain hyphens.
-          else if lib.hasInfix "-" testbed then
-            builtins.throw "testbed name must not contain hyphens (-): ${testbed}"
+          else if lib.hasInfix testbedFieldSeparator testbed then
+            builtins.throw "testbed name must not contain the '${testbedFieldSeparator}' testbed field separator: ${testbed}"
 
           else
             {
@@ -131,7 +132,12 @@ let
   makeTestbed =
     testbed: stylix:
     let
-      name = "testbed-${testbed.module}-${testbed.name}-${stylix.polarity}";
+      name = builtins.concatStringsSep testbedFieldSeparator [
+        "testbed"
+        testbed.module
+        testbed.name
+        stylix.polarity
+      ];
 
       system = lib.nixosSystem {
         inherit (pkgs) system;

commit 064cc10d139afe7064549a300a915693808de337

stylix: apply testbed field separator check to each fields
Fixes: f121a142abde ("stylix: parametrize and change testbed field separator (#887)")

stylix/testbed.nix
@@ -113,11 +113,6 @@ let
           else if testbed == ".nix" then
             builtins.throw "testbed must have a name: ${testbed}"
 
-          # To prevent ambiguity with the final derivation's hyphen field
-          # separator, testbed names should not contain hyphens.
-          else if lib.hasInfix testbedFieldSeparator testbed then
-            builtins.throw "testbed name must not contain the '${testbedFieldSeparator}' testbed field separator: ${testbed}"
-
           else
             {
               inherit module;
@@ -132,12 +127,21 @@ let
   makeTestbed =
     testbed: stylix:
     let
-      name = builtins.concatStringsSep testbedFieldSeparator [
-        "testbed"
-        testbed.module
-        testbed.name
-        stylix.polarity
-      ];
+      name = builtins.concatStringsSep testbedFieldSeparator (
+        map
+          (
+            field:
+            lib.throwIf (lib.hasInfix testbedFieldSeparator field)
+              "testbed field must not contain the '${testbedFieldSeparator}' testbed field separator: ${field}"
+              field
+          )
+          [
+            "testbed"
+            testbed.module
+            testbed.name
+            stylix.polarity
+          ]
+      );
 
       system = lib.nixosSystem {
         inherit (pkgs) system;