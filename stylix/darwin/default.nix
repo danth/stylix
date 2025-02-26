e1728e828abb6b85964d72f6f55cf771c639ae3a
Flameopathic <flameopathic@gmail.com>
2025-02-26 16:58:38 -0500

    stylix: propagate inputs

--- stylix/darwin/default.nix
+++ stylix/darwin/default.nix
@@ -1,5 +1,11 @@
 inputs:
-{ lib, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  options,
+  ...
+}:
 
 # Imported modules which define new options must use an absolute path based
 # on ${inputs.self}, otherwise those options will not appear in the generated
@@ -9,15 +15,32 @@ let
   autoload = import ../autoload.nix { inherit lib inputs; } "darwin";
 in
 {
-  imports = [
-    "${inputs.self}/stylix/darwin/fonts.nix"
-    "${inputs.self}/stylix/darwin/palette.nix"
-    "${inputs.self}/stylix/fonts.nix"
-    "${inputs.self}/stylix/home-manager-integration.nix"
-    "${inputs.self}/stylix/opacity.nix"
-    "${inputs.self}/stylix/palette.nix"
-    "${inputs.self}/stylix/pixel.nix"
-    "${inputs.self}/stylix/target.nix"
-    (import ../templates.nix inputs)
-  ] ++ autoload;
+  imports =
+    (
+      map
+        (
+          module:
+          import module {
+            inherit
+              lib
+              config
+              pkgs
+              inputs
+              options
+              ;
+          }
+        )
+        [
+          "${inputs.self}/stylix/darwin/fonts.nix"
+          "${inputs.self}/stylix/darwin/palette.nix"
+          "${inputs.self}/stylix/fonts.nix"
+          "${inputs.self}/stylix/home-manager-integration.nix"
+          "${inputs.self}/stylix/opacity.nix"
+          "${inputs.self}/stylix/palette.nix"
+          "${inputs.self}/stylix/pixel.nix"
+          "${inputs.self}/stylix/target.nix"
+        ]
+      ++ autoload
+    )
+    ++ [ (import ../templates.nix inputs) ];
 }
--- stylix/hm/default.nix
+++ stylix/hm/default.nix
@@ -1,5 +1,11 @@
 inputs:
-{ lib, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  options,
+  ...
+}:
 
 # Imported modules which define new options must use an absolute path based
 # on ${inputs.self}, otherwise those options will not appear in the generated
@@ -9,18 +15,35 @@ let
   autoload = import ../autoload.nix { inherit lib inputs; } "hm";
 in
 {
-  imports = [
-    "${inputs.self}/stylix/cursor.nix"
-    "${inputs.self}/stylix/fonts.nix"
-    "${inputs.self}/stylix/hm/cursor.nix"
-    "${inputs.self}/stylix/hm/fonts.nix"
-    "${inputs.self}/stylix/hm/icon.nix"
-    "${inputs.self}/stylix/hm/palette.nix"
-    "${inputs.self}/stylix/icon.nix"
-    "${inputs.self}/stylix/opacity.nix"
-    "${inputs.self}/stylix/palette.nix"
-    "${inputs.self}/stylix/pixel.nix"
-    "${inputs.self}/stylix/target.nix"
-    (import ../templates.nix inputs)
-  ] ++ autoload;
+  imports =
+    (
+      map
+        (
+          module:
+          import module {
+            inherit
+              lib
+              config
+              pkgs
+              inputs
+              options
+              ;
+          }
+        )
+        [
+          "${inputs.self}/stylix/cursor.nix"
+          "${inputs.self}/stylix/fonts.nix"
+          "${inputs.self}/stylix/hm/cursor.nix"
+          "${inputs.self}/stylix/hm/fonts.nix"
+          "${inputs.self}/stylix/hm/icon.nix"
+          "${inputs.self}/stylix/hm/palette.nix"
+          "${inputs.self}/stylix/icon.nix"
+          "${inputs.self}/stylix/opacity.nix"
+          "${inputs.self}/stylix/palette.nix"
+          "${inputs.self}/stylix/pixel.nix"
+          "${inputs.self}/stylix/target.nix"
+        ]
+      ++ autoload
+    )
+    ++ [ (import ../templates.nix inputs) ];
 }
