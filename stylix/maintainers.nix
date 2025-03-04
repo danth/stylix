fe3687c4a30e0bbb6ab7acd37054bb84bc6911f2
awwpotato <awwpotato@voidq.com>
2025-03-03 21:44:49 -0800

    stylix: add maintainers

---
+++ stylix/maintainers.nix
@@ -0,0 +1,15 @@
+# Stylix maintainers.
+#
+# This attribute set contains Stylix module maintainers that do
+# not have an entry in the Nixpkgs maintainer list [1]. Entries here
+# are expected to be follow the same format as described in [1].
+#
+# [1] https://github.com/NixOS/nixpkgs/blob/fca0d6e093c82b31103dc0dacc48da2a9b06e24b/maintainers/maintainer-list.nix#LC1
+{
+  awwpotato = {
+    email = "awwpotato@voidq.com";
+    github = "awwpotato";
+    githubId = 153149335;
+    name = "awwpotato";
+  };
+}
