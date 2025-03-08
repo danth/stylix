256b57e410f05b37f9c31bbeb710787431244c58
lanovets <lanovets@proton.me>
2025-03-08 12:05:00 +0300

    river: add cursor size and background color

--- modules/river/hm.nix
+++ modules/river/hm.nix
@@ -5,11 +5,17 @@
     config.lib.stylix.mkEnableTarget "River" true;
 
   config = lib.mkIf (config.stylix.enable && config.stylix.targets.river.enable) {
-    wayland.windowManager.river.settings = {
-      border-color-focused = "0x${config.lib.stylix.colors.base0D}";
-      border-color-unfocused = "0x${config.lib.stylix.colors.base03}";
-      border-color-urgent = "0x${config.lib.stylix.colors.base08}";
-      xcursor-theme = config.stylix.cursor.name;
-    };
+    wayland.windowManager.river.settings =
+      let
+        inherit (config.lib.stylix) colors;
+        inherit (config.stylix) cursor;
+      in
+      {
+        border-color-focused = "0x${colors.base0D}";
+        border-color-unfocused = "0x${colors.base03}";
+        border-color-urgent = "0x${colors.base08}";
+        background-color = "0x${colors.base00}";
+        xcursor-theme = "${cursor.name} ${toString cursor.size}";
+      };
   };
 }
