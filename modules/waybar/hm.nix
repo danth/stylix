584cfde6ef59899b026a0b476c9a8c1fafad7efb
awwpotato <awwpotato@voidq.com>
2025-03-09 11:38:27 -0700

    waybar: add font option; default to monospace

--- modules/waybar/hm.nix
+++ modules/waybar/hm.nix
@@ -14,6 +14,17 @@ in
 {
   options.stylix.targets.waybar = {
     enable = config.lib.stylix.mkEnableTarget "Waybar" true;
+    font = lib.mkOption {
+      type = lib.types.enum [
+        "serif"
+        "sansSerif"
+        "monospace"
+        "emoji"
+      ];
+      default = "monospace";
+      example = "sansSerif";
+      description = "The font for waybar to use";
+    };
     addCss = lib.mkOption {
       type = lib.types.bool;
       default = true;
@@ -47,7 +58,7 @@ in
         @define-color base0C ${base0C}; @define-color base0D ${base0D}; @define-color base0E ${base0E}; @define-color base0F ${base0F};
 
         * {
-            font-family: "${config.stylix.fonts.sansSerif.name}";
+            font-family: "${config.stylix.fonts.${cfg.font}.name}";
             font-size: ${builtins.toString config.stylix.fonts.sizes.desktop}pt;
         }
       ''
