c9c9380d3bd0359def35ad0a83a7a1afb55b23de
bricked <git@bricked.dev>
2025-03-11 19:21:25 +0100

    gtksourceview: repeat style config for all package versions

--- modules/gtksourceview/hm.nix
+++ modules/gtksourceview/hm.nix
@@ -14,6 +14,11 @@ in
   config =
     lib.mkIf (config.stylix.enable && config.stylix.targets.gtksourceview.enable)
       {
-        xdg.dataFile."gtksourceview-3.0/styles/stylix.xml".source = style;
+        xdg.dataFile = {
+          "gtksourceview-2.0/styles/stylix.xml".source = style;
+          "gtksourceview-3.0/styles/stylix.xml".source = style;
+          "gtksourceview-4/styles/stylix.xml".source = style;
+          "gtksourceview-5/styles/stylix.xml".source = style;
+        };
       };
 }
