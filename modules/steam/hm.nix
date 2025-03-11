a33b6fe6f16da1042da8f36bf2fd9a5c665993da
bricked <git@bricked.dev>
2025-03-11 20:48:50 +0100

    steam: symlink adwaita theme

--- modules/steam/hm.nix
+++ modules/steam/hm.nix
@@ -1,38 +1,60 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   cfg = config.stylix.targets.steam;
-in {
+
+  adwaitaCustomCss = config.lib.stylix.colors {
+    template = ./custom.mustache;
+    extension = "css";
+  };
+
+  adwaitaTheme = pkgs.stdenv.mkDerivation (_self: {
+    name = "Adwaita-for-Steam";
+    version = "3.1";
+
+    src = config.stylix.inputs.adwaita-for-steam;
+
+    buildInputs = with pkgs; [ python3 ];
+
+    buildPhase = ''
+      mkdir --parents $out/steamui/css
+      touch $out/steamui/css/library.css
+      cp ${adwaitaCustomCss} ./custom/custom.css
+      python3 install.py --custom-css --target $out
+    '';
+  });
+in
+{
   options.stylix.targets.steam = {
     enable = config.lib.stylix.mkEnableTarget "Steam" true;
     adwaitaForSteam.enable = config.lib.stylix.mkEnableTarget "Adwaita for Steam" true;
   };
 
-  config = lib.mkIf (config.stylix.enable && cfg.enable) (lib.mkMerge [
-    {
-      # Generic configuration
-    }
+  config =
+    lib.mkIf (config.stylix.enable && cfg.enable && cfg.adwaitaForSteam.enable)
+      {
+        home.activation.backupSteamCss =
+          let
+            shellScript = pkgs.writeShellScript "backup-steam-css" ''
+              css_dir="${config.xdg.dataHome}/Steam/steamui/css"
+              cp --no-clobber "$css_dir/library.css" "$css_dir/library.original.css"
+            '';
+          in
+          config.lib.dag.entryBefore [ "writeBoundary" ] ''
+            run --quiet ${shellScript}
+          '';
 
-    (lib.mkIf cfg.adwaitaForSteam.enable {
-      home.packages = with pkgs; [adwsteamgtk];
-
-      home.activation.adwaitaForSteam = let
-        shellScript = pkgs.writeShellScript "adwaitaForSteam" ''
-          rm -f "$HOME/.cache/AdwSteamInstaller/extracted/custom/custom.css"
-          ${lib.getExe pkgs.adwsteamgtk} --install
-        '';
-      in
-        config.lib.dag.entryAfter ["writeBoundary" "dconfSettings"] ''
-          run --quiet ${shellScript}
-        '';
-
-      # Use custom.css
-      dconf.settings."io/github/Foldex/AdwSteamGtk".prefs-install-custom-css = true;
-
-      xdg.configFile."AdwSteamGtk/custom.css".source = config.lib.stylix.colors {
-        template = ./custom.mustache;
-        extension = "css";
+        xdg.dataFile = {
+          "Steam/steamui/adwaita".source = "${adwaitaTheme}/steamui/adwaita";
+          "Steam/steamui/libraryroot.custom.css".source =
+            "${adwaitaTheme}/steamui/libraryroot.custom.css";
+          "Steam/steamui/css/library.css".source =
+            "${adwaitaTheme}/steamui/css/library.css";
+        };
       };
-    })
-  ]);
 }
