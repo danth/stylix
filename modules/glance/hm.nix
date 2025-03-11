e28bd4de28a51216fd7c89158d82b688c3793e8e
MorganTL <60503995+MorganTL@users.noreply.github.com>
2025-03-12 05:59:10 +0800

    glance: fix RGB to HSL conversion (#932)
    Link: https://github.com/danth/stylix/pull/932
    
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
    Reviewed-by: Daniel Thwaites <danth@danth.me>

--- modules/glance/hm.nix
+++ modules/glance/hm.nix
@@ -11,17 +11,18 @@
             rgb-to-hsl =
               color:
               let
-                r = ((lib.toInt config.lib.stylix.colors."${color}-rgb-r") * 100) / 255;
-                g = ((lib.toInt config.lib.stylix.colors."${color}-rgb-g") * 100) / 255;
-                b = ((lib.toInt config.lib.stylix.colors."${color}-rgb-b") * 100) / 255;
+                r = ((lib.toInt config.lib.stylix.colors."${color}-rgb-r") * 100.0) / 255;
+                g = ((lib.toInt config.lib.stylix.colors."${color}-rgb-g") * 100.0) / 255;
+                b = ((lib.toInt config.lib.stylix.colors."${color}-rgb-b") * 100.0) / 255;
                 max = lib.max r (lib.max g b);
                 min = lib.min r (lib.min g b);
                 delta = max - min;
+                fmod = base: int: base - (int * builtins.floor (base / int));
                 h =
                   if delta == 0 then
                     0
                   else if max == r then
-                    60 * (lib.mod ((g - b) / delta) 6)
+                    60 * (fmod ((g - b) / delta) 6)
                   else if max == g then
                     60 * (((b - r) / delta) + 2)
                   else if max == b then
@@ -30,9 +31,17 @@
                     0;
                 l = (max + min) / 2;
                 s =
-                  if delta == 0 then 0 else delta / (100 - lib.max (2 * l - 100) (100 - (2 * l)));
+                  if delta == 0 then
+                    0
+                  else
+                    100 * delta / (100 - lib.max (2 * l - 100) (100 - (2 * l)));
+                roundToString = value: toString (builtins.floor (value + 0.5));
               in
-              "${builtins.toString h} ${builtins.toString s} ${builtins.toString l}";
+              lib.concatMapStringsSep " " roundToString [
+                h
+                s
+                l
+              ];
           in
           {
             light = config.stylix.polarity == "light";
