126bb9d0d8a0e47870becdd136041ab51192ac8a
bricked <git@bricked.dev>
2025-03-11 17:55:55 +0100

    gnome-text-editor: remove nixos module

--- modules/gnome-text-editor/hm.nix
+++ modules/gnome-text-editor/hm.nix
@@ -1,12 +1,25 @@
-{ config, lib, ... }:
-
 {
-  imports = [ ./common.nix ];
+  config,
+  options,
+  lib,
+  ...
+}:
+
+let
+  inherit (lib) optional;
+in
+{
+  options.stylix.targets.gnome-text-editor.enable =
+    config.lib.stylix.mkEnableTarget "GNOME Text Editor" false;
 
   config =
     lib.mkIf
       (config.stylix.enable && config.stylix.targets.gnome-text-editor.enable)
       {
         dconf.settings."org/gnome/TextEditor".style-scheme = "stylix";
+
+        warnings =
+          optional (!options.stylix.targets.gtksourceview.enable)
+            "stylix: gnome-text-editor: This module will probably not work because the `gtksourceview' target is not enabled.";
       };
 }
