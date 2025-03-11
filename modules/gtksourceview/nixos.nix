99ce09388d97ae1799b6fa610b976cf14157c93f
bricked <git@bricked.dev>
2025-03-11 17:34:32 +0100

    gtksourceview: add nixos module with override

---
+++ modules/gtksourceview/nixos.nix
@@ -0,0 +1,34 @@
+{ config, lib, ... }:
+
+let
+  style = config.lib.stylix.colors {
+    template = ./template.xml.mustache;
+    extension = "xml";
+  };
+
+  attrsOverride = oldAttrs: {
+    postFixup = ''
+      ${oldAttrs.postFixup or ""}
+      styles_dir="$out/share/gtksourceview-3.0/styles"
+      mkdir --parents "$styles_dir"
+      cp ${style} "$styles_dir/stylix.xml"
+    '';
+  };
+
+in
+{
+  options.stylix.targets.gtksourceview.enable =
+    config.lib.stylix.mkEnableTarget "GTKSourceView, which is used by GNOME Text Editor among others" true;
+
+  config =
+    lib.mkIf (config.stylix.enable && config.stylix.targets.gtksourceview.enable)
+      {
+        nixpkgs.overlays = [
+          (_: prev: {
+            gtksourceview = prev.gtksourceview.overrideAttrs attrsOverride;
+            gtksourceview4 = prev.gtksourceview4.overrideAttrs attrsOverride;
+            gtksourceview5 = prev.gtksourceview5.overrideAttrs attrsOverride;
+          })
+        ];
+      };
+}
