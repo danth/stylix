danth/stylix

commit e3233ead63f555bba891b991b384eac9eb5ea76e

stylix: init droid (#778)
stylix: init nixOnDroidModules.stylix module to support Nix-on-Droid

Closes: https://github.com/danth/stylix/issues/775
Link: https://github.com/danth/stylix/pull/778

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
Reviewed-by: Daniel Thwaites <danth@danth.me>

flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Theming framework for NixOS, Home Manager, and nix-darwin";
+  description = "Theming framework for NixOS, Home Manager, nix-darwin and Nix-on-Droid";
 
   inputs = {
     base16-fish = {
@@ -253,6 +253,21 @@
             {
               stylix = {
                 inherit inputs;
+                paletteGenerator = self.packages.${pkgs.system}.palette-generator;
+                base16 = base16.lib args;
+                homeManagerIntegration.module = self.homeManagerModules.stylix;
+              };
+            }
+          ];
+        };
+
+      nixOnDroidModules.stylix =
+        { pkgs, ... }@args:
+        {
+          imports = [
+            (import ./stylix/droid inputs)
+            {
+              stylix = {
                 paletteGenerator = self.packages.${pkgs.system}.palette-generator;
                 base16 = base16.lib args;
                 homeManagerIntegration.module = self.homeManagerModules.stylix;

modules/console/droid.nix
@@ -1,0 +1,36 @@
+{ config, lib, ... }:
+
+{
+  options.stylix.targets.console.enable =
+    config.lib.stylix.mkEnableTarget "the Nix-on-Droid console" true;
+
+  config =
+    lib.mkIf (config.stylix.enable && config.stylix.targets.console.enable)
+      {
+        terminal.colors = with config.lib.stylix.colors.withHashtag; rec {
+          background = base00;
+          foreground = base05;
+          cursor = base05;
+
+          # normal
+          color0 = base00;
+          color1 = base08;
+          color2 = base0B;
+          color3 = base0A;
+          color4 = base0D;
+          color5 = base0E;
+          color6 = base0C;
+          color7 = base05;
+
+          # bright
+          color8 = base02;
+          color9 = color1;
+          color10 = color2;
+          color11 = color3;
+          color12 = color4;
+          color13 = color5;
+          color14 = color6;
+          color15 = base07;
+        };
+      };
+}

stylix/droid/default.nix
@@ -1,0 +1,21 @@
+inputs:
+{ lib, ... }:
+
+let
+  autoload = import ../autoload.nix { inherit lib inputs; } "droid";
+in
+{
+  imports = [
+    "${inputs.self}/stylix/fonts.nix"
+    "${inputs.self}/stylix/home-manager-integration.nix"
+    "${inputs.self}/stylix/opacity.nix"
+    "${inputs.self}/stylix/palette.nix"
+    "${inputs.self}/stylix/pixel.nix"
+    "${inputs.self}/stylix/target.nix"
+  ] ++ autoload;
+
+  # See https://github.com/nix-community/nix-on-droid/issues/436
+  options.lib = lib.mkOption {
+    type = with lib.types; attrsOf attrs;
+  };
+}

stylix/droid/fonts.nix
@@ -1,0 +1,30 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  mkFont =
+    font:
+    pkgs.runCommand "stylix-font-${font.package.name}"
+      {
+        FONTCONFIG_FILE = pkgs.makeFontsConf { fontDirectories = [ font.package ]; };
+      }
+      ''
+        font="$(
+          ${lib.getExe' pkgs.fontconfig "fc-match"} \
+          ${lib.escapeShellArg font.name} \
+          --format %{file}
+        )"
+
+        cp "$font" "$out"
+      '';
+  terminalFont = mkFont config.stylix.fonts.monospace;
+in
+{
+  imports = [ ../fonts.nix ];
+
+  config.terminal.font = lib.mkIf config.stylix.enable terminalFont;
+}

stylix/droid/palette.nix
@@ -1,0 +1,10 @@
+args:
+{ config, lib, ... }:
+
+{
+  imports = [ (import ../palette.nix args) ];
+
+  config = lib.mkIf config.stylix.enable {
+    environment.etc = config.stylix.generated.fileTree;
+  };
+}

commit 3266531f06e4bda02a48a77c1bc92925bb9a357d

stylix: add missing comma in flake description
Fixes: e3233ead63f5 ("stylix: init droid (#778)")

flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Theming framework for NixOS, Home Manager, nix-darwin and Nix-on-Droid";
+  description = "Theming framework for NixOS, Home Manager, nix-darwin, and Nix-on-Droid";
 
   inputs = {
     base16-fish = {