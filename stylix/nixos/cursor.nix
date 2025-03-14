6a2e5258876c46b62edacb3e51a759ed1c06332b
Flameopathic <64027365+Flameopathic@users.noreply.github.com>
2025-03-14 09:48:06 -0400

    stylix: optionalize cursor and disable by default (#943)
    Closes: https://github.com/danth/stylix/issues/940
    Link: https://github.com/danth/stylix/pull/943
    
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
    Reviewed-by: Daniel Thwaites <danth@danth.me>

--- stylix/nixos/cursor.nix
+++ stylix/nixos/cursor.nix
@@ -1,7 +1,7 @@
 { config, lib, ... }:
 
 {
-  config = lib.mkIf config.stylix.enable {
+  config = lib.mkIf (config.stylix.enable && config.stylix.cursor != null) {
     environment.variables.XCURSOR_SIZE = toString config.stylix.cursor.size;
   };
 }
