danth/stylix

commit ff9ae322bcaeccabc65812390000276455331123

treewide: clean up `with` and stuff (#975)
Fixes #973

modules/avizo/hm.nix
@@ -4,12 +4,6 @@
   options,
   ...
 }:
-
-with config.lib.stylix.colors;
-with config.stylix.fonts;
-let
-  aviOpacity = toString config.stylix.opacity.popups;
-in
 {
   options.stylix.targets.avizo.enable =
     config.lib.stylix.mkEnableTarget "Avizo" true;
@@ -19,12 +13,12 @@ in
     lib.mkIf (config.stylix.enable && config.stylix.targets.avizo.enable) {
       services.avizo = {
         settings = {
-          default = {
+          default = with config.lib.stylix.colors; {
             background = "rgba(${base01-rgb-r}, ${base01-rgb-g}, ${base01-rgb-b}, ${aviOpacity})";
             border-color = "rgba(${base0D-rgb-r}, ${base0D-rgb-g}, ${base0D-rgb-b}, ${aviOpacity})";
             bar-fg-color = "rgba(${base05-rgb-r}, ${base05-rgb-g}, ${base05-rgb-b}, ${aviOpacity})";
             bar-bg-color = "rgba(${base01-rgb-r}, ${base01-rgb-g}, ${base01-rgb-b}, ${aviOpacity})";
-            image-opacity = aviOpacity;
+            image-opacity = toString config.stylix.opacity.popups;
           };
         };
       };

modules/bemenu/hm.nix
@@ -1,12 +1,4 @@
 { config, lib, ... }:
-
-with config.lib.stylix.colors.withHashtag;
-with config.stylix.fonts;
-let
-  bemenuOpacity = lib.toHexString (
-    ((builtins.ceil (config.stylix.opacity.popups * 100)) * 255) / 100
-  );
-in
 {
   options.stylix.targets.bemenu = {
     enable = config.lib.stylix.mkEnableTarget "bemenu" true;
@@ -16,7 +8,7 @@ in
         Font size used for bemenu.
       '';
       type = with lib.types; nullOr int;
-      default = sizes.popups;
+      default = config.stylix.fonts.sizes.popups;
     }; # optional argument
 
     alternate = lib.mkOption {
@@ -31,28 +23,37 @@ in
   config =
     lib.mkIf (config.stylix.enable && config.stylix.targets.bemenu.enable)
       {
-        programs.bemenu.settings = with config.stylix.targets.bemenu; {
-          tb = "${base01}${bemenuOpacity}"; # Title bg
-          nb = "${base01}${bemenuOpacity}"; # Normal bg
-          fb = "${base01}${bemenuOpacity}"; # Filter bg
-          hb = "${base03}${bemenuOpacity}"; # Highlighted bg
-          sb = "${base03}${bemenuOpacity}"; # Selected bg
-          scb = "${base01}"; # Scrollbar bg
+        programs.bemenu.settings =
+          with config.lib.stylix.colors.withHashtag;
+          with config.stylix.fonts;
+          with config.stylix.targets.bemenu;
+          let
+            bemenuOpacity = lib.toHexString (
+              ((builtins.ceil (config.stylix.opacity.popups * 100)) * 255) / 100
+            );
+          in
+          {
+            tb = "${base01}${bemenuOpacity}"; # Title bg
+            nb = "${base01}${bemenuOpacity}"; # Normal bg
+            fb = "${base01}${bemenuOpacity}"; # Filter bg
+            hb = "${base03}${bemenuOpacity}"; # Highlighted bg
+            sb = "${base03}${bemenuOpacity}"; # Selected bg
+            scb = "${base01}"; # Scrollbar bg
 
-          hf = "${base0A}"; # Highlighted fg
-          sf = "${base0B}"; # Selected fg
-          tf = "${base05}"; # Title fg
-          ff = "${base05}"; # Filter fg
-          nf = "${base05}"; # Normal fg
-          scf = "${base03}"; # Scrollbar fg
+            hf = "${base0A}"; # Highlighted fg
+            sf = "${base0B}"; # Selected fg
+            tf = "${base05}"; # Title fg
+            ff = "${base05}"; # Filter fg
+            nf = "${base05}"; # Normal fg
+            scf = "${base03}"; # Scrollbar fg
 
-          ab = "${if alternate then base00 else base01}"; # Alternate bg
-          af = "${if alternate then base04 else base05}"; # Alternate fg
+            ab = "${if alternate then base00 else base01}"; # Alternate bg
+            af = "${if alternate then base04 else base05}"; # Alternate fg
 
-          # Font name
-          fn = "${sansSerif.name} ${
-            lib.optionalString (fontSize != null) (builtins.toString fontSize)
-          }";
-        };
+            # Font name
+            fn = "${sansSerif.name} ${
+              lib.optionalString (fontSize != null) (builtins.toString fontSize)
+            }";
+          };
       };
 }

modules/bspwm/hm.nix
@@ -1,18 +1,15 @@
 { config, lib, ... }:
-
-let
-  colors = config.lib.stylix.colors.withHashtag;
-in
 {
   options.stylix.targets.bspwm.enable =
     config.lib.stylix.mkEnableTarget "bspwm" true;
 
   config = lib.mkIf (config.stylix.enable && config.stylix.targets.bspwm.enable) {
-    xsession.windowManager.bspwm.settings = {
-      normal_border_color = colors.base03;
-      active_border_color = colors.base0C;
-      focused_border_color = colors.base0D;
-      presel_feedback_color = colors.base00;
-    };
+    xsession.windowManager.bspwm.settings =
+      with config.lib.stylix.colors.withHashtag; {
+        normal_border_color = base03;
+        active_border_color = base0C;
+        focused_border_color = base0D;
+        presel_feedback_color = base00;
+      };
   };
 }

modules/btop/hm.nix
@@ -1,8 +1,4 @@
 { config, lib, ... }:
-
-let
-  colors = config.lib.stylix.colors.withHashtag;
-in
 {
   options.stylix.targets.btop.enable =
     config.lib.stylix.mkEnableTarget "btop" true;
@@ -21,50 +17,51 @@ in
           theme_background = lib.mkIf (config.stylix.opacity.terminal != 1.0) false;
         };
 
-        xdg.configFile."btop/themes/stylix.theme".text = with colors; ''
-          #Generated by Stylix
-          theme[main_bg]="${base00}"
-          theme[main_fg]="${base05}"
-          theme[title]="${base05}"
-          theme[hi_fg]="${base0D}"
-          theme[selected_bg]="${base03}"
-          theme[selected_fg]="${base0D}"
-          theme[inactive_fg]="${base04}"
-          theme[graph_text]="${base06}"
-          theme[meter_bg]="${base03}"
-          theme[proc_misc]="${base06}"
-          theme[cpu_box]="${base0E}"
-          theme[mem_box]="${base0B}"
-          theme[net_box]="${base0C}"
-          theme[proc_box]="${base0D}"
-          theme[div_line]="${base01}"
-          theme[temp_start]="${base0B}"
-          theme[temp_mid]="${base0A}"
-          theme[temp_end]="${base08}"
-          theme[cpu_start]="${base0B}"
-          theme[cpu_mid]="${base0A}"
-          theme[cpu_end]="${base08}"
-          theme[free_start]="${base0A}"
-          theme[free_mid]="${base0B}"
-          theme[free_end]="${base0B}"
-          theme[cached_start]="${base0C}"
-          theme[cached_mid]="${base0C}"
-          theme[cached_end]="${base0A}"
-          theme[available_start]="${base08}"
-          theme[available_mid]="${base0A}"
-          theme[available_end]="${base0B}"
-          theme[used_start]="${base0A}"
-          theme[used_mid]="${base09}"
-          theme[used_end]="${base08}"
-          theme[download_start]="${base0B}"
-          theme[download_mid]="${base0A}"
-          theme[download_end]="${base08}"
-          theme[upload_start]="${base0B}"
-          theme[upload_mid]="${base0A}"
-          theme[upload_end]="${base08}"
-          theme[process_start]="${base0B}"
-          theme[process_mid]="${base0A}"
-          theme[process_end]="${base08}"
-        '';
+        xdg.configFile."btop/themes/stylix.theme".text =
+          with config.lib.stylix.colors.withHashtag; ''
+            #Generated by Stylix
+            theme[main_bg]="${base00}"
+            theme[main_fg]="${base05}"
+            theme[title]="${base05}"
+            theme[hi_fg]="${base0D}"
+            theme[selected_bg]="${base03}"
+            theme[selected_fg]="${base0D}"
+            theme[inactive_fg]="${base04}"
+            theme[graph_text]="${base06}"
+            theme[meter_bg]="${base03}"
+            theme[proc_misc]="${base06}"
+            theme[cpu_box]="${base0E}"
+            theme[mem_box]="${base0B}"
+            theme[net_box]="${base0C}"
+            theme[proc_box]="${base0D}"
+            theme[div_line]="${base01}"
+            theme[temp_start]="${base0B}"
+            theme[temp_mid]="${base0A}"
+            theme[temp_end]="${base08}"
+            theme[cpu_start]="${base0B}"
+            theme[cpu_mid]="${base0A}"
+            theme[cpu_end]="${base08}"
+            theme[free_start]="${base0A}"
+            theme[free_mid]="${base0B}"
+            theme[free_end]="${base0B}"
+            theme[cached_start]="${base0C}"
+            theme[cached_mid]="${base0C}"
+            theme[cached_end]="${base0A}"
+            theme[available_start]="${base08}"
+            theme[available_mid]="${base0A}"
+            theme[available_end]="${base0B}"
+            theme[used_start]="${base0A}"
+            theme[used_mid]="${base09}"
+            theme[used_end]="${base08}"
+            theme[download_start]="${base0B}"
+            theme[download_mid]="${base0A}"
+            theme[download_end]="${base08}"
+            theme[upload_start]="${base0B}"
+            theme[upload_mid]="${base0A}"
+            theme[upload_end]="${base08}"
+            theme[process_start]="${base0B}"
+            theme[process_mid]="${base0A}"
+            theme[process_end]="${base08}"
+          '';
       };
 }

modules/discord/hm.nix
@@ -6,12 +6,10 @@
   ...
 }:
 let
-  template =
-    let
-      inherit (config.lib.stylix) colors;
-      inherit (config.stylix) fonts;
-    in
-    import ./template.nix { inherit colors fonts; };
+  template = import ./template.nix {
+    inherit (config.lib.stylix) colors;
+    inherit (config.stylix) fonts;
+  };
 in
 {
   options.stylix.targets =

modules/dunst/hm.nix
@@ -1,7 +1,4 @@
 { config, lib, ... }:
-
-with config.lib.stylix.colors.withHashtag;
-with config.stylix.fonts;
 let
   dunstOpacity = lib.toHexString (
     ((builtins.floor (config.stylix.opacity.popups * 100 + 0.5)) * 255) / 100
@@ -12,10 +9,10 @@ in
     config.lib.stylix.mkEnableTarget "Dunst" true;
 
   config = lib.mkIf (config.stylix.enable && config.stylix.targets.dunst.enable) {
-    services.dunst.settings = {
+    services.dunst.settings = with config.lib.stylix.colors.withHashtag; {
       global = {
         separator_color = base02;
-        font = "${sansSerif.name} ${toString sizes.popups}";
+        font = with config.stylix.fonts; "${sansSerif.name} ${toString sizes.popups}";
       };
 
       urgency_low = {

modules/emacs/hm.nix
@@ -4,15 +4,12 @@
   lib,
   ...
 }:
-
-with config.lib.stylix.colors.withHashtag;
-with config.stylix.fonts;
-
 let
+  inherit (config.stylix) fonts;
   emacsOpacity = builtins.toString (
     builtins.ceil (config.stylix.opacity.applications * 100)
   );
-  emacsSize = builtins.toString (sizes.terminal * 1.0);
+  emacsSize = builtins.toString (fonts.sizes.terminal * 1.0);
 in
 {
   options.stylix.targets.emacs.enable =
@@ -21,49 +18,52 @@ in
   config = lib.mkIf (config.stylix.enable && config.stylix.targets.emacs.enable) {
     programs.emacs = {
       extraPackages = epkgs: [
-        (epkgs.trivialBuild {
-          pname = "base16-stylix-theme";
-          version = "0.1.0";
-          src = pkgs.writeText "base16-stylix-theme.el" ''
-            (require 'base16-theme)
+        (epkgs.trivialBuild (
+          with config.lib.stylix.colors.withHashtag;
+          {
+            pname = "base16-stylix-theme";
+            version = "0.1.0";
+            src = pkgs.writeText "base16-stylix-theme.el" ''
+              (require 'base16-theme)
 
-            (defvar base16-stylix-theme-colors
-              '(:base00 "${base00}"
-                :base01 "${base01}"
-                :base02 "${base02}"
-                :base03 "${base03}"
-                :base04 "${base04}"
-                :base05 "${base05}"
-                :base06 "${base06}"
-                :base07 "${base07}"
-                :base08 "${base08}"
-                :base09 "${base09}"
-                :base0A "${base0A}"
-                :base0B "${base0B}"
-                :base0C "${base0C}"
-                :base0D "${base0D}"
-                :base0E "${base0E}"
-                :base0F "${base0F}")
-              "All colors for Base16 stylix are defined here.")
+              (defvar base16-stylix-theme-colors
+                '(:base00 "${base00}"
+                  :base01 "${base01}"
+                  :base02 "${base02}"
+                  :base03 "${base03}"
+                  :base04 "${base04}"
+                  :base05 "${base05}"
+                  :base06 "${base06}"
+                  :base07 "${base07}"
+                  :base08 "${base08}"
+                  :base09 "${base09}"
+                  :base0A "${base0A}"
+                  :base0B "${base0B}"
+                  :base0C "${base0C}"
+                  :base0D "${base0D}"
+                  :base0E "${base0E}"
+                  :base0F "${base0F}")
+                "All colors for Base16 stylix are defined here.")
 
-            ;; Define the theme
-            (deftheme base16-stylix)
+              ;; Define the theme
+              (deftheme base16-stylix)
 
-            ;; Add all the faces to the theme
-            (base16-theme-define 'base16-stylix base16-stylix-theme-colors)
+              ;; Add all the faces to the theme
+              (base16-theme-define 'base16-stylix base16-stylix-theme-colors)
 
-            ;; Mark the theme as provided
-            (provide-theme 'base16-stylix)
+              ;; Mark the theme as provided
+              (provide-theme 'base16-stylix)
 
-            ;; Add path to theme to theme-path
-            (add-to-list 'custom-theme-load-path
-                (file-name-directory
-                    (file-truename load-file-name)))
+              ;; Add path to theme to theme-path
+              (add-to-list 'custom-theme-load-path
+                  (file-name-directory
+                      (file-truename load-file-name)))
 
-            (provide 'base16-stylix-theme)
-          '';
-          packageRequires = [ epkgs.base16-theme ];
-        })
+              (provide 'base16-stylix-theme)
+            '';
+            packageRequires = [ epkgs.base16-theme ];
+          }
+        ))
       ];
 
       extraConfig = ''
@@ -72,7 +72,7 @@ in
         (setq base16-theme-256-color-source 'colors)
         (load-theme 'base16-stylix t)
         ;; Set font
-        (set-face-attribute 'default nil :font (font-spec :family "${monospace.name}" :size ${emacsSize}))
+        (set-face-attribute 'default nil :font (font-spec :family "${fonts.monospace.name}" :size ${emacsSize}))
         ;; -----------------------------
         ;; set opacity
         (add-to-list 'default-frame-alist '(alpha-background . ${emacsOpacity}))

modules/foot/hm.nix
@@ -1,18 +1,14 @@
 { config, lib, ... }:
-
 let
-  cfg = config.stylix.targets.foot;
-
   theme = config.lib.stylix.colors {
     templateRepo = config.stylix.inputs.tinted-foot;
   };
-
 in
 {
   options.stylix.targets.foot.enable =
     config.lib.stylix.mkEnableTarget "Foot" true;
 
-  config.programs.foot.settings = lib.mkIf cfg.enable {
+  config.programs.foot.settings = lib.mkIf config.stylix.targets.foot.enable {
     main = {
       include = theme;
       font =
@@ -20,6 +16,6 @@ in
         "${monospace.name}:size=${toString sizes.terminal}";
       dpi-aware = "no";
     };
-    colors.alpha = with config.stylix.opacity; terminal;
+    colors.alpha = config.stylix.opacity.terminal;
   };
 }

commit f122d70925ca44e5ee4216661769437ab36a6a3f

avizo: hotfix aviOpcaity missing (#1023)
Closes: https://github.com/danth/stylix/issues/1018
Fixes: ff9ae322bcae ("treewide: clean up `with` and stuff (#975)")
Link: https://github.com/danth/stylix/pull/1023

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

modules/avizo/hm.nix
@@ -13,13 +13,18 @@
     lib.mkIf (config.stylix.enable && config.stylix.targets.avizo.enable) {
       services.avizo = {
         settings = {
-          default = with config.lib.stylix.colors; {
-            background = "rgba(${base01-rgb-r}, ${base01-rgb-g}, ${base01-rgb-b}, ${aviOpacity})";
-            border-color = "rgba(${base0D-rgb-r}, ${base0D-rgb-g}, ${base0D-rgb-b}, ${aviOpacity})";
-            bar-fg-color = "rgba(${base05-rgb-r}, ${base05-rgb-g}, ${base05-rgb-b}, ${aviOpacity})";
-            bar-bg-color = "rgba(${base01-rgb-r}, ${base01-rgb-g}, ${base01-rgb-b}, ${aviOpacity})";
-            image-opacity = toString config.stylix.opacity.popups;
-          };
+          default =
+            with config.lib.stylix.colors;
+            let
+              aviOpacity = toString config.stylix.opacity.popups;
+            in
+            {
+              background = "rgba(${base01-rgb-r}, ${base01-rgb-g}, ${base01-rgb-b}, ${aviOpacity})";
+              border-color = "rgba(${base0D-rgb-r}, ${base0D-rgb-g}, ${base0D-rgb-b}, ${aviOpacity})";
+              bar-fg-color = "rgba(${base05-rgb-r}, ${base05-rgb-g}, ${base05-rgb-b}, ${aviOpacity})";
+              bar-bg-color = "rgba(${base01-rgb-r}, ${base01-rgb-g}, ${base01-rgb-b}, ${aviOpacity})";
+              image-opacity = aviOpacity;
+            };
         };
       };
     }