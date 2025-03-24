danth/stylix

commit e43eb4e2a7dfbd96454df2b1c9418299b4373773

stylix: init module maintainers framework (#977)
Initialize module maintainers framework to streamline the future
population of module maintainers [1].

[1]: https://github.com/danth/stylix/issues/275

Link: https://github.com/danth/stylix/pull/977

Reviewed-by: Daniel Thwaites <danth@danth.me>
Co-authored-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

stylix/maintainers.nix
@@ -1,0 +1,8 @@
+# Stylix maintainers.
+#
+# This attribute set contains Stylix module maintainers that do
+# not have an entry in the Nixpkgs maintainer list [1]. Entries here
+# are expected to follow the same format as described in [1].
+#
+# [1]: https://github.com/NixOS/nixpkgs/blob/1da63e6cc622a0cb6fd5b86d49923e4eb1e33b70/maintainers/maintainer-list.nix
+{ }

commit 080a393f584a40620a760677a38c8685f2f2ae7b

stylix: wrap code comment to 80 characters
Fixes: e43eb4e2a7df ("stylix: init module maintainers framework (#977)")

stylix/maintainers.nix
@@ -1,8 +1,8 @@
 # Stylix maintainers.
 #
-# This attribute set contains Stylix module maintainers that do
-# not have an entry in the Nixpkgs maintainer list [1]. Entries here
-# are expected to follow the same format as described in [1].
+# This attribute set contains Stylix module maintainers that do not have an
+# entry in the Nixpkgs maintainer list [1]. Entries here are expected to follow
+# the same format as described in [1].
 #
 # [1]: https://github.com/NixOS/nixpkgs/blob/1da63e6cc622a0cb6fd5b86d49923e4eb1e33b70/maintainers/maintainer-list.nix
 {