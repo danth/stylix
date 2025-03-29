24a0ee37cdc3e45bd64e9a3c77dff1288ce1a4a7
Daniel Thwaites <danth@danth.me>
2025-03-29 21:44:37 +0000

    discord: filter out Vencord testbed on `aarch64-linux`

--- flake.nix
+++ flake.nix
@@ -202,8 +202,19 @@
             testbedPackages = lib.optionalAttrs (lib.hasSuffix "-linux" system) (
               import ./stylix/testbed.nix { inherit pkgs inputs lib; }
             );
+
+            # Discord is not available on arm64. This workaround filters out
+            # testbeds using that package, until we have a better way to handle
+            # this.
+            testbedPackages' =
+              if system == "aarch64-linux" then
+                lib.filterAttrs (
+                  name: _: !lib.hasPrefix "testbed:discord:vencord" name
+                ) testbedPackages
+              else
+                testbedPackages;
           in
-          universalPackages // testbedPackages;
+          universalPackages // testbedPackages';
       }
     )
     // {
