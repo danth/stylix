danth/stylix

commit eb19696b18fd65ddcc6fbec5056b92b75b4f4343

stylix: add overlay module (#1048)
Closes: https://github.com/danth/stylix/issues/865
Link: https://github.com/danth/stylix/pull/1048

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

stylix/home-manager-integration.nix
@@ -208,11 +208,18 @@ in
     };
   };
 
-  config = lib.optionalAttrs (options ? home-manager) (
-    lib.mkIf config.stylix.homeManagerIntegration.autoImport {
+  config = lib.optionalAttrs (options ? home-manager) lib.mkMerge [
+    (lib.mkIf config.stylix.homeManagerIntegration.autoImport {
       home-manager.sharedModules =
-        [ config.stylix.homeManagerIntegration.module ]
+        [
+          config.stylix.homeManagerIntegration.module
+        ]
         ++ (lib.optionals config.stylix.homeManagerIntegration.followSystem copyModules);
-    }
-  );
+    })
+    (lib.mkIf config.home-manager.useGlobalPkgs {
+      home-manager.sharedModules = lib.singleton {
+        config.stylix.overlays.enable = false;
+      };
+    })
+  ];
 }

modules/gnome-text-editor/common.nix
@@ -1,28 +1,0 @@
-{ config, lib, ... }:
-
-let
-  style = config.lib.stylix.colors {
-    template = ../gedit/template.xml.mustache;
-    extension = "xml";
-  };
-in
-{
-  options.stylix.targets.gnome-text-editor.enable =
-    config.lib.stylix.mkEnableTarget "GNOME Text Editor" true;
-
-  config =
-    lib.mkIf
-      (config.stylix.enable && config.stylix.targets.gnome-text-editor.enable)
-      {
-        nixpkgs.overlays = [
-          (_: prev: {
-            gnome-text-editor = prev.gnome-text-editor.overrideAttrs (oldAttrs: {
-              postFixup = ''
-                ${oldAttrs.postFixup or ""}
-                cp ${style} $out/share/gnome-text-editor/styles/stylix.xml
-              '';
-            });
-          })
-        ];
-      };
-}

modules/gnome-text-editor/hm.nix
@@ -1,8 +1,5 @@
 { config, lib, ... }:
-
 {
-  imports = [ ./common.nix ];
-
   config =
     lib.mkIf
       (config.stylix.enable && config.stylix.targets.gnome-text-editor.enable)

modules/gnome-text-editor/nixos.nix
@@ -1,3 +1,0 @@
-{
-  imports = [ ./common.nix ];
-}

modules/gnome-text-editor/overlay.nix
@@ -1,0 +1,24 @@
+{ config, lib, ... }:
+let
+  style = config.lib.stylix.colors {
+    template = ../gedit/template.xml.mustache;
+    extension = "xml";
+  };
+in
+{
+  options.stylix.targets.gnome-text-editor.enable =
+    config.lib.stylix.mkEnableOverlay "GNOME Text Editor";
+
+  overlay =
+    _: prev:
+    lib.optionalAttrs
+      (config.stylix.enable && config.stylix.targets.gnome-text-editor.enable)
+      {
+        gnome-text-editor = prev.gnome-text-editor.overrideAttrs (oldAttrs: {
+          postFixup = ''
+            ${oldAttrs.postFixup or ""}
+            cp ${style} $out/share/gnome-text-editor/styles/stylix.xml
+          '';
+        });
+      };
+}

modules/nixos-icons/nixos.nix
@@ -1,31 +1,0 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}:
-{
-  options.stylix.targets.nixos-icons.enable =
-    config.lib.stylix.mkEnableTarget "the NixOS logo" true;
-
-  config.nixpkgs.overlays =
-    lib.mkIf (config.stylix.enable && config.stylix.targets.nixos-icons.enable)
-      [
-        (_: super: {
-          nixos-icons = super.nixos-icons.overrideAttrs (oldAttrs: {
-            src = pkgs.applyPatches {
-              inherit (oldAttrs) src;
-              prePatch = with config.lib.stylix.colors; ''
-                substituteInPlace logo/nix-snowflake-white.svg --replace-fail '#ffffff' '#${base05}'
-
-                # Insert attribution comment after the XML prolog
-                sed \
-                  --in-place \
-                  '2i<!-- The original NixOS logo from ${oldAttrs.src.url} is licensed under https://creativecommons.org/licenses/by/4.0 and has been modified to match the ${scheme} color scheme. -->' \
-                  logo/nix-snowflake-white.svg
-              '';
-            };
-          });
-        })
-      ];
-}

modules/nixos-icons/overlay.nix
@@ -1,0 +1,31 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
+  options.stylix.targets.nixos-icons.enable =
+    config.lib.stylix.mkEnableOverlay "the NixOS logo";
+
+  overlay =
+    _: super:
+    lib.optionalAttrs
+      (config.stylix.enable && config.stylix.targets.nixos-icons.enable)
+      {
+        nixos-icons = super.nixos-icons.overrideAttrs (oldAttrs: {
+          src = pkgs.applyPatches {
+            inherit (oldAttrs) src;
+            prePatch = with config.lib.stylix.colors; ''
+              substituteInPlace logo/nix-snowflake-white.svg --replace-fail '#ffffff' '#${base05}'
+
+              # Insert attribution comment after the XML prolog
+              sed \
+                --in-place \
+                '2i<!-- The original NixOS logo from ${oldAttrs.src.url} is licensed under https://creativecommons.org/licenses/by/4.0 and has been modified to match the ${scheme} color scheme. -->' \
+                logo/nix-snowflake-white.svg
+            '';
+          };
+        });
+      };
+}

stylix/darwin/default.nix
@@ -1,5 +1,9 @@
 inputs:
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  ...
+}:
 
 # Imported modules which define new options must use an absolute path based
 # on ${inputs.self}, otherwise those options will not appear in the generated
@@ -19,6 +23,7 @@ in
     "${inputs.self}/stylix/pixel.nix"
     "${inputs.self}/stylix/target.nix"
     "${inputs.self}/stylix/release.nix"
+    (import "${inputs.self}/stylix/overlays.nix" inputs)
   ] ++ autoload;
   config.warnings =
     lib.mkIf

commit 711bd28ac96dec8c9187f8db4a297677eef2e654

stylix: add missing parentheses around mkMerge (#1078)
Fixes: eb19696b18fd ("stylix: add overlay module (#1048)")
Link: https://github.com/danth/stylix/pull/1078

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
Tested-by: https://github.com/nimr0d

stylix/home-manager-integration.nix
@@ -208,18 +208,20 @@ in
     };
   };
 
-  config = lib.optionalAttrs (options ? home-manager) lib.mkMerge [
-    (lib.mkIf config.stylix.homeManagerIntegration.autoImport {
-      home-manager.sharedModules =
-        [
-          config.stylix.homeManagerIntegration.module
-        ]
-        ++ (lib.optionals config.stylix.homeManagerIntegration.followSystem copyModules);
-    })
-    (lib.mkIf config.home-manager.useGlobalPkgs {
-      home-manager.sharedModules = lib.singleton {
-        config.stylix.overlays.enable = false;
-      };
-    })
-  ];
+  config = lib.optionalAttrs (options ? home-manager) (
+    lib.mkMerge [
+      (lib.mkIf config.stylix.homeManagerIntegration.autoImport {
+        home-manager.sharedModules =
+          [
+            config.stylix.homeManagerIntegration.module
+          ]
+          ++ (lib.optionals config.stylix.homeManagerIntegration.followSystem copyModules);
+      })
+      (lib.mkIf config.home-manager.useGlobalPkgs {
+        home-manager.sharedModules = lib.singleton {
+          config.stylix.overlays.enable = false;
+        };
+      })
+    ]
+  );
 }