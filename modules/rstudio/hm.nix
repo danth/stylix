fdc8384ba985da31262df60f939e2b4378bedb9c
Grady B <broggl@broggl.farm>
2025-04-06 11:44:06 +1000

    added hm activation script to enable theme

--- modules/rstudio/hm.nix
+++ modules/rstudio/hm.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   config,
   lib,
   ...
@@ -16,5 +17,16 @@
             extension = ".rstheme";
           };
         };
+        # Modifies the RStudio config file to change the theme to stylix
+        home.activation.rstudioThemeSelect =
+          lib.hm.dag.entryAfter [ "writeBoundary" ]
+            ''
+              CONF=$HOME/.config/rstudio/rstudio-prefs.json
+              if [ -f "$CONF" ]; then
+                ${pkgs.jq}/bin/jq -r '.editor_theme|= "stylixBase16"' "$CONF" | ${pkgs.moreutils}/bin/sponge "$CONF"
+              else
+                echo "{\"editor_theme\": \"stylixBase16\"}" >"$CONF"
+              fi
+            '';
       };
 }
