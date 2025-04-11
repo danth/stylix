991db59c9888bfd9c1e837b547e445aead56cd0f
bricked <git@bricked.dev>
2025-04-11 19:19:44 +0200

    gtksourceview: convert nixos module to overlay module

---
+++ modules/gtksourceview/overlay.nix
@@ -0,0 +1,34 @@
+{ config, lib, ... }:
+
+let
+  style = config.lib.stylix.colors {
+    template = ./template.xml.mustache;
+    extension = "xml";
+  };
+
+  attrsOverride = version: oldAttrs: {
+    postFixup = ''
+      ${oldAttrs.postFixup or ""}
+      styles_dir="$out/share/gtksourceview-${version}/styles"
+      mkdir --parents "$styles_dir"
+      cp ${style} "$styles_dir/stylix.xml"
+    '';
+  };
+in
+{
+  options.stylix.targets.gtksourceview.enable =
+    config.lib.stylix.mkEnableTarget "GTKSourceView" false;
+
+  overlay =
+    _final: prev:
+    lib.optionalAttrs
+      (config.stylix.enable && config.stylix.targets.gtksourceview.enable)
+      {
+        gnome2.gtksourceview = prev.gnome2.gtksourceview.overrideAttrs (
+          attrsOverride "2.0"
+        );
+        gtksourceview = prev.gtksourceview.overrideAttrs (attrsOverride "3.0");
+        gtksourceview4 = prev.gtksourceview4.overrideAttrs (attrsOverride "4");
+        gtksourceview5 = prev.gtksourceview5.overrideAttrs (attrsOverride "5");
+      };
+}
