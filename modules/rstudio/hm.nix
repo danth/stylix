5e46fbff985988cb9397c0f24eee4d3e7b646671
Grady B <broggl@broggl.farm>
2025-04-18 00:36:45 +1000

    working theme polarity

--- modules/rstudio/hm.nix
+++ modules/rstudio/hm.nix
@@ -5,43 +5,54 @@
   ...
 }:
 {
-  options.stylix.targets.rstudio.enable =
-    config.lib.stylix.mkEnableTarget "RStudio" true;
+  options.stylix.targets.rstudio.enable = config.lib.stylix.mkEnableTarget "RStudio" true;
 
-  config =
-    lib.mkIf (config.stylix.enable && config.stylix.targets.rstudio.enable)
+  config = lib.mkIf (config.stylix.enable && config.stylix.targets.rstudio.enable) {
+
+    xdg.configFile."rstudio/themes/stylix.rstheme" =
+      let
+        polarity =
+          if config.stylix.polarity == "dark" then
+            "/* rs-theme-is-dark: TRUE */"
+          else
+            "/* rs-theme-is-dark: FALSE */";
+        mustache =
+          ''
+            /* rsthemes 0.5.0 */
+            /* https://github.com/danth/stylix */
+            /* rs-theme-name: stylixBase16*/
+            /* Theme Generator by Grady B  */
+          ''
+          + polarity
+          + "\n"
+          + builtins.readFile ./base.rstheme.mustache;
+
+      in
       {
-        xdg.configFile."rstudio/themes/stylix.rstheme" = {
-          source = config.lib.stylix.colors {
-            template = ./base.rstheme.mustache;
-            extension = ".rstheme";
-          };
+        source = config.lib.stylix.colors {
+          template = mustache;
+          extension = ".rstheme";
         };
-
-        home.activation.rstudioThemeSelect =
-          let
-            file = builtins.toFile "rstudio-prefs.json" (
-              builtins.toJSON { editor_theme = name; }
-            );
-
-            name = "stylixBase16";
-          in
-          lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-            config="$HOME/.config/rstudio/rstudio-prefs.json"
-
-            if [[ -f "$config" ]]; then
-              run ${lib.getExe pkgs.jq} \
-                --raw-output \
-                '.editor_theme |= "stylixBase16"' \
-                "$config" |
-                ${lib.getExe' pkgs.moreutils "sponge"} "$config"
-
-              verboseEcho \
-                "stylix: rstudio: setting editor_theme to '${name}' in $config"
-
-            else
-              run cp ${file} "$config"
-            fi
-          '';
       };
+    home.activation.rstudioThemeSelect =
+      let
+        file = builtins.toFile "rstudio-prefs.json" (builtins.toJSON { editor_theme = name; });
+
+        name = "stylixBase16";
+      in
+      lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        config="$HOME/.config/rstudio/rstudio-prefs.json"
+        if [[ -f "$config" ]]; then
+          run ${lib.getExe pkgs.jq} \
+            --raw-output \
+            '.editor_theme |= "stylixBase16"' \
+            "$config" |
+            ${lib.getExe' pkgs.moreutils "sponge"} "$config"
+          verboseEcho \
+            "stylix: rstudio: setting editor_theme to '${name}' in $config"
+        else
+          run cp ${file} "$config"
+        fi
+      '';
+  };
 }
