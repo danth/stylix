839c188833ef95285841536f30d8aa9acc2a82ba
awwpotato <awwpotato@voidq.com>
2025-04-21 15:06:11 -0700

    stylix: gtk convert assertion to warning

--- modules/gtk/hm.nix
+++ modules/gtk/hm.nix
@@ -41,13 +41,9 @@ in
   config = lib.mkIf cfg.enable (
     lib.mkMerge [
       {
-        assertions = lib.singleton {
-          assertion = config.gtk.gtk3.extraCss == "" && config.gtk.gtk4.extraCss == "";
-          message = ''
-            stylix: `gtk.gtk3.extraCss` and `gtk.gtk4.extraCss` have no effect.
-            Use `stylix.targets.gtk.extraCss` instead.
-          '';
-        };
+        warnings =
+          lib.optional (config.gtk.gtk3.extraCss == "" && config.gtk.gtk4.extraCss == "")
+            "stylix: `gtk.gtk3.extraCss` and `gtk.gtk4.extraCss` have no effect. Use `stylix.targets.gtk.extraCss` instead.";
 
         # programs.dconf.enable = true; required in system config
         gtk = {
