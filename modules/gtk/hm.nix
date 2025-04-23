danth/stylix

commit 375b1de2424aac1338ee6e8b4ef15976b64b0462

gtk: warn about non-functional gtk.gtk{3,4}.extraCss options (#1153)
Link: https://github.com/danth/stylix/pull/1153

Co-authored-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

modules/gtk/hm.nix
@@ -41,6 +41,10 @@ in
   config = lib.mkIf cfg.enable (
     lib.mkMerge [
       {
+        warnings =
+          lib.optional (config.gtk.gtk3.extraCss == "" && config.gtk.gtk4.extraCss == "")
+            "stylix: `gtk.gtk3.extraCss` and `gtk.gtk4.extraCss` have no effect. Use `stylix.targets.gtk.extraCss` instead.";
+
         # programs.dconf.enable = true; required in system config
         gtk = {
           enable = true;

commit f46d58be39a7822a88f29b1650ec14c961c81414

gtk: invert extraCss warning (#1159)
Fixes: 375b1de2424a ("gtk: warn about non-functional gtk.gtk{3,4}.extraCss options (#1153)")
Link: https://github.com/danth/stylix/pull/1159

Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

modules/gtk/hm.nix
@@ -42,7 +42,7 @@ in
     lib.mkMerge [
       {
         warnings =
-          lib.optional (config.gtk.gtk3.extraCss == "" && config.gtk.gtk4.extraCss == "")
+          lib.optional (config.gtk.gtk3.extraCss != "" || config.gtk.gtk4.extraCss != "")
             "stylix: `gtk.gtk3.extraCss` and `gtk.gtk4.extraCss` have no effect. Use `stylix.targets.gtk.extraCss` instead.";
 
         # programs.dconf.enable = true; required in system config