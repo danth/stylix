e9f0451c2f562216589db9ddd31a10409fdd1d68
Flameopathic <flameopathic@gmail.com>
2025-04-24 14:55:18 -0400

    fcitx5: nixify theme

--- modules/fcitx5/hm.nix
+++ modules/fcitx5/hm.nix
@@ -11,7 +11,7 @@
     lib.mkIf (config.stylix.enable && config.stylix.targets.fcitx5.enable)
       {
         i18n.inputMethod.fcitx5 = {
-          settings.addons.classicui.globalSection.Theme = "stylix";
+          # settings.addons.classicui.globalSection.Theme = "stylix";
           themes.stylix = {
             # Adapted from https://github.com/sanweiya/fcitx5-mellow-themes under the BSD 2 license (compatible with this project's license (MIT))
             # Copyright (c) 2024, sanweiya
@@ -23,10 +23,7 @@
               template = ./panel.svg.mustache;
               extension = ".svg";
             };
-            theme = config.lib.stylix.colors {
-              template = ./theme.conf.mustache;
-              extension = ".conf";
-            };
+            theme = import ./theme.nix { colors = config.lib.stylix.colors.withHashtag; };
           };
         };
       };
