a49bff748c63a67f4d3be3bb975002fcad6dbb07
awwpotato <153149335+awwpotato@users.noreply.github.com>
2025-04-23 19:51:35 -0700

    stylix: use awwpotato's base16.nix fork (#1164)
    Link: https://github.com/danth/stylix/pull/1164
    
    Closes https://github.com/danth/stylix/issues/1161
    Closes https://github.com/danth/stylix/discussions/1163
    Required for https://github.com/danth/stylix/pull/1158

--- modules/foot/hm.nix
+++ modules/foot/hm.nix
@@ -1,16 +1,15 @@
 { config, lib, ... }:
-let
-  theme = config.lib.stylix.colors {
-    templateRepo = config.stylix.inputs.tinted-foot;
-  };
-in
 {
   options.stylix.targets.foot.enable =
     config.lib.stylix.mkEnableTarget "Foot" true;
 
   config.programs.foot.settings = lib.mkIf config.stylix.targets.foot.enable {
     main = {
-      include = theme;
+      include = toString (
+        config.lib.stylix.colors {
+          templateRepo = config.stylix.inputs.tinted-foot;
+        }
+      );
       font =
         with config.stylix.fonts;
         "${monospace.name}:size=${toString sizes.terminal}";
