f3e490b651c06d968a56b35834165db575d6a85d
awwpotato <awwpotato@voidq.com>
2025-04-29 09:41:08 -0700

    stylix/get-maintainers: fix list string error

--- stylix/get-maintainers.nix
+++ stylix/get-maintainers.nix
@@ -7,7 +7,7 @@ let
   ghIds = lib.mapAttrs' (
     name: value:
     lib.nameValuePair "modules/${name}" (
-      builtins.concatMap (m: m.github or [ ]) value.maintainers
+      builtins.concatMap (m: [ m.github ]) value.maintainers
     )
   ) (import ./meta.nix args);
 in
