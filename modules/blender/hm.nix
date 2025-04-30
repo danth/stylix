95932362992ffd8a885049ec5d98d4a486dca3fb
Louis Dalibard <louis.dalibard@gmail.com>
2025-04-30 19:40:20 +0200

    Update modules/blender/hm.nix
    Co-authored-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

--- modules/blender/hm.nix
+++ modules/blender/hm.nix
@@ -17,26 +17,30 @@ in
   config =
     lib.mkIf (config.stylix.enable && config.stylix.targets.blender.enable)
       {
-        xdg.configFile = builtins.listToAttrs (
-          builtins.concatMap (version: [
-            {
-              name = "${version}/Stylix.xml";
-              value.text =
-                let
-                  coloredFile = config.lib.stylix.colors {
-                    template = ./Stylix.xml.mustache;
-                    extension = ".xml";
-                  };
-                in
-                builtins.replaceStrings
-                  [ "%POPUPSFONTSIZE%" "%DESKTOPFONTSIZE%" ]
-                  [
-                    (toString config.stylix.fonts.sizes.popups)
-                    (toString config.stylix.fonts.sizes.desktop)
-                  ]
-                  (builtins.readFile coloredFile);
-            }
-          ]) blenderVersions
-        );
+        {
+          xdg.configFile =
+            let
+              theme = builtins.readFile (
+                config.lib.stylix.colors {
+                  template = ./Stylix.xml.mustache;
+                  extension = ".xml";
+                }
+              );
+            in
+            builtins.foldl' (
+              acc: version:
+              acc
+              // {
+                "blender/${version}/scripts/presets/interface_theme/Stylix.xml".text =
+                  builtins.replaceStrings
+                    [ "%POPUPSFONTSIZE%" "%DESKTOPFONTSIZE%" ]
+                    [
+                      (toString config.stylix.fonts.sizes.popups)
+                      (toString config.stylix.fonts.sizes.desktop)
+                    ]
+                    theme;
+              }
+            ) { } versions;
+        }
       };
 }
