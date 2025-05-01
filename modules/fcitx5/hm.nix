44957b251b236ec89dc1923885141cd04e5e82ab
Flameopathic <64027365+Flameopathic@users.noreply.github.com>
2025-04-30 20:49:42 -0400

    fcitx5: use upstream options (#1158)
    Link: https://github.com/danth/stylix/pull/1158
    
    Co-authored-by: awwpotato <awwpotato@voidq.com>
    Tested-by: awwpotato <awwpotato@voidq.com>
    Reviewed-by: awwpotato <awwpotato@voidq.com>
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

--- modules/fcitx5/hm.nix
+++ modules/fcitx5/hm.nix
@@ -10,22 +10,29 @@
   config =
     lib.mkIf (config.stylix.enable && config.stylix.targets.fcitx5.enable)
       {
-        xdg = {
-          configFile."fcitx5/conf/classicui.conf".text = "Theme=stylix";
-          dataFile = {
+        i18n.inputMethod.fcitx5 = {
+          settings.addons.classicui.globalSection = with config.stylix.fonts; {
+            Theme = "stylix";
+            UseDarkTheme = false;
+            UseAccentColor = false;
+
+            Font = "${sansSerif.name} ${toString sizes.popups}";
+            MenuFont = "${sansSerif.name} ${toString sizes.popups}";
+            TrayFont = "${sansSerif.name} ${toString sizes.popups}";
+          };
+          themes.stylix = {
             # Adapted from https://github.com/sanweiya/fcitx5-mellow-themes under the BSD 2 license (compatible with this project's license (MIT))
             # Copyright (c) 2024, sanweiya
-            "fcitx5/themes/stylix/highlight.svg".source = config.lib.stylix.colors {
+            highlightImage = config.lib.stylix.colors {
               template = ./highlight.svg.mustache;
               extension = ".svg";
             };
-            "fcitx5/themes/stylix/panel.svg".source = config.lib.stylix.colors {
+            panelImage = config.lib.stylix.colors {
               template = ./panel.svg.mustache;
               extension = ".svg";
             };
-            "fcitx5/themes/stylix/theme.conf".source = config.lib.stylix.colors {
-              template = ./theme.conf.mustache;
-              extension = ".conf";
+            theme = import ./template.nix {
+              colors = config.lib.stylix.colors.withHashtag;
             };
           };
         };
