danth/stylix

commit b631dffa61e04b6d13ef6f1d86020e1e7df4153e

mako: update to new home-manager module (#1211)
Link: https://github.com/danth/stylix/pull/1211

Closes: https://github.com/danth/stylix/issues/1210

flake.nix
@@ -141,7 +141,13 @@
               };
 
               statix.enable = true;
-              typos.enable = true;
+              typos = {
+                enable = true;
+                settings.configuration = ''
+                  [default.extend-words]
+                  criterias="criterias"
+                '';
+              };
               yamllint.enable = true;
             };
 

modules/mako/hm.nix
@@ -20,23 +20,25 @@
         in
         with config.lib.stylix.colors.withHashtag;
         {
-          backgroundColor = base00 + makoOpacity;
-          borderColor = base0D;
-          textColor = base05;
-          progressColor = "over ${base02}";
-          font = "${fonts.sansSerif.name} ${toString fonts.sizes.popups}";
-          # I wish the mako hm module was like the dunst one
-          extraConfig = ''
-            [urgency=low]
-            background-color=${base00}${makoOpacity}
-            border-color=${base0D}
-            text-color=${base0A}
-
-            [urgency=high]
-            background-color=${base00}${makoOpacity}
-            border-color=${base0D}
-            text-color=${base08}
-          '';
+          settings = {
+            background-color = base00 + makoOpacity;
+            border-color = base0D;
+            text-color = base05;
+            progress-color = "over ${base02}";
+            font = "${fonts.sansSerif.name} ${toString fonts.sizes.popups}";
+          };
+          criterias = {
+            "urgency=low" = {
+              background-color = "${base00}${makoOpacity}";
+              border-color = base0D;
+              text-color = base0A;
+            };
+            "urgency=high" = {
+              background-color = "${base00}${makoOpacity}";
+              border-color = base0D;
+              text-color = base08;
+            };
+          };
         };
     }
   );

commit 340a9c5455a5690b45dc9812939f824b932bbd7d

mako: remove typo from hm module (#1224)
Fixes: b631dffa61e0 ("mako: update to new home-manager module (#1211)")
Link: https://github.com/danth/stylix/pull/1224

Reviewed-by: https://github.com/ddogfoodd

flake.nix
@@ -141,13 +141,7 @@
               };
 
               statix.enable = true;
-              typos = {
-                enable = true;
-                settings.configuration = ''
-                  [default.extend-words]
-                  criterias="criterias"
-                '';
-              };
+              typos.enable = true;
               yamllint.enable = true;
             };
 

modules/mako/hm.nix
@@ -27,7 +27,7 @@
             progress-color = "over ${base02}";
             font = "${fonts.sansSerif.name} ${toString fonts.sizes.popups}";
           };
-          criterias = {
+          criteria = {
             "urgency=low" = {
               background-color = "${base00}${makoOpacity}";
               border-color = base0D;