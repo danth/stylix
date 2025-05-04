5b64ad1d69f8c70f0a7fb932fca9fddcbefd4688
Mariusz Łapkowski <171329112+lapkowski@users.noreply.github.com>
2025-05-04 22:33:57 +0200

    xfce: add support for xfce4-terminal
    Added support for coloring the xfce-terminal terminal screen.

--- modules/xfce/hm.nix
+++ modules/xfce/hm.nix
@@ -14,6 +14,29 @@
         "Gtk/FontName" = "${sansSerif.name} ${toString sizes.applications}";
         "Gtk/MonospaceFontName" = "${monospace.name} ${toString sizes.applications}";
       };
+      xfce4-terminal = with config.lib.stylix.colors.withHashtag; {
+        "color-palette" = lib.strings.concatStringsSep ";" [
+          "${base00}"
+          "${base08}"
+          "${base0B}"
+          "${base0A}"
+          "${base0D}"
+          "${base0E}"
+          "${base0C}"
+          "${base05}"
+          "${base02}"
+          "${base08}"
+          "${base0B}"
+          "${base0A}"
+          "${base0D}"
+          "${base0E}"
+          "${base0C}"
+          "${base07}"
+        ];
+        "color-foreground" = "${base05}";
+        "color-background" = "${base00}";
+        "color-bold-is-bright" = false;
+      };
     };
   };
 }
