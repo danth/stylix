f161c03a69c7f049429b8f0feb33cb3d69f67092
Grady B <broggl@broggl.farm>
2025-05-06 16:19:06 +1000

    fixed missing run on chmod

--- modules/rstudio/hm.nix
+++ modules/rstudio/hm.nix
@@ -81,7 +81,7 @@ in
                   "stylix: rstudio: changing editor_theme from $theme to ${name} in $config"
               else
                 run cp ${file} "$config"
-                chmod 644 "$config"
+                run chmod 644 "$config"
               fi
             ''
           else
