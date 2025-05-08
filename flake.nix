834467d1d33314d5c50bfc4dda06116e4ca17887
Louis Dalibard <louis.dalibard@gmail.com>
2025-05-08 21:58:32 +0200

    remove haskell related references

--- flake.nix
+++ flake.nix
@@ -198,18 +198,12 @@
         formatter = pkgs.treefmt.withConfig {
           runtimeInputs = with pkgs; [
             nixfmt-rfc-style
-            stylish-haskell
           ];
 
           settings = {
             on-unmatched = "info";
             tree-root-file = "flake.nix";
-
             formatter = {
-              stylish-haskell = {
-                command = "stylish-haskell";
-                includes = [ "*.hx" ];
-              };
               nixfmt = {
                 command = "nixfmt";
                 options = [ "--width=80" ];
