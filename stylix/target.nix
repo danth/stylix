cfb62692fd95325fa0d9b2ed7046762cfb74bd8e
Matt Sturgeon <matt@sturgeon.me.uk>
2025-05-09 13:31:54 +0100

    stylix: split `mkEnableTarget` into `mkEnableTargetWith`

--- stylix/target.nix
+++ stylix/target.nix
@@ -38,9 +38,16 @@
     in
     {
       mkEnableTarget =
-        humanName: autoEnable:
+        name: autoEnable:
+        config.lib.stylix.mkEnableTargetWith { inherit name autoEnable; };
+
+      mkEnableTargetWith =
+        {
+          name,
+          autoEnable ? true,
+        }:
         self.mkEnableIf {
-          description = "Whether to enable theming for ${humanName}";
+          description = "Whether to enable theming for ${name}";
           condition = cfg.autoEnable && autoEnable;
           ${if autoEnable then "conditionText" else null} =
             lib.literalExpression "stylix.autoEnable";
