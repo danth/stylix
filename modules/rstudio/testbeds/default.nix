6ccf93b3627a054a2a52de3ee306539bcedfc217
Grady B <broggl@broggl.farm>
2025-05-10 19:09:53 +1000

    testing testbed rstudio with added gyp and distutils dependecies manually added

--- modules/rstudio/testbeds/default.nix
+++ modules/rstudio/testbeds/default.nix
@@ -7,6 +7,10 @@ in
   stylix.testbed.ui.command.text = "rstudio";
 
   home-manager.sharedModules = lib.singleton {
-    home.packages = [ package ];
+    home.packages = [
+      package
+      pkgs.python313Packages.distutils
+      pkgs.python313Packages.gyp
+    ];
   };
 }
