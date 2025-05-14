98444a942a85072baf12c4a1c4cd5ef9531c8ab0
awwpotato <awwpotato@voidq.com>
2025-05-14 11:40:43 -0700

    stylix: sort flake inputs (#1265)

--- flake.nix
+++ flake.nix
@@ -2,19 +2,27 @@
   description = "Theming framework for NixOS, Home Manager, nix-darwin, and Nix-on-Droid";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+
+    systems.url = "github:nix-systems/default";
+
+    # keep-sorted start block=yes newline_separated=yes
     base16-fish = {
-      flake = false;
       url = "github:tomyun/base16-fish";
+      flake = false;
     };
 
     base16-helix = {
-      flake = false;
       url = "github:tinted-theming/base16-helix";
+      flake = false;
     };
 
     base16-vim = {
-      flake = false;
-
       # TODO: Unlock this input once [1] ("Seemingly bad parsing of whitespace
       # in abbriviated lists (affecting stylix's handling of base16-vim)") is
       # resolved, preventing us from fetching commit [2] ("fix(theme): Remove
@@ -23,48 +31,46 @@
       # [1]: https://github.com/SenchoPens/fromYaml/issues/1
       # [2]: https://github.com/tinted-theming/tinted-vim/commit/0508601eff146db2537eff23e93dd0c543914896
       url = "github:tinted-theming/base16-vim/577fe8125d74ff456cf942c733a85d769afe58b7";
+      flake = false;
     };
 
     base16.url = "github:SenchoPens/base16.nix";
-    flake-compat.url = "github:edolstra/flake-compat";
 
-    flake-utils = {
-      inputs.systems.follows = "systems";
-      url = "github:numtide/flake-utils";
+    firefox-gnome-theme = {
+      url = "github:rafaelmardojai/firefox-gnome-theme";
+      flake = false;
     };
 
+    flake-compat.url = "github:edolstra/flake-compat";
+
     git-hooks = {
+      url = "github:cachix/git-hooks.nix";
       inputs = {
         flake-compat.follows = "flake-compat";
         nixpkgs.follows = "nixpkgs";
       };
-
-      url = "github:cachix/git-hooks.nix";
     };
 
     gnome-shell = {
-      flake = false;
-
       # TODO: Unlocking the input and pointing to official repository requires
       # updating the patch:
       # https://github.com/danth/stylix/pull/224#discussion_r1460339607.
       url = "github:GNOME/gnome-shell/47.2";
+      flake = false;
     };
 
     # The 'home-manager' input is used to generate the documentation.
     home-manager = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    # Interface flake systems.
-    systems.url = "github:nix-systems/default";
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     tinted-foot = {
-      flake = false;
-
       # Lock the tinted-foot input to prevent upstream breaking changes.
       #
       # Considering that Stylix eventually re-implements this input's
@@ -73,21 +79,10 @@
       #
       # [1]: https://github.com/danth/stylix/issues/571
       url = "github:tinted-theming/tinted-foot/fd1b924b6c45c3e4465e8a849e67ea82933fcbe4";
-    };
-
-    tinted-zed = {
       flake = false;
-      url = "github:tinted-theming/base16-zed";
-    };
-
-    tinted-tmux = {
-      flake = false;
-      url = "github:tinted-theming/tinted-tmux";
     };
 
     tinted-kitty = {
-      flake = false;
-
       # Lock the tinted-kitty input to prevent upstream breaking changes.
       #
       # Considering that Stylix eventually re-implements this input's
@@ -96,6 +91,7 @@
       #
       # [1]: https://github.com/danth/stylix/issues/534
       url = "github:tinted-theming/tinted-kitty/eb39e141db14baef052893285df9f266df041ff8";
+      flake = false;
     };
 
     tinted-schemes = {
@@ -103,15 +99,16 @@
       flake = false;
     };
 
-    firefox-gnome-theme = {
+    tinted-tmux = {
+      url = "github:tinted-theming/tinted-tmux";
       flake = false;
-      url = "github:rafaelmardojai/firefox-gnome-theme";
     };
 
-    nur = {
-      url = "github:nix-community/NUR";
-      inputs.nixpkgs.follows = "nixpkgs";
+    tinted-zed = {
+      url = "github:tinted-theming/base16-zed";
+      flake = false;
     };
+    # keep-sorted end
   };
 
   outputs =
