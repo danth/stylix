ecfa5c40b6a063a8d20272c123d05a3b91e14608
awwpotato <awwpotato@voidq.com>
2025-05-16 16:17:26 -0700

    flake: include packages in checks (#1285)
    Include the packages in the checks output to resolve the regression
    introduced in commit 8b015b5fa0d7 ("flake: use flake-parts (#1208)").
    
    Fixes: 8b015b5fa0d7 ("flake: use flake-parts (#1208)")
    Link: https://github.com/danth/stylix/pull/1285
    
    Co-authored-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
    Co-authored-by: Matt Sturgeon <matt@sturgeon.me.uk>
    Reviewed-by: Matt Sturgeon <matt@sturgeon.me.uk>
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

--- flake/packages.nix
+++ flake/packages.nix
@@ -7,15 +7,22 @@
 {
 
   perSystem =
-    { pkgs, system, ... }:
     {
+      pkgs,
+      system,
+      config,
+      ...
+    }:
+    {
+      # Build all packages with 'nix flake check' instead of only verifying they
+      # are derivations.
+      checks = config.packages;
+
       packages =
         let
-          # Testbeds are virtual machines based on NixOS, therefore they are
-          # only available for Linux systems.
-          testbedPackages = lib.mkIf (lib.hasSuffix "-linux" system) (
-            import "${self}/stylix/testbed.nix" { inherit pkgs inputs lib; }
-          );
+          testbedPackages = import "${self}/stylix/testbed.nix" {
+            inherit pkgs inputs lib;
+          };
 
           # Discord is not available on arm64. This workaround filters out
           # testbeds using that package, until we have a better way to handle
@@ -29,7 +36,9 @@
               testbedPackages;
         in
         lib.mkMerge [
-          testbedPackages'
+          # Testbeds are virtual machines based on NixOS, therefore they are
+          # only available for Linux systems.
+          (lib.mkIf pkgs.stdenv.hostPlatform.isLinux testbedPackages')
           {
             docs = pkgs.callPackage "${self}/docs" {
               inherit inputs;
