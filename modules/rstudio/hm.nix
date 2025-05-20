385d6368576ef6c92cd22d9e6a77a005f273a8d1
Grady B <broggl@broggl.farm>
2025-05-20 12:39:43 +1000

    added comments for various things and changed script to run on lib.mkIf

--- modules/rstudio/hm.nix
+++ modules/rstudio/hm.nix
@@ -24,15 +24,20 @@
         xdg.configFile."rstudio/themes/stylix.rstheme" =
           let
             inherit (config.lib.stylix) colors;
+
+            # Magic numbers to convert from RGB to luminance
             luminance =
               (0.2126 * (lib.toInt colors.base00-rgb-r))
               + (0.7152 * (lib.toInt colors.base00-rgb-g))
               + (0.0722 * (lib.toInt colors.base00-rgb-b));
+
             polarity =
               if luminance < 128 then
-                "/* rs-theme-is-dark: TRUE */"
+                "/* rs-theme-is-dark: TRUE */\n"
               else
-                "/* rs-theme-is-dark: FALSE */";
+                "/* rs-theme-is-dark: FALSE */\n";
+
+            # Concatenates the headers required for rstudio and forms a valid mustache file
             mustache =
               ''
                 /* rsthemes 0.5.0 */
@@ -41,7 +46,6 @@
                 /* Theme Generator by Grady B  */
               ''
               + polarity
-              + "\n"
               + builtins.readFile ./base.rstheme.mustache;
 
           in
@@ -51,15 +55,14 @@
               extension = ".rstheme";
             };
           };
-        home.activation.rstudioThemeSelect =
-          if config.stylix.targets.rstudio.themeAutoset then
+        home.activation = lib.mkIf config.stylix.targets.rstudio.themeAutoset {
+          rstudioThemeSelect =
             let
               file = builtins.toFile "rstudio-prefs.json" (
                 builtins.toJSON { editor_theme = name; }
               );
               name = "stylixBase16";
             in
-
             lib.hm.dag.entryAfter [ "writeBoundary" ] ''
               config="$HOME/.config/rstudio/rstudio-prefs.json"
 
@@ -70,18 +73,13 @@
                   "$config" |
                   ${lib.getExe' pkgs.moreutils "sponge"} "$config"
 
-                verboseEcho \
-                  "stylix: rstudio: changing editor_theme to ${name} in $config"
+              verboseEcho \
+                "stylix: rstudio: changing editor_theme to ${name} in $config"
               else
                 run cp ${file} "$config"
                 run chmod 644 "$config"
               fi
-            ''
-          else
-            lib.hm.dag.entryAfter ''
-
-              verboseEcho \
-                "stylix: rstudio: not changing editor_theme: option disabled"
             '';
+        };
       };
 }
