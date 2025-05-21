02f3d00467af0f4269d6ee1c80a28cfd15757b4a
awwpotato <awwpotato@voidq.com>
2025-05-21 14:02:23 -0700

    foliate: use mkTarget

--- modules/foliate/hm.nix
+++ modules/foliate/hm.nix
@@ -1,30 +1,29 @@
-{ config, lib, ... }:
+{ mkTarget, ... }:
+mkTarget {
+  name = "foliate";
+  humanName = "Foliate";
 
-{
-  options.stylix.targets.foliate.enable =
-    config.lib.stylix.mkEnableTarget "Foliate" true;
+  configElements =
+    { colors }:
+    {
+      programs.foliate = {
+        settings."viewer/view" = {
+          theme = "stylix.json";
+        };
 
-  config =
-    lib.mkIf (config.stylix.enable && config.stylix.targets.foliate.enable)
-      {
-        programs.foliate = {
-          settings."viewer/view" = {
-            theme = "stylix.json";
+        themes.stylix = with colors.withHashtag; {
+          label = "Stylix";
+          light = {
+            fg = base00;
+            bg = base05;
+            link = base0D;
           };
-
-          themes.stylix = with config.lib.stylix.colors.withHashtag; {
-            label = "Stylix";
-            light = {
-              fg = base00;
-              bg = base05;
-              link = base0D;
-            };
-            dark = {
-              fg = base05;
-              bg = base00;
-              link = base0D;
-            };
+          dark = {
+            fg = base05;
+            bg = base00;
+            link = base0D;
           };
         };
       };
+    };
 }
