a04bb564cf7694be8bfd3cb8a6c6ea1ac126ac3e
Matt Sturgeon <matt@sturgeon.me.uk>
2025-05-21 13:21:29 +0100

    doc: add `nix run .#docs` server
    Adds:
    - `serve-docs` to the default devShell
    - `packages.serve-docs`
    - `apps.docs`
    
    Having `apps.docs` defined causes `nix run .#docs` to run the "app"
    instead of `packages.docs`. `nix build .#docs` will still build the
    "package", not the "app".
    
    The `serve-docs` script added to the devShell runs `nix run .#docs`
    instead of having `packages.serve-docs` in its closure. This avoids the
    devShell actually building the docs.
    
    Added a note to `doc/src/modules.md`.
    
    Co-authored-by: NAHO <90870942+trueNAHO@users.noreply.github.com>

--- flake/packages.nix
+++ flake/packages.nix
@@ -13,6 +13,9 @@
       # are derivations.
       checks = config.packages;
 
+      # Make 'nix run .#docs' serve the docs
+      apps.docs.program = config.packages.serve-docs;
+
       packages = lib.mkMerge [
         # Testbeds are virtual machines based on NixOS, therefore they are
         # only available for Linux systems.
@@ -26,6 +29,9 @@
             inherit inputs;
             inherit (inputs.nixpkgs.lib) nixosSystem;
             inherit (inputs.home-manager.lib) homeManagerConfiguration;
+          };
+          serve-docs = pkgs.callPackage "${self}/doc/server.nix" {
+            inherit (config.packages) docs;
           };
           palette-generator = pkgs.callPackage "${self}/palette-generator" { };
         }
