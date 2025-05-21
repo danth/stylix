f93115a0618604c5b121880ff984963b35710a6c
awwpotato <awwpotato@voidq.com>
2025-05-21 14:10:53 -0700

    zellij: use mkTarget
    (cherry picked from commit 019d9f11b3e23b9814de65633d00763af0aea23f)

--- modules/zellij/hm.nix
+++ modules/zellij/hm.nix
@@ -1,34 +1,27 @@
-{ config, lib, ... }:
+{ mkTarget, ... }:
+mkTarget {
+  name = "zellij";
+  humanName = "zellij";
 
-{
-  options.stylix.targets.zellij.enable =
-    config.lib.stylix.mkEnableTarget "zellij" true;
-
-  config =
-    lib.mkIf
-      (
-        config.stylix.enable
-        && config.stylix.targets.zellij.enable
-        && config.programs.zellij.enable
-      )
-      {
-        programs.zellij.themes.stylix = {
-          themes = with config.lib.stylix.colors.withHashtag; {
-            default = {
-              bg = base03;
-              fg = base05;
-              red = base01;
-              green = base0B;
-              blue = base0D;
-              yellow = base0A;
-              magenta = base0E;
-              orange = base09;
-              cyan = base0C;
-              black = base00;
-              white = base07;
-            };
+  configElements =
+    { colors }:
+    {
+      programs.zellij.themes.stylix = {
+        themes = with colors.withHashtag; {
+          default = {
+            bg = base03;
+            fg = base05;
+            red = base01;
+            green = base0B;
+            blue = base0D;
+            yellow = base0A;
+            magenta = base0E;
+            orange = base09;
+            cyan = base0C;
+            black = base00;
+            white = base07;
           };
         };
       };
-
+    };
 }
