594e02cbe17401cfcc3386b0366d2f45a07d04bd
awwpotato <awwpotato@voidq.com>
2025-05-23 10:23:39 -0700

    treewide: use mkTarget (batch 2) (#1362)
    Link: https://github.com/nix-community/stylix/pull/1362
    
    Reviewed-by: Flameopathic <64027365+Flameopathic@users.noreply.github.com>
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
    (cherry picked from commit 7ffb31da698511030f17ddc94c92c59be1a30b2c)

--- modules/dunst/hm.nix
+++ modules/dunst/hm.nix
@@ -1,40 +1,50 @@
-{ config, lib, ... }:
-{
-  options.stylix.targets.dunst.enable =
-    config.lib.stylix.mkEnableTarget "Dunst" true;
+{ mkTarget, lib, ... }:
+mkTarget {
+  name = "dunst";
+  humanName = "Dunst";
 
-  config = lib.mkIf (config.stylix.enable && config.stylix.targets.dunst.enable) {
-    services.dunst.settings =
-      with config.lib.stylix.colors.withHashtag;
-      let
-        inherit (config.stylix) fonts;
-        dunstOpacity = lib.toHexString (
-          ((builtins.floor (config.stylix.opacity.popups * 100 + 0.5)) * 255) / 100
-        );
-      in
+  configElements = [
+    (
+      { fonts }:
       {
-        global = {
-          separator_color = base02;
-          font = "${fonts.sansSerif.name} ${toString fonts.sizes.popups}";
-        };
+        services.dunst.settings.global.font =
+          "${fonts.sansSerif.name} ${toString fonts.sizes.popups}";
+      }
+    )
+    (
+      { colors, opacity }:
+      {
+        services.dunst.settings =
+          with colors.withHashtag;
+          let
+            dunstOpacity = lib.toHexString (
+              ((builtins.floor (opacity.popups * 100 + 0.5)) * 255) / 100
+            );
+          in
+          {
+            global = {
+              separator_color = base02;
+            };
 
-        urgency_low = {
-          background = base01 + dunstOpacity;
-          foreground = base05;
-          frame_color = base0B;
-        };
+            urgency_low = {
+              background = base01 + dunstOpacity;
+              foreground = base05;
+              frame_color = base0B;
+            };
 
-        urgency_normal = {
-          background = base01 + dunstOpacity;
-          foreground = base05;
-          frame_color = base0E;
-        };
+            urgency_normal = {
+              background = base01 + dunstOpacity;
+              foreground = base05;
+              frame_color = base0E;
+            };
 
-        urgency_critical = {
-          background = base01 + dunstOpacity;
-          foreground = base05;
-          frame_color = base08;
-        };
-      };
-  };
+            urgency_critical = {
+              background = base01 + dunstOpacity;
+              foreground = base05;
+              frame_color = base08;
+            };
+          };
+      }
+    )
+  ];
 }
--- modules/starship/hm.nix
+++ modules/starship/hm.nix
@@ -1,76 +1,72 @@
 # Starship configuration documentation: https://starship.rs/config/
-{
-  config,
-  lib,
-  ...
-}:
-{
-  options.stylix.targets.starship.enable =
-    config.lib.stylix.mkEnableTarget "Starship" true;
+{ mkTarget, ... }:
+mkTarget {
+  name = "starship";
+  humanName = "Starship";
 
-  config =
-    lib.mkIf (config.stylix.enable && config.stylix.targets.starship.enable)
-      {
-        programs.starship.settings = {
-          palette = "base16";
-          palettes.base16 = with config.lib.stylix.colors.withHashtag; {
-            black = base00;
-            bright-black = base03;
-            white = base05;
-            bright-white = base07;
+  configElements =
+    { colors }:
+    {
+      programs.starship.settings = {
+        palette = "base16";
+        palettes.base16 = with colors.withHashtag; {
+          black = base00;
+          bright-black = base03;
+          white = base05;
+          bright-white = base07;
 
-            # Starship calls magenta purple.
-            purple = magenta;
-            bright-purple = bright-magenta;
+          # Starship calls magenta purple.
+          purple = magenta;
+          bright-purple = bright-magenta;
 
-            inherit
-              # Set Starship's standard normal color names.
-              red
-              orange
-              yellow
-              green
-              cyan
-              blue
-              magenta
-              brown
+          inherit
+            # Set Starship's standard normal color names.
+            red
+            orange
+            yellow
+            green
+            cyan
+            blue
+            magenta
+            brown
 
-              # Set Starship's standard bright color names.
-              bright-red
-              bright-yellow
-              bright-green
-              bright-cyan
-              bright-blue
-              bright-magenta
+            # Set Starship's standard bright color names.
+            bright-red
+            bright-yellow
+            bright-green
+            bright-cyan
+            bright-blue
+            bright-magenta
 
-              # Add base16 names to the template for custom usage.
-              base00
-              base01
-              base02
-              base03
-              base04
-              base05
-              base06
-              base07
-              base08
-              base09
-              base0A
-              base0B
-              base0C
-              base0D
-              base0E
-              base0F
+            # Add base16 names to the template for custom usage.
+            base00
+            base01
+            base02
+            base03
+            base04
+            base05
+            base06
+            base07
+            base08
+            base09
+            base0A
+            base0B
+            base0C
+            base0D
+            base0E
+            base0F
 
-              # Add base24 names to the template for custom usage.
-              base10
-              base11
-              base12
-              base13
-              base14
-              base15
-              base16
-              base17
-              ;
-          };
+            # Add base24 names to the template for custom usage.
+            base10
+            base11
+            base12
+            base13
+            base14
+            base15
+            base16
+            base17
+            ;
         };
       };
+    };
 }
