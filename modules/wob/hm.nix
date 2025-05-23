a230188a329b32696dd0d7d338ea09c62b38d5b8
awwpotato <awwpotato@voidq.com>
2025-05-23 10:23:39 -0700

    treewide: use mkTarget (batch 2) (#1362)
    Link: https://github.com/nix-community/stylix/pull/1362
    
    Reviewed-by: Flameopathic <64027365+Flameopathic@users.noreply.github.com>
    Reviewed-by: NAHO <90870942+trueNAHO@users.noreply.github.com>
    (cherry picked from commit 7ffb31da698511030f17ddc94c92c59be1a30b2c)

--- modules/wob/hm.nix
+++ modules/wob/hm.nix
@@ -1,18 +1,20 @@
-{ config, lib, ... }:
+{ mkTarget, ... }:
+mkTarget {
+  name = "wob";
+  humanName = "wob";
 
-{
-  options.stylix.targets.wob.enable = config.lib.stylix.mkEnableTarget "wob" true;
-
-  config = lib.mkIf (config.stylix.enable && config.stylix.targets.wob.enable) {
-    services.wob.settings = {
-      "" = with config.lib.stylix.colors; {
-        border_color = base05;
-        background_color = base00;
-        bar_color = base0A;
-        overflow_bar_color = base08;
-        overflow_background_color = base00;
-        overflow_border_color = base05;
+  configElements =
+    { colors }:
+    {
+      services.wob.settings = {
+        "" = with colors; {
+          border_color = base05;
+          background_color = base00;
+          bar_color = base0A;
+          overflow_bar_color = base08;
+          overflow_background_color = base00;
+          overflow_border_color = base05;
+        };
       };
     };
-  };
 }
