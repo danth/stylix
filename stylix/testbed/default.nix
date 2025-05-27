e13d234bcff4b65f6601a87d4a243f5d29141dbf
Matt Sturgeon <matt@sturgeon.me.uk>
2025-05-27 14:42:29 +0100

    stylix: move testbed `isEnabled` to its own file
    (cherry picked from commit f996bf88703547cb7a21173ba1674fec9b32afe0)

--- stylix/testbed/default.nix
+++ stylix/testbed/default.nix
@@ -6,25 +6,7 @@
 }:
 
 let
-  # Creates a minimal configuration to extract the `stylix.testbed.enable`
-  # option value.
-  #
-  # This is for performance reasons. Primarily, to avoid fully evaluating
-  # testbed system configurations to determine flake outputs.
-  # E.g., when running `nix flake show`.
-  isEnabled =
-    module:
-    let
-      minimal = lib.evalModules {
-        modules = [
-          module
-          ./modules/enable.nix
-          { _module.check = false; }
-          { _module.args = { inherit pkgs; }; }
-        ];
-      };
-    in
-    minimal.config.stylix.testbed.enable;
+  isEnabled = pkgs.callPackage ./is-enabled.nix { };
 
   autoload =
     let
