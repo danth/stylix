22c1dc2ad00b9e8fc46f1d5006a9659db8685cb2
awwpotato <awwpotato@voidq.com>
2025-05-31 11:41:18 -0700

    stylix: refactor overlay to mirror mkTarget

--- modules/gnome-text-editor/overlay.nix
+++ modules/gnome-text-editor/overlay.nix
@@ -1,28 +1,21 @@
 {
-  lib,
-  config,
-  ...
-}:
-let
-  style = config.lib.stylix.colors {
-    template = ../gedit/template.xml.mustache;
-    extension = ".xml";
-  };
-in
-{
+  name = "gnome-text-editor";
+
   overlay =
-    _: prev:
-    lib.optionalAttrs
-      (
-        config.stylix.enable && config.stylix.targets.gnome-text-editor.enable or false
-      )
-      {
-        gnome-text-editor = prev.gnome-text-editor.overrideAttrs (oldAttrs: {
-          postFixup = ''
-            ${oldAttrs.postFixup or ""}
-            mkdir -p $out/share/gtksourceview-5/styles
-            cp ${style} $out/share/gtksourceview-5/styles/stylix.xml
-          '';
-        });
+    { colors }:
+    let
+      style = colors {
+        template = ../gedit/template.xml.mustache;
+        extension = ".xml";
       };
+    in
+    _: prev: {
+      gnome-text-editor = prev.gnome-text-editor.overrideAttrs (oldAttrs: {
+        postFixup = ''
+          ${oldAttrs.postFixup or ""}
+          mkdir -p $out/share/gtksourceview-5/styles
+          cp ${style} $out/share/gtksourceview-5/styles/stylix.xml
+        '';
+      });
+    };
 }
--- stylix/mk-target/default.nix
+++ stylix/mk-target/default.nix
@@ -163,50 +163,10 @@ let
     { config, lib, ... }:
     let
       cfg = config.stylix.targets.${name};
-
-      # Get the list of function de-structured argument names.
-      functionArgNames =
-        fn:
-        lib.pipe fn [
-          lib.functionArgs
-          builtins.attrNames
-        ];
-
-      getStylixAttrs =
-        fn:
-        lib.genAttrs (functionArgNames fn) (
-          arg:
-          if arg == "cfg" then
-            cfg
-          else if arg == "colors" then
-            config.lib.stylix.colors
-          else
-            config.stylix.${arg}
-              or (throw "stylix: mkTarget expected one of `cfg`, `colors`, ${
-                lib.concatMapStringsSep ", " (name: "`${name}`") (
-                  builtins.attrNames config.stylix
-                )
-              }, but got: ${arg}")
-        );
-
-      # Call the configuration function with its required Stylix arguments.
-      mkConfig = fn: fn (getStylixAttrs fn);
-
-      # Safeguard configuration functions when any of their arguments is
-      # disabled, while non-function configurations are unguarded.
-      mkConditionalConfig =
-        c:
-        if builtins.isFunction c then
-          let
-            allAttrsNonNull = lib.pipe c [
-              getStylixAttrs
-              builtins.attrValues
-              (builtins.all (attr: attr != null))
-            ];
-          in
-          lib.mkIf allAttrsNonNull (mkConfig c)
-        else
-          c;
+      inherit (import ./lib.nix { inherit cfg config lib; })
+        mkConfig
+        mkConditionalConfig
+        ;
     in
     {
       inherit imports;
