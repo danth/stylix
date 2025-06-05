baf2f5eecefed87aafe1d4a06eb4abfcc49e30b0
NAHO <90870942+trueNAHO@users.noreply.github.com>
2025-06-05 20:18:59 +0200

    blender: move README into meta description (#1460)
    (cherry picked from commit 937a154dc30ebcf6d2b74fe74c930dd892a127d9)

--- modules/blender/README.md
+++
@@ -1,4 +0,0 @@
-> [!IMPORTANT]
-> The Blender target will have no effect unless the Blender theme is properly
-> [enabled](https://docs.blender.org/manual/en/latest/editors/preferences/themes.html)
-> within Blender itself.
--- modules/blender/meta.nix
+++ modules/blender/meta.nix
@@ -3,4 +3,12 @@
   maintainers = [ lib.maintainers.make-42 ];
   name = "Blender";
   homepage = "https://www.blender.org/";
+  description = ''
+    > [!IMPORTANT]
+    >
+    > This target will have no effect unless the Blender theme is properly
+    > [enabled](
+    > https://docs.blender.org/manual/en/latest/editors/preferences/themes.html)
+    > within Blender itself.
+  '';
 }
