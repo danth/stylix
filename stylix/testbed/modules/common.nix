93894be1d47907b77f8a35669ebbca9679ecd19b
Daniel Thwaites <danth@danth.me>
2025-06-09 16:19:58 +0100

    stylix: test that `documentation.nixos.includeAllModules` works

--- stylix/testbed/modules/common.nix
+++ stylix/testbed/modules/common.nix
@@ -11,6 +11,12 @@ in
 
   nixpkgs.config.allowAliases = false;
 
+  # Test for regressions of https://github.com/nix-community/stylix/issues/98
+  documentation.nixos = {
+    enable = true;
+    includeAllModules = true;
+  };
+
   # The state version can safely track the latest release because the disk
   # image is ephemeral.
   system.stateVersion = config.system.nixos.release;
