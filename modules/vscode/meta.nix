67c8eca32f1baf158ffb9a56f28a2fc8e9852fb0
LemmusLemmus <63229554+LemmusLemmus@users.noreply.github.com>
2025-06-14 14:51:08 +0200

    doc: Add VSCode color customization tip (#1461)
    (cherry picked from commit 751d6c5df0841a0dc213c71f7d43b63abe9449db)

--- modules/vscode/meta.nix
+++ modules/vscode/meta.nix
@@ -6,4 +6,28 @@
     Flameopathic
     danth
   ];
+  description = ''
+    When theming is enabled for VSCode, Stylix will create and apply its own
+    VSCode theme, as well as configure settings concerning fonts.
+    > [!TIP]
+    > The theme created is called
+    > [`Stylix`](
+    > https://github.com/nix-community/stylix/blob/master/modules/vscode/templates/theme.nix),
+    > and if one so desires, its colors can be changed using
+    > `programs.vscode.profiles.<name>.userSettings`
+    > (exactly as one would customize any other VSCode theme).
+    > See the example below; the syntax is explained in the
+    > [VSCode documentation](
+    > https://code.visualstudio.com/docs/configure/themes#_customize-a-color-theme).
+    > ```nix
+    > programs.vscode.profiles.default.userSettings = {
+    >   "workbench.colorCustomizations" = {
+    >     "[Stylix]" = {
+    >       "editor.wordHighlightBackground"  = "#''${colors.base02}66";
+    >       "editor.hoverHighlightBackground" = "#''${colors.base02}66";
+    >     };
+    >   };
+    > };
+    > ```
+  '';
 }
